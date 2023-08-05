af55eee9886fff340f58831bffa455307d4e62f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package fastgac/1.0.5/fastgac-1.0.5-cp36-cp36m-win_amd64.whl

---
+++ packages/fastgac/fastgac-1.0.5-cp36-cp36m-win_amd64.whl/fastgac/__init__.py
@@ -0,0 +1,11 @@
+try:
+    from .fastgac_pybind import * # py2 py3 compatible
+except Exception:
+    # this was installed with as a python wheel
+    from fastgac_pybind import *
+
+try:
+    import pkg_resources  # part of setuptools
+    __version__ = pkg_resources.require("fastgac")[0].version
+except Exception:
+    __version__ = '@PROJECT_VERSION@'
