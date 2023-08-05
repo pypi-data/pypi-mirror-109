33dd75fceaf476a69d98f1d7ac297e66b577d753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package wdiff/0.0.9/wdiff-0.0.9.tar.gz

---
+++ packages/wdiff/wdiff-0.0.9.tar.gz/wdiff-0.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wdiff']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['docrep>=0.3.1,<0.4.0', 'pandas>=0.25,<0.26']
+
+setup_kwargs = {
+    'name': 'wdiff',
+    'version': '0.0.9',
+    'description': 'Analyze how difficult a Spanish word will be to spell',
+    'long_description': None,
+    'author': 'Mario E. Bermonti Pérez',
+    'author_email': 'mbermonti1132@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
