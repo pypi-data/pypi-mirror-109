aa72e9b296754085ce0f651f2454d659d9972768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:53 +0000

    Add package signinghubapi/0.1.1/signinghubapi-0.1.1.tar.gz

---
+++ packages/signinghubapi/signinghubapi-0.1.1.tar.gz/signinghubapi-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='signinghubapi',
+    version='0.1.1',
+    description='Package with default calls for the SigningHub API',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/erwinmintiens/signinghub-api',
+    author='Erwin Mintiens',
+    author_email='erwin.mintiens@gmail.com',
+    keywords=['SigningHub', 'API'],
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=['requests'],
+    license_files=('LICENSE',),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
