c37536876986e6e311422b9c2a13ea39ab6d9ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package dpytest/0.5.0/dpytest-0.5.0.tar.gz

---
+++ packages/dpytest/dpytest-0.5.0.tar.gz/dpytest-0.5.0/discord/ext/test/__init__.py
@@ -0,0 +1,10 @@
+
+__title__ = "dpytest"
+__author__ = "Rune Tynan"
+__license__ = "MIT"
+__copyright__ = "Copyright 2018-2019 CraftSpider"
+__version__ = "0.5.0"
+
+from . import backend
+from .runner import *
+from .verify import verify, Verify, VerifyMessage, VerifyActivity
