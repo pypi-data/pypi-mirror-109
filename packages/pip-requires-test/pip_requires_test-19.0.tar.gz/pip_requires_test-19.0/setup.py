1089e4e1f8dc1f21a4d500cf9468030c49783693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package pip-requires-test/19.0/pip_requires_test-19.0.tar.gz

---
+++ packages/pip-requires-test/pip_requires_test-19.0.tar.gz/pip_requires_test-19.0/setup.py
@@ -0,0 +1,13 @@
+import pkg_resources
+from setuptools import setup
+
+
+version = "19.0"
+
+pkg_resources.require([f"pip >= {version}"])
+
+setup(
+    name="pip_requires_test",
+    version=f"{version}",
+    # install_requires=f"pip>={version}",
+)
