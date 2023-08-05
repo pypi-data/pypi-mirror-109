fa4b190fbc0b2c17051184e011153f9b531cf727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:36 +0000

    Add package rlockertools/0.3.3/rlockertools-0.3.3-py3.8.egg

---
+++ packages/rlockertools/rlockertools-0.3.3-py3.8.egg/rlockertools/exceptions.py
@@ -0,0 +1,5 @@
+class BadRequestError(Exception):
+    ''' Error from the Resource Locker Server! '''
+
+class TimeoutReachedForLockingResource(Exception):
+    ''' In the given timeout range, there were no lockable resources that got free  '''
