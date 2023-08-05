e31b48461dacf35aec327c9a3e585d36a2735d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package amulet-map-editor/0.8.8/amulet_map_editor-0.8.8-py3-none-any.whl

---
+++ packages/amulet-map-editor/amulet_map_editor-0.8.8-py3-none-any.whl/amulet_map_editor/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+if sys.version_info[:2] < (3, 7):
+    raise Exception("Must be using Python 3.7+")
+
+from amulet_map_editor import log
+from amulet_map_editor.api.framework import AmuletApp
+import traceback
+
+
+if __name__ == "__main__":
+    try:
+        app = AmuletApp(0)
+        app.MainLoop()
+    except Exception as e:
+        log.critical(
+            f"Amulet Crashed. Sorry about that. Please report it to a developer if you think this is an issue. \n{traceback.format_exc()}"
+        )
+        input("Press ENTER to continue.")
