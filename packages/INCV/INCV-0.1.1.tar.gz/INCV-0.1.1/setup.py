ec0c8330d3993d57c7e3781fa434551dfd24000d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package INCV/0.1.1/INCV-0.1.1.tar.gz

---
+++ packages/INCV/INCV-0.1.1.tar.gz/INCV-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="INCV",
+      license='MIT',
+      version="0.1.1",
+      description="This package validates the national code of Iranians",
+      author="Majid Azizian",
+      author_email='your.email@domain.com',
+      url='https://github.com/majidazizian/incv',
+      packages=["INCV"]
+      )
