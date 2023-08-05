1c3218dbd4bf300d12c0b843babb0de5b8689b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package deci-client/1.17.1/deci_client-1.17.1-py3-none-any.whl

---
+++ packages/deci-client/deci_client-1.17.1-py3-none-any.whl/deci_client/deci_api/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from deci_client.deci_api.api.companies_api import CompaniesApi
+from deci_client.deci_api.api.platform_api import PlatformApi
+from deci_client.deci_api.api.settings_api import SettingsApi
+from deci_client.deci_api.api.version_api import VersionApi
