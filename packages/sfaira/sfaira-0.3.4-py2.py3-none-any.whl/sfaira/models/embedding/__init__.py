8e62e3119d6d1d3eb07dbd58a45748713c590f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:34 +0000

    Add package sfaira/0.3.4/sfaira-0.3.4-py2.py3-none-any.whl

---
+++ packages/sfaira/sfaira-0.3.4-py2.py3-none-any.whl/sfaira/models/embedding/__init__.py
@@ -0,0 +1,5 @@
+from sfaira.models.embedding.ae import ModelKerasAe, ModelAeVersioned
+from sfaira.models.embedding.vae import ModelKerasVae, ModelVaeVersioned
+from sfaira.models.embedding.linear import ModelKerasLinear, ModelLinearVersioned
+from sfaira.models.embedding.vaeiaf import ModelKerasVaeIAF, ModelVaeIAFVersioned
+from sfaira.models.embedding.vaevamp import ModelKerasVaeVamp, ModelVaeVampVersioned
