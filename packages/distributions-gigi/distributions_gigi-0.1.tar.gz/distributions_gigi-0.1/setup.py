730da82a615746cd1dbc394e40938fbbc3fc7604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package distributions-gigi/0.1/distributions_gigi-0.1.tar.gz

---
+++ packages/distributions-gigi/distributions_gigi-0.1.tar.gz/distributions_gigi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_gigi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_gigi'],
+      zip_safe=False)
