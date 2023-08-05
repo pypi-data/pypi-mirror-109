dd68980c90700169f383f264318d5c6edecd773c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package rabbitmq-alphamoon/0.8.3/rabbitmq-alphamoon-0.8.3.tar.gz

---
+++ packages/rabbitmq-alphamoon/rabbitmq-alphamoon-0.8.3.tar.gz/rabbitmq-alphamoon-0.8.3/rabbitmq_alphamoon/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.8.3'
+
+from rabbitmq_alphamoon.connectors import LOGGER  # noqa
+from rabbitmq_alphamoon.connectors import IQueueConnector  # noqa
+from rabbitmq_alphamoon.connectors import RabbitMQConnector  # noqa
