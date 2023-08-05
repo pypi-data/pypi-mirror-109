68a8b0cbc118e7b2e9ed17168b0b43b280621a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package Orange3/3.29.3/Orange3-3.29.3-cp36-cp36m-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/Orange3/Orange3-3.29.3-cp36-cp36m-manylinux_2_5_x86_64.manylinux1_x86_64.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.29.3'
+version = '3.29.3'
+full_version = '3.29.3'
+git_revision = '93f8ab2cd3d044b484d2b45d11d492872ec7a0ed'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
