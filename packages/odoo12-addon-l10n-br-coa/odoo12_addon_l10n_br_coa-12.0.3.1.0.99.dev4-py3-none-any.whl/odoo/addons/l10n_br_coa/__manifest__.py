ac05722361756b8d16f396ecc353cd72fff1cf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:55 +0000

    Add package odoo12-addon-l10n-br-coa/12.0.3.1.0.99.dev4/odoo12_addon_l10n_br_coa-12.0.3.1.0.99.dev4-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-br-coa/odoo12_addon_l10n_br_coa-12.0.3.1.0.99.dev4-py3-none-any.whl/odoo/addons/l10n_br_coa/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2020 KMEE
+# Copyright (C) 2020 - TODAY Renato Lima - Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/lic enses/agpl).
+
+{
+    "name": "Brazilian COA",
+    "summary": """
+        Base Brasilian Localization for the Chart of Accounts""",
+    "version": "12.0.3.1.0",
+    "license": "AGPL-3",
+    "author": "Akretion, KMEE, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": ["account"],
+    "data": [
+        # Data
+        "data/l10n_br_coa_template.xml",
+        "data/account_tax_tag.xml",
+        "data/account_tax_group.xml",
+        "data/account_tax_template.xml",
+        "data/account_type_data.xml",
+        # Views
+        "views/account_tax_template.xml",
+        "views/account_tax.xml",
+    ],
+    "development_status": "Production/Stable",
+    "installable": True,
+}
