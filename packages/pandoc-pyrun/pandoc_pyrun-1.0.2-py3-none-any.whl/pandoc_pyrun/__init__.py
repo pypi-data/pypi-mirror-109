b959e381383e9eefe80d68e5b1d7e9e51338311a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package pandoc-pyrun/1.0.2/pandoc_pyrun-1.0.2-py3-none-any.whl

---
+++ packages/pandoc-pyrun/pandoc_pyrun-1.0.2-py3-none-any.whl/pandoc_pyrun/__init__.py
@@ -0,0 +1,7 @@
+"""
+Pandoc_pyrun: python code compiler from Markdown files
+====================================
+Pandoc_pyrun is a Python package of a `Pandoc <http://pandoc.org/>` filter.
+It executes python code inside `CodeBlock`. **Code has to be trusted**.
+"""
+from .pandoc_pyrun import *
