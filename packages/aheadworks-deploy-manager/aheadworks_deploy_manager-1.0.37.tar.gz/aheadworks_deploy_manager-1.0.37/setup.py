6a25ff63c29d9a21c2eb391e635a198a8dc88487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:56 +0000

    Add package aheadworks-deploy-manager/1.0.37/aheadworks_deploy_manager-1.0.37.tar.gz

---
+++ packages/aheadworks-deploy-manager/aheadworks_deploy_manager-1.0.37.tar.gz/aheadworks_deploy_manager-1.0.37/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='aheadworks_deploy_manager',
+    version='1.0.37',
+    zip_safe=True,
+    packages=['aheadworks_bitbucket_manager'],
+    package_dir={'aheadworks_bitbucket_manager': 'aheadworks_bitbucket_manager'},
+    package_data={'aheadworks_bitbucket_manager': ['*', '*/*', '*/*/*', '*/*/*/*']},
+    include_package_data=True,
+    url='https://bitbucket.org/awimpl/deploy-tools',
+    license='EULA',
+    author='nnuser',
+    author_email='sednev@aheadworks.com',
+    description='deploy manager from aheadworks',
+    install_requires=[
+        'fire',
+        'python-dateutil',
+        'requests',
+        'docker',
+        'regex',
+    ]
+)
