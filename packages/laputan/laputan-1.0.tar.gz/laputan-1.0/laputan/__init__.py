bf26496f7f438f8ca14a8af1a24ee721464bd3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:13 +0000

    Add package laputan/1.0/laputan-1.0.tar.gz

---
+++ packages/laputan/laputan-1.0.tar.gz/laputan-1.0/laputan/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+print("---------------------------------------------\n")
+print("           -=-=-=-=-=-=-=-=-=-")
+print("          |      laputan      |")
+print("           -=-=-=-=-=-=-=-=-=-")
+# print("\n            Author: D. HU")
+print("\n          Version 1.0 (20210610)")
+print("\n---------------------------------------------")
+
+
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+def iTest():
+	'''
+	Initial test
+	'''
+	print("\nHouston, Tranquility Base here. The Eagle has landed.\n")
+
+if __name__ == '__main__':
+    
+    iTest()
