dbb947261e94dd01bd01cd524e3168c2a686afef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:40 +0000

    Add package aiohttp-sqlalchemy/0.10.0/aiohttp-sqlalchemy-0.10.0.tar.gz

---
+++ packages/aiohttp-sqlalchemy/aiohttp-sqlalchemy-0.10.0.tar.gz/aiohttp-sqlalchemy-0.10.0/aiohttp_sqlalchemy/__init__.py
@@ -0,0 +1,42 @@
+from typing import TYPE_CHECKING
+
+from aiohttp_sqlalchemy.constants import DEFAULT_KEY
+from aiohttp_sqlalchemy.decorators import sa_decorator
+from aiohttp_sqlalchemy.exceptions import DuplicateAppKeyError, \
+                                          DuplicateRequestKeyError
+from aiohttp_sqlalchemy.middlewares import sa_middleware
+from aiohttp_sqlalchemy.views import SAAbstractView, SABaseView, SAView
+
+
+if TYPE_CHECKING:
+    from aiohttp.web import Application
+    from sqlalchemy.ext.asyncio import AsyncSession
+    from typing import Callable, Iterable, Tuple
+
+    TSessionFactory = Callable[..., AsyncSession]
+    TSABinding = Tuple[TSessionFactory, str, bool]
+
+
+__version__ = '0.10.0'
+
+__all__ = ['DuplicateAppKeyError', 'DuplicateRequestKeyError',
+           'SAAbstractView', 'SABaseView', 'sa_bind', 'sa_decorator',
+           'sa_middleware', 'SAView', 'setup',]
+
+
+def sa_bind(factory: 'TSessionFactory', key: str = DEFAULT_KEY, *,
+            middleware: bool = True) -> 'TSABinding':
+    """ Session factory wrapper for binding in setup function. """
+    return factory, key, middleware
+
+
+def setup(app: 'Application', bindings: 'Iterable[TSABinding]'):
+    """ Setup function for binding SQLAlchemy engines. """
+    for factory, key, middleware in bindings:
+        if key in app:
+            raise DuplicateAppKeyError(key)
+
+        app[key] = factory
+
+        if middleware:
+            app.middlewares.append(sa_middleware(key))
