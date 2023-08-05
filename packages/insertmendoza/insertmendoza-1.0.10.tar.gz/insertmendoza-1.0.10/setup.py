281285f39d4910b4c5e35c289347fffde02e00be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:39 +0000

    Add package insertmendoza/1.0.10/insertmendoza-1.0.10.tar.gz

---
+++ packages/insertmendoza/insertmendoza-1.0.10.tar.gz/insertmendoza-1.0.10/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="insertmendoza",
+    version="1.0.10",
+    author="Insert Mendoza",
+    author_email="njavilas@insertmendoza.com.ar",
+    description="Cliente para sarys 6.x",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/njavilas2015/sdk-python-v6",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["requests", "python-socketio"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
