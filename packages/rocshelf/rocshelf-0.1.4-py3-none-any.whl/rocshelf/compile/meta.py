2a55e4735b4021ad2bd347029503f01e37ad3ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:05 +0000

    Add package rocshelf/0.1.4/rocshelf-0.1.4-py3-none-any.whl

---
+++ packages/rocshelf/rocshelf-0.1.4-py3-none-any.whl/rocshelf/compile/meta.py
@@ -0,0 +1,34 @@
+""" Работа с мета файломи """
+
+import shutil
+
+import rlogging
+from rocshelf.config import pcf
+
+logger = rlogging.get_logger('mainLogger')
+
+
+def move_files():
+    """ Перенос файлов корневой директории """
+
+    logger.info('Перемещение мата файлов из "import->meta" в "export->meta"')
+
+    sourceFolderPath = pcf.path('import', 'meta')
+    targetFolderPath = pcf.path('export', 'meta')
+
+    if not sourceFolderPath.check():
+        logger.warning('Перемещение мата файлов остановлено, так как папка "import->meta" не существует')
+        return
+
+    shutil.copytree(
+        str(sourceFolderPath),
+        str(targetFolderPath)
+    )
+
+
+def run():
+    """ Запуск компиляции мета файлов """
+
+    logger.info('Компиляция мета файлов')
+
+    move_files()
