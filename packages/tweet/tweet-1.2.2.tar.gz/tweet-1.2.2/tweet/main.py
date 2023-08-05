a0f23c7e503c89b78da2b60a7e6e7eade7df6c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:57 +0000

    Add package tweet/1.2.2/tweet-1.2.2.tar.gz

---
+++ packages/tweet/tweet-1.2.2.tar.gz/tweet-1.2.2/tweet/main.py
@@ -0,0 +1,60 @@
+import readline
+import signal
+from typing import Final
+
+import typer
+import twitter
+from tweet import settings
+from rich.console import Console
+
+APP_NAME: Final[str] = "tweet"
+
+app: Final[typer.Typer] = typer.Typer()
+console: Final[Console] = Console()
+api: twitter.Api = twitter.Api(
+    consumer_key=settings.CONSUMER_TOKEN,
+    consumer_secret=settings.CONSUMER_SECRET,
+    access_token_key=settings.ACCESS_TOKEN,
+    access_token_secret=settings.ACCESS_SECRET,
+)
+
+
+@app.command()
+def tweet(status: str) -> None:
+    """Tweet
+
+    :param status: str
+    :return: None
+    """
+    print(status)
+    api.PostUpdate(status=status)
+
+
+@app.command()
+def endless() -> None:
+    """
+    :return: None
+    """
+    while True:
+        status = console.input(f":bird: < What's happening?  ")
+        if len(status) == 0:
+            continue
+        api.PostUpdate(status=status)
+        console.print(f":bird: < Tweeted! [bold]“{status}”[/bold]")
+
+
+def init() -> None:
+    global api
+    api = twitter.Api(
+        consumer_key=settings.CONSUMER_TOKEN,
+        consumer_secret=settings.CONSUMER_SECRET,
+        access_token_key=settings.ACCESS_TOKEN,
+        access_token_secret=settings.ACCESS_SECRET,
+    )
+
+
+if __name__ == "__main__":
+    PERIOD: int = 60 * 20
+    signal.signal(signal.SIGALRM, init)
+    signal.setitimer(signal.ITIMER_REAL, PERIOD, PERIOD)
+    app()
