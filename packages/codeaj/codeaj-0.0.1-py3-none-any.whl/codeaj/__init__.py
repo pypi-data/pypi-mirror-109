08734327a68415e86b4c0c4726fd463b2016ade9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:45 +0000

    Add package codeaj/0.0.1/codeaj-0.0.1-py3-none-any.whl

---
+++ packages/codeaj/codeaj-0.0.1-py3-none-any.whl/codeaj/__init__.py
@@ -0,0 +1,6 @@
+from scrap import googleTranslate
+from scrap import scrapall_translate
+from scrap import scrapallparagraph
+from scrap import scrapalldata
+from scrap import scrapone
+from scrap import scrap_div_class
