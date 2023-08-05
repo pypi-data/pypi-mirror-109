a65715fce078ede1ae5095b3283c6aa74a02d6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:29 +0000

    Add package SSML/1.0.2/SSML-1.0.2.tar.gz

---
+++ packages/SSML/SSML-1.0.2.tar.gz/SSML-1.0.2/SSML/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from ssml.Google import Google
+from ssml.Exceptions import *
+
+
+__description__ = "Python library to generate SSML for voice Assistant"
+__version__ = "1.0.2"
