f79c6b97da0ad4d313739c5eb0dafd95583f8057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:54 +0000

    Add package mypy-boto3-sagemaker-featurestore-runtime/1.17.91/mypy_boto3_sagemaker_featurestore_runtime-1.17.91-py3-none-any.whl

---
+++ packages/mypy-boto3-sagemaker-featurestore-runtime/mypy_boto3_sagemaker_featurestore_runtime-1.17.91-py3-none-any.whl/mypy_boto3_sagemaker_featurestore_runtime/type_defs.py
@@ -0,0 +1,39 @@
+"""
+Type annotations for sagemaker-featurestore-runtime service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sagemaker_featurestore_runtime/type_defs.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_sagemaker_featurestore_runtime.type_defs import FeatureValueTypeDef
+
+    data: FeatureValueTypeDef = {...}
+    ```
+"""
+import sys
+from typing import List
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = ("FeatureValueTypeDef", "GetRecordResponseTypeDef")
+
+FeatureValueTypeDef = TypedDict(
+    "FeatureValueTypeDef",
+    {
+        "FeatureName": str,
+        "ValueAsString": str,
+    },
+)
+
+GetRecordResponseTypeDef = TypedDict(
+    "GetRecordResponseTypeDef",
+    {
+        "Record": List["FeatureValueTypeDef"],
+    },
+    total=False,
+)
