82eaecf7a071559947e653dc0f40e1995fc71061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package hcai-datasets/0.0.7/hcai_datasets-0.0.7-py3-none-any.whl

---
+++ packages/hcai-datasets/hcai_datasets-0.0.7-py3-none-any.whl/hcai_datasets/hcai_ckplus/example.py
@@ -0,0 +1,25 @@
+import os
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import hcai_ckplus
+from matplotlib import pyplot as plt
+
+def pp(x, y):
+  img = x.numpy()
+  return img, y
+
+
+## Load Data
+ds, ds_info = tfds.load(
+  'hcai_ckplus',
+  split='train',
+  with_info=True,
+  as_supervised=True,
+  builder_kwargs={'dataset_dir': os.path.join('\\\\137.250.171.12', 'Korpora', 'CK+')}
+)
+
+ds = ds.map(lambda x, y: (tf.py_function(func=pp, inp=[x, y], Tout=[tf.float32, tf.int64])))
+img, label = next(ds.as_numpy_iterator())
+
+plt.imshow(img / 255.)
+plt.show()
