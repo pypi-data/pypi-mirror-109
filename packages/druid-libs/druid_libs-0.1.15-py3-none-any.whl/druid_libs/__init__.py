c2372563170d089e4a9ef2a1f692fc274febca5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package druid-libs/0.1.15/druid_libs-0.1.15-py3-none-any.whl

---
+++ packages/druid-libs/druid_libs-0.1.15-py3-none-any.whl/druid_libs/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for druid-libs"""
+
+from .tests_tools import FakeEvent, context, load_lambda_module  # noqa: F401
+
+__version__ = "0.1.15"
+__author__ = """DRUID DEVAX"""
---
+++ packages/druid-libs/druid_libs-0.1.15-py3-none-any.whl/druid_libs/tests_tools/__init__.py
@@ -0,0 +1,5 @@
+
+from .fake_event import FakeEvent
+from .lambda_module import context, load_lambda_module
+
+__all__ = ["FakeEvent", "context", "load_lambda_module"]
