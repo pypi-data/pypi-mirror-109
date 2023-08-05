4c3fc7c2f5cdbbb80e89406645e3986d3289ddc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package croppy/0.1.0.1.5/croppy-0.1.0.1.5.tar.gz

---
+++ packages/croppy/croppy-0.1.0.1.5.tar.gz/croppy-0.1.0.1.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="croppy", # Replace with your own username
+    version="0.1.0.1.5",
+    author="Surasak Choedpasuporn",
+    author_email="surasak.cho@gmail.com",
+    description="A crop insurance specific package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/surasakcho/croppy",
+    packages=setuptools.find_packages(),    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
