b49cabcc1f3b0af6bdcfb6104dbdb5b018fd2a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package xortool-ciphey/0.1.13/xortool_ciphey-0.1.13.tar.gz

---
+++ packages/xortool-ciphey/xortool_ciphey-0.1.13.tar.gz/xortool_ciphey-0.1.13/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xortool_ciphey']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['docopt>=0.6.2,<0.7.0']
+
+setup_kwargs = {
+    'name': 'xortool-ciphey',
+    'version': '0.1.13',
+    'description': 'Multi-byte xor analysis for Ciphey',
+    'long_description': None,
+    'author': 'bee-san',
+    'author_email': 'github@skerritt.blog',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.4,<4.0',
+}
+
+
+setup(**setup_kwargs)
