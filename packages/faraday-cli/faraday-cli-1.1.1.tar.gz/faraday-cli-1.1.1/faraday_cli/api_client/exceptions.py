609bce7636e844f325f082f481d30a336ac9a100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:08 +0000

    Add package faraday-cli/1.1.1/faraday-cli-1.1.1.tar.gz

---
+++ packages/faraday-cli/faraday-cli-1.1.1.tar.gz/faraday-cli-1.1.1/faraday_cli/api_client/exceptions.py
@@ -0,0 +1,18 @@
+class DuplicatedError(Exception):
+    pass
+
+
+class InvalidCredentials(Exception):
+    pass
+
+
+class Invalid2FA(Exception):
+    pass
+
+
+class MissingConfig(Exception):
+    pass
+
+
+class ExpiredLicense(Exception):
+    pass
