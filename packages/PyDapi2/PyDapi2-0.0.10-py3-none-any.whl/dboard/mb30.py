04a80a6789f07f4521918a84f4ffb2cb59d422c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:44 +0000

    Add package PyDapi2/0.0.10/PyDapi2-0.0.10-py3-none-any.whl

---
+++ packages/PyDapi2/PyDapi2-0.0.10-py3-none-any.whl/dboard/mb30.py
@@ -0,0 +1,20 @@
+'''Module to define the class of MB-30 Dassym's electronic board representation.
+
+:author: F. Voillat
+:date: 2021-02-24 Creation
+'''
+
+from .base import DBoard
+from dboard.base import DBoardPreferedDapiMode
+
+class Board30(DBoard):
+    '''Class for MB-30 Dassym's board.'''
+    
+    number = '30'
+    '''Board type number (str)'''
+
+    def __init__(self, dapi, dmode=DBoardPreferedDapiMode.REGISTER):
+        '''Constructor'''
+        super().__init__(dapi, dmode)
+        self.speed_range.set(1000,40000) 
+        
