8735266fdce29057135b91ba12791da7e1bbf582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:13 +0000

    Add package rappislack/1.5.0/rappislack-1.5.0-py3-none-any.whl

---
+++ packages/rappislack/rappislack-1.5.0-py3-none-any.whl/rappislack/main.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import sys
+
+def incomingWebhook(url, message):
+	try:
+		data = {
+				'blocks': [
+				{
+					'type': "section",
+					'text': {
+						'type': 'mrkdwn',
+						'text': message
+					}
+				},
+				{
+                        'type': 'divider'
+                }
+			]
+		}
+
+		requests.post(url, headers={ 'Content-type': 'application/json' }, data=json.dumps(data))
+	except Exception as e:
+		print(e)
