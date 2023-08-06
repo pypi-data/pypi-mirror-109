500dc48071f9a39eae2ea8ad6bb97e33b87ceb72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:00 +0000

    Add package ajaxlogin/0.0.2/ajaxlogin-0.0.2-py3-none-any.whl

---
+++ packages/ajaxlogin/ajaxlogin-0.0.2-py3-none-any.whl/ajaxlogin/settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+DEFAULT_TOKEN = b"ThisIsAnInsecurePublicTokenDontUseIt"
+
+DEFAULT_SALT = "Quisque vulputate nibh eros, vehicula sollicitudin neque pellentesque quis"
+
+DEFAULT_LOGIN_PATH = 'ajaxlogin'
+
+DEFAULT_NEW_TOKEN_PATH = 'ajaxtoken'
