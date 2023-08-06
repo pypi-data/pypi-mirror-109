7ed7a7b85ba71866cb4bee0db3f2bb7fa203a3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:03 +0000

    Add package fastbooru/0.1.3/fastbooru-0.1.3.tar.gz

---
+++ packages/fastbooru/fastbooru-0.1.3.tar.gz/fastbooru-0.1.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+	name = "fastbooru",
+	version= "0.1.3",
+	description = "Because all other cli downloaders suck...",
+	long_description = open("README.md", "r").read(),
+	long_description_content_type = "text/markdown",
+	author = "Cloud11665",
+	author_email = 'Cloud11665@protonmail.com',
+	url = "https://github.com/cloud11665/fastbooru",
+	packages = setuptools.find_packages(),
+	install_requires = setuptools.find_packages(),
+	license= "gnu GPL-3.0",
+	include_package_data = True,
+	package_data = {"fastbooru": ["*.hy"]},
+	entry_points = {
+		"console_scripts": [
+			"fastbooru = fastbooru.runner:_hook"
+		]
+	},
+	classifiers=[
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Operating System :: OS Independent"
+	],
+	python_requires = ">=3.7"
+)
