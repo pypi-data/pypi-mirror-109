e8fcc97eba202b9e3d948bc51844d3bb926cf33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:38 +0000

    Add package SavageML/0.1.0/SavageML-0.1.0.tar.gz

---
+++ packages/SavageML/SavageML-0.1.0.tar.gz/SavageML-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SavageML',
+    version='0.1.0',
+    packages=['savageml'],
+    py_modules=['test'],
+    url='https://github.com/SavagePrograming/SavageML',
+    license='MIT License',
+    author='William Savage',
+    author_email='savage.programing@gmail.com',
+    description='A Personal Experimental Machine Learning Library'
+)
