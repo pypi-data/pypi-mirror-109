22ab4e45b89fdc6d79d8f47ca6b66df0ca24a448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:30 +0000

    Add package algosquare/0.0.1/algosquare-0.0.1.tar.gz

---
+++ packages/algosquare/algosquare-0.0.1.tar.gz/algosquare-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  name = 'algosquare',         
+  version = '0.0.1',      
+  description = '',
+  long_description = '',  
+  author = 'AlgoSquare Contributors',                 
+  author_email = 'dev@algosquare.ai',     
+  url = 'https://algosquare.ai',   
+  packages=[],
+  install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
