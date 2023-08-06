256479ab1f2fb494ffa9c1fe2c1ef80cb6cae632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:33 +0000

    Add package traductorkq/0.1.0/traductorkq-0.1.0.tar.gz

---
+++ packages/traductorkq/traductorkq-0.1.0.tar.gz/traductorkq-0.1.0/README.md
@@ -0,0 +1,13 @@
+# Traductor
+
+Programa para traducir palabras de Ingles a Espanol
+
+## Instalacion
+
+pip install traductorkq
+
+## Forma de uso
+
+traductorkq "hello"
+
+hello -> hola
---
+++ packages/traductorkq/traductorkq-0.1.0.tar.gz/traductorkq-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['traductorkq']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.1,<9.0.0', 'translate>=3.5.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'traductorkq',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '# Traductor\n\nPrograma para traducir palabras de Ingles a Espanol\n\n## Instalacion\n\npip install traductorkq\n\n## Forma de uso\n\ntraductorkq "hello"\n\nhello -> hola',
+    'author': 'Kevin',
+    'author_email': 'kevin.kjqm@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Kevinq181/translatorkq.git',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
