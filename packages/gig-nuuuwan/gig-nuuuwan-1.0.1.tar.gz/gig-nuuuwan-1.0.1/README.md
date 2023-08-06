6df47bbdce2d708c5cbb59a0c24d27972f82fed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:53 +0000

    Add package gig-nuuuwan/1.0.1/gig-nuuuwan-1.0.1.tar.gz

---
+++ packages/gig-nuuuwan/gig-nuuuwan-1.0.1.tar.gz/gig-nuuuwan-1.0.1/README.md
@@ -0,0 +1,26 @@
+# gig
+
+## Setup and Install
+
+To install a stable version:
+
+```
+pip install gig-nuuuwan
+```
+
+To install a pre-release (which might have more features, but also be
+less stable):
+
+```
+pip install -i https://test.pypi.org/simple/ gig-nuuuwan
+```
+
+## Release History
+
+### 1.0.1
+
+* Added ents.get_fuzzy_fp
+
+### 1.0.0
+
+* Initial Release . See source for details.
---
+++ packages/gig-nuuuwan/gig-nuuuwan-1.0.1.tar.gz/gig-nuuuwan-1.0.1/setup.py
@@ -0,0 +1,44 @@
+"""Setup."""
+import time
+import setuptools
+DIST_NAME = 'gig'
+
+IS_PRE_RELEASE = False
+MAJOR, MINOR, PATCH = 1, 0, 1
+if IS_PRE_RELEASE:
+    PRE_RELEASE_LABEL = '%d' % (time.time())
+    version = '%d.%d.%drc%s' % (MAJOR, MINOR, PATCH, PRE_RELEASE_LABEL)
+else:
+    version = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
+
+with open("src/%s/README.md" % DIST_NAME, "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="%s-nuuuwan" % DIST_NAME,
+    version=version,
+    author="Nuwan I. Senaratna",
+    author_email="nuuuwan@gmail.com",
+    description="Generalized information graph.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nuuuwan/%s" % DIST_NAME,
+    project_urls={
+        "Bug Tracker": "https://github.com/nuuuwan/%s/issues" % DIST_NAME,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+
+    install_requires=[
+        'utils-nuuuwan',
+    ],
+
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
