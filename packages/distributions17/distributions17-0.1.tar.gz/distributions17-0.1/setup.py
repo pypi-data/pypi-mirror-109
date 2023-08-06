8caa17c1c6d2224e2f6afb268e5c11e055671550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:39 +0000

    Add package distributions17/0.1/distributions17-0.1.tar.gz

---
+++ packages/distributions17/distributions17-0.1.tar.gz/distributions17-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions17',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions17'],
+      author = 'Andrew Alarcon',
+      zip_safe=False)
