ef58b1ec03ef09e0fe5a7a76e86667af6ac3c9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:50 +0000

    Add package hue-api/0.4.0/hue_api-0.4.0-py2.py3-none-any.whl

---
+++ packages/hue-api/hue_api-0.4.0-py2.py3-none-any.whl/hue/api/bridge.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import Any
+
+from . import http
+
+
+class Bridge:
+    def __init__(self: Bridge, *, ip: str, user: str):
+        self.ip: str = ip
+        self.user: str = user
+        self.info: dict[str, Any] = {}
+
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        return f"<{class_name} {self.ip}>"
+
+    @property
+    def url(self) -> str:
+        return f"http://{self.ip}/api/{self.user}"
+
+    @staticmethod
+    async def discover() -> dict[str, str]:
+        return await http.get_json("https://discovery.meethue.com/")
+
+    async def get_info(self) -> dict[str, Any]:
+        self.info = await http.get_json(self.url)
+        return self.info
+
+    async def get_config(self) -> dict[str, Any]:
+        resp = await self.get_info()
+        return resp["config"]
