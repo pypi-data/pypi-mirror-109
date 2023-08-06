1df26b5dc74c038bcbc79f1ced8ceff12bfb6ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:55:46 +0000

    Add package libretranslatepy/2.1.1/libretranslatepy-2.1.1.tar.gz

---
+++ packages/libretranslatepy/libretranslatepy-2.1.1.tar.gz/libretranslatepy-2.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+        name='libretranslatepy',
+        version='2.1.1',
+        description='Python bindings for LibreTranslate API',
+        url='https://www.argosopentech.com',
+        packages=find_packages()
+)
