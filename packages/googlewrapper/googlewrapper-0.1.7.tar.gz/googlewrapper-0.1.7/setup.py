149dbcb8853add0409e9e2437f76e49429dc8593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:41 +0000

    Add package googlewrapper/0.1.7/googlewrapper-0.1.7.tar.gz

---
+++ packages/googlewrapper/googlewrapper-0.1.7.tar.gz/googlewrapper-0.1.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+#https://stackoverflow.com/questions/1471994/what-is-setup-py#:~:text=setup.py%20is%20a%20python,to%20easily%20install%20Python%20packages.
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="googlewrapper",
+    version="0.1.7",
+    author="Jace Iverson",
+    author_email="iverson.jace@gmail.com",
+    description="simple wrapper on Google API connections",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaceiverson/google-wrapper",
+    packages = ["googlewrapper"
+            ],
+    install_requires=[
+            "google-api-python-client==2.4.0",
+            "oauth2client==4.1.3",
+            "pandas==1.2.4",
+            "pygsheets==2.0.5",
+            "google-cloud-bigquery==2.16.1",
+            "pandas-gbq==0.15.0"
+                ]
+)
