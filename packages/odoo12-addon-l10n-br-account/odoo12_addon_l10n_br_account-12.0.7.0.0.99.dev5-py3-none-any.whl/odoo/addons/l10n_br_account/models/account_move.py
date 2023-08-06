46d1946ebab1398e34f36f545868688dd6fc5fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:28 +0000

    Add package odoo12-addon-l10n-br-account/12.0.7.0.0.99.dev5/odoo12_addon_l10n_br_account-12.0.7.0.0.99.dev5-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-br-account/odoo12_addon_l10n_br_account-12.0.7.0.0.99.dev5-py3-none-any.whl/odoo/addons/l10n_br_account/models/account_move.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2021 - TODAY Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models
+
+from odoo.addons.l10n_br_fiscal.constants.fiscal import (
+    DOCUMENT_ISSUER_COMPANY,
+    SITUACAO_EDOC_AUTORIZADA,
+)
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def post(self, invoice=False):
+        dummy_doc = self.env.ref("l10n_br_fiscal.fiscal_document_dummy")
+        result = super().post(invoice)
+        if invoice:
+            if (
+                invoice.fiscal_document_id != dummy_doc
+                and invoice.document_electronic
+                and invoice.issuer == DOCUMENT_ISSUER_COMPANY
+                and invoice.state_edoc != SITUACAO_EDOC_AUTORIZADA
+            ):
+                self.button_cancel()
+        return result
