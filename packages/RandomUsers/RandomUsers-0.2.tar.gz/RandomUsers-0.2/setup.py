b2f51b9d797fcb1b1c30ba6464bc8b8351abc10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:03:48 +0000

    Add package RandomUsers/0.2/RandomUsers-0.2.tar.gz

---
+++ packages/RandomUsers/RandomUsers-0.2.tar.gz/RandomUsers-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RandomUsers",
+    description="A simple package helps you generate fake users.",
+    long_description=open("README.md").read(),
+    version="0.2",
+    author="SiriusKoan",
+    author_email="k.peihsun@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/SiriusKoan/RandomUsers",
+    license="MIT",
+    install_requires=[],
+)
