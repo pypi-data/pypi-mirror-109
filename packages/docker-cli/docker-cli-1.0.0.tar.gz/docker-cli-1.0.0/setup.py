6b787bb4e70c969a9c582573657dc21fcaae88ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:14 +0000

    Add package docker-cli/1.0.0/docker-cli-1.0.0.tar.gz

---
+++ packages/docker-cli/docker-cli-1.0.0.tar.gz/docker-cli-1.0.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['docker_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cleantext>=1.1.3,<2.0.0',
+ 'delegator.py>=0.1.1,<0.2.0',
+ 'docker-compose>=1.29.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'docker-cli',
+    'version': '1.0.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Manu',
+    'author_email': 'grajmanu@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
