bc6075c532544b327f81807bbc7b78bbc486fa3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:39 +0000

    Add package pfsc/0.0.1/pfsc-0.0.1.tar.gz

---
+++ packages/pfsc/pfsc-0.0.1.tar.gz/pfsc-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pfsc",
+    version="0.0.1",
+    author="Steve Kieffer",
+    author_email="sk314159@runbox.com",
+    description="Utilities for Proofscape",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
+
