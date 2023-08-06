74492dbd83c147ed683b5cffb9cbf8557d73870f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:28 +0000

    Add package colorlog/6.3.0a1/colorlog-6.3.0a1.tar.gz

---
+++ packages/colorlog/colorlog-6.3.0a1.tar.gz/colorlog-6.3.0a1/colorlog/__init__.py
@@ -0,0 +1,51 @@
+"""A logging formatter for colored output."""
+
+import sys
+import warnings
+
+from colorlog.formatter import (
+    default_log_colors,
+    ColoredFormatter,
+    LevelFormatter,
+    TTYColoredFormatter,
+)
+
+from colorlog.logging import (
+    basicConfig,
+    root,
+    getLogger,
+    log,
+    debug,
+    info,
+    warning,
+    error,
+    exception,
+    critical,
+    StreamHandler,
+)
+
+__all__ = (
+    "ColoredFormatter",
+    "default_log_colors",
+    "basicConfig",
+    "root",
+    "getLogger",
+    "debug",
+    "info",
+    "warning",
+    "error",
+    "exception",
+    "critical",
+    "log",
+    "exception",
+    "StreamHandler",
+    "LevelFormatter",
+    "TTYColoredFormatter",
+)
+
+if sys.version_info < (3, 6):
+    warnings.warn(
+        "Colorlog requires Python 3.6 or above. Pin 'colorlog<5' to your dependencies "
+        "if you require compatibility with older versions of Python. See "
+        "https://github.com/borntyping/python-colorlog#status for more information."
+    )
---
+++ packages/colorlog/colorlog-6.3.0a1.tar.gz/colorlog-6.3.0a1/colorlog/logging.py
@@ -0,0 +1,63 @@
+"""Wrappers around the logging module."""
+
+import functools
+import logging
+import typing
+
+import colorlog.formatter
+
+BASIC_FORMAT = "%(log_color)s%(levelname)s%(reset)s:%(name)s:%(message)s"
+
+
+def basicConfig(
+    style: str = "%",
+    log_colors: typing.Optional[colorlog.formatter.LogColors] = None,
+    reset: bool = True,
+    secondary_log_colors: typing.Optional[colorlog.formatter.SecondaryLogColors] = None,
+    format: str = BASIC_FORMAT,
+    datefmt: typing.Optional[str] = None,
+    **kwargs
+) -> None:
+    """Call ``logging.basicConfig`` and override the formatter it creates."""
+    logging.basicConfig(**kwargs)
+    logging._acquireLock()  # type: ignore
+    try:
+        handler = logging.root.handlers[0]
+        handler.setFormatter(
+            colorlog.formatter.ColoredFormatter(
+                fmt=format,
+                datefmt=datefmt,
+                style=style,
+                log_colors=log_colors,
+                reset=reset,
+                secondary_log_colors=secondary_log_colors,
+                stream=kwargs.get("stream", None),
+            )
+        )
+    finally:
+        logging._releaseLock()  # type: ignore
+
+
+def ensure_configured(func):
+    """Modify a function to call ``basicConfig`` first if no handlers exist."""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if len(logging.root.handlers) == 0:
+            basicConfig()
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+root = logging.root
+getLogger = logging.getLogger
+debug = ensure_configured(logging.debug)
+info = ensure_configured(logging.info)
+warning = ensure_configured(logging.warning)
+error = ensure_configured(logging.error)
+critical = ensure_configured(logging.critical)
+log = ensure_configured(logging.log)
+exception = ensure_configured(logging.exception)
+
+StreamHandler = logging.StreamHandler
