8248c4964a5f98904d2f4e464b19e76e0258f08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:38 +0000

    Add package dj-gravatar/0.0.2/dj_gravatar-0.0.2-py3-none-any.whl

---
+++ packages/dj-gravatar/dj_gravatar-0.0.2-py3-none-any.whl/dj_gravatar/templatetags/gravatar.py
@@ -0,0 +1,34 @@
+from django.template import Library
+from django.conf import settings
+from django.utils.safestring import mark_safe
+from  ..settings import configure_default_settings
+
+configure_default_settings()
+
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+
+import hashlib
+
+register = Library()
+
+
+@register.simple_tag
+def gravatar_url(email, size=80):
+    url = 'http'
+    if settings.GRAVATAR_SECURE:
+        url += 's'
+    url += '://www.gravatar.com/avatar/'  + hashlib.md5(email.lower().encode('utf-8')).hexdigest() + '?'
+    url += urlencode([
+        ('s', str(size)),
+        ('d', settings.GRAVATAR_DEFAULT_URL)
+    ])
+    return url
+
+
+@register.simple_tag
+def gravatar(email, size=80, options=""):
+    url = gravatar_url(email, size)
+    return mark_safe('<img src="%s" width="%s" height="%s" %s >' % (url, size, size, options))
