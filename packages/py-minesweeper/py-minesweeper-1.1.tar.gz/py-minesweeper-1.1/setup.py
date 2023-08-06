bbfcdb9ee10ab6edd4482ced25a6e9a93f68e7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:11 +0000

    Add package py-minesweeper/1.1/py-minesweeper-1.1.tar.gz

---
+++ packages/py-minesweeper/py-minesweeper-1.1.tar.gz/py-minesweeper-1.1/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "py-minesweeper",
+    version = "1.1",
+    author = "Tekcno",
+    author_email = "tekcno@tekcno.com",
+    description = ("Minesweeper engine for python"),
+    url = "",
+    packages=['pyminesweeper'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities"
+    ],
+)
