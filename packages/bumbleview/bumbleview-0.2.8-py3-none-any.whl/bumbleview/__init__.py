397a9314251cd5b32bbc73c08166f025e85ea706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:54 +0000

    Add package bumbleview/0.2.8/bumbleview-0.2.8-py3-none-any.whl

---
+++ packages/bumbleview/bumbleview-0.2.8-py3-none-any.whl/bumbleview/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# version
+__version__ = "0.2.8"
+
+# run
+print("Welcome to the bumbleview package. Here you can convert emission spectra to its reaction in an insect eye.")
