4ed01fc7ce210aca5e6c6df1bcaacdd57ab29e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:10 +0000

    Add package color-palette-cli/0.1.11/color_palette_cli-0.1.11.tar.gz

---
+++ packages/color-palette-cli/color_palette_cli-0.1.11.tar.gz/color_palette_cli-0.1.11/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name = 'color_palette_cli',
+    version = '0.1.11',
+    author = 'Adam T',
+    author_email = 'adam.tabaczynski@bitcomp.fi',
+    description = 'Command Line Interface allowing interaction with color_palette_at library.',
+    long_description = readme(),
+    long_description_content_type = "text/markdown",
+    licence = 'MIT',
+    keywords = 'color color_palette colors cli',
+    url = '',
+    entry_points = {
+        'console_scripts': [
+            'cli_color = color_palette_cli.color_palette_cli:CLIColor.get_color_palette',
+            ],
+        },
+    packages = find_packages(include=['color_palette_cli']),
+    install_requires = [
+        'color_palette_at',
+    ],
+    python_requires = '>=3.7',   
+    classifiers = [
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
