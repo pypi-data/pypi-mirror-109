c5fc9f59ff85c2eb0e91d0bcf2c2e6fe9d78565b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:31 +0000

    Add package absolutlynothing/1.1.0/absolutlynothing-1.1.0.tar.gz

---
+++ packages/absolutlynothing/absolutlynothing-1.1.0.tar.gz/absolutlynothing-1.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+readme = "jhbjhbghujhbgyuhgvhyhbvgyujhbvgyuujhbghyjnbvgyjhgyujhgyujhgtyuiujhyikjhuy78uhgyt7yt67ygftygtyuygtyyug"
+
+setuptools.setup(
+    name="absolutlynothing",
+    version="1.1.0",
+    author="Nathan Adams",
+    description="Nothing",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+
+    python_requires='>=3.6',
+    py_modules=["absolutlynothing"],
+    install_requires=[]
+    )
