4e5d7301afca786cfdc9bd707ce062c8ecea12c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:31 +0000

    Add package semantic-search-faiss/0.0.3/semantic_search_faiss-0.0.3-py3-none-any.whl

---
+++ packages/semantic-search-faiss/semantic_search_faiss-0.0.3-py3-none-any.whl/semanticsearch/config.py
@@ -0,0 +1,7 @@
+"""Project configurations
+"""
+
+CROSS_ENCODER = "cross-encoder/ms-marco-TinyBERT-L-2-v2"
+BI_ENCODER = "retrieve_rank"
+INDEX="faiss_index"
+DATA="https://github.com/Nandhagopalan/Semanticsearch/releases/download/0.0.1/covid_papers.csv"
