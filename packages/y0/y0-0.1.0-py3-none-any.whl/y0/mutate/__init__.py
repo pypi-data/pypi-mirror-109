6a761dfbb57455d884e4f563ebd401fefd60f95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:49 +0000

    Add package y0/0.1.0/y0-0.1.0-py3-none-any.whl

---
+++ packages/y0/y0-0.1.0-py3-none-any.whl/y0/mutate/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Functions that mutate probability expressions."""
+
+from .canonicalize_expr import canonicalize
+from .chain import bayes_expand, chain_expand, fraction_expand
+
+__all__ = [
+    "canonicalize",
+    "chain_expand",
+    "fraction_expand",
+    "bayes_expand",
+]
