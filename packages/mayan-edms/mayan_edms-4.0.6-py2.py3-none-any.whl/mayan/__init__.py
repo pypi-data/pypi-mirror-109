46f663193eb08067c6ecd486d1e99a95ab00409a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:12 +0000

    Add package mayan-edms/4.0.6/mayan_edms-4.0.6-py2.py3-none-any.whl

---
+++ packages/mayan-edms/mayan_edms-4.0.6-py2.py3-none-any.whl/mayan/__init__.py
@@ -0,0 +1,12 @@
+__title__ = 'Mayan EDMS'
+__version__ = '4.0.6'
+__build__ = 0x040006
+__build_string__ = 'v4.0.6_Thu Jun 10 11:44:09 2021 -0400'
+__django_version__ = '2.2'
+__author__ = 'Roberto Rosario'
+__author_email__ = 'roberto.rosario@mayan-edms.com'
+__description__ = 'Free Open Source Electronic Document Management System'
+__license__ = 'Apache 2.0'
+__copyright_short__ = '2011 Roberto Rosario'
+__copyright__ = '{} {}'.format('Copyright', __copyright_short__)
+__website__ = 'https://www.mayan-edms.com'
