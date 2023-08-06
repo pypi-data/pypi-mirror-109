abb215e92bb838f5cdca21d5d5f2abe5135a9798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:41 +0000

    Add package django-betterforms3/2/django-betterforms3-2.tar.gz

---
+++ packages/django-betterforms3/django-betterforms3-2.tar.gz/django-betterforms3-2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+__doc__ = """
+App for Django 3 featuring improved form base classes.
+"""
+
+version = '2'
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='django-betterforms3',
+    version=version,
+    description=__doc__,
+    long_description=read('README.rst'),
+    author="el0ny",
+    packages=[package for package in find_packages()
+              if package.startswith('betterforms')],
+    install_requires=['Django>=1.8', 'six>=1.16.0'],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
