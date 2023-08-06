a49ed44f893d018f005da14ec2810af87521ddbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:27 +0000

    Add package django-loader/0.0.1/django-loader-0.0.1.tar.gz

---
+++ packages/django-loader/django-loader-0.0.1.tar.gz/django-loader-0.0.1/loader/__init__.py
@@ -0,0 +1,13 @@
+# ******************************************************************************
+#
+# django-loader, a configuration and secret loader for Django
+#
+# __init__.py:  loader module interface
+#
+# Copyright (C) 2021 Jeremy A Gray <gray@flyquackswim.com>.
+#
+# SPDX-License-Identifier: MIT
+#
+# ******************************************************************************
+#
+"""Loader module."""
