4a1cb517ba8072bae29c95fa67f42550d3bc7c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:03 +0000

    Add package pltRDF/0.0.1/pltRDF-0.0.1.tar.gz

---
+++ packages/pltRDF/pltRDF-0.0.1.tar.gz/pltRDF-0.0.1/README.md
@@ -0,0 +1,29 @@
+# pltRDF
+
+# Radial Distribution Function (RDF)
+
+pltRDF is a Python package to plot RDF by reading a GSD file.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install pltRDF.
+
+```bash
+pip install pltRDF
+```
+
+## Usage
+
+```python
+import pltRDF
+
+pltRDF.rdf("input_file.gsd", "output_file.pdf") # a PDF file for RDF
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/
---
+++ packages/pltRDF/pltRDF-0.0.1.tar.gz/pltRDF-0.0.1/setup.py
@@ -0,0 +1,46 @@
+"""A setuptools based setup module."""
+
+import pathlib
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+        name='pltRDF',
+        version='0.0.1',
+        description='A sample Python project',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/sumitavakundu007/pltRDF',
+        author='Sumitava Kundu',
+        author_email='kundusumitava@gmail.com',
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            ],
+        keywords='sample, setuptools, development', 
+        packages=['pltRDF'],
+        python_requires='>=3.6',
+        install_requires=['gsd', 'freud-analysis', 'matplotlib'],
+        
+        entry_points={
+                'console_scripts': [
+                'pltRDF=pltRDF.plot_rdf:rdf',
+            ],
+        },
+        
+        project_urls={  # Optional
+            'Bug Reports': 'https://github.com/sumitavakundu007/pltRDF/issues',
+            'Say Thanks!': 'http://saythanks.io/to/example',
+            'Source': 'https://github.com/sumitavakundu007/pltRDF/',
+            },
+        )
