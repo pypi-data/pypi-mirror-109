e3494e5ced67b215c1c6e3adc866a5be9e8b0aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:41 +0000

    Add package pdscheduling/0.1.0/pdscheduling-0.1.0.tar.gz

---
+++ packages/pdscheduling/pdscheduling-0.1.0.tar.gz/pdscheduling-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pdscheduling']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pdscheduling',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '# PDScheduling - a library to create schedules in PagerDuty\n\nJust generate an array with a user for every hour for the next week - and the library will push it to PagerDuty. \n\n## Example:\n\n```python\nimport random\nfrom pdscheduling import PagerDuty\n\npd = PagerDuty("token")\nusers = pd.get_users()\nschedule = []\nfor day in range(7):\n    user = random.choice(users) # your fancy algorithm to select a user for the day\n    for hour in range(24): # btw, the week starts in UTC timezone\n        schedule += [user["id"]]\npd.create_or_update_schedule(name="Automatic Schedule", hours=schedule)\n```\n\n## Why library? Can I just use PagerDuty API?\n\nYou can, but it will be harder. PagerDuty don\'t give straightforward API for this, instead you need to create schedule\nwith a layer for every developer with proper restriction.\n\n## Limitation\n\nLibrary can fetch only first 100 users and schedules. It\'s possible to fix, I just did not prioritize this feature.\n\n## OptDuty\n\nThe library extracted from https://optduty.com. If you need a help with library or to create scheduling system please\nreach out roman@optduty.com\n',
+    'author': 'Roman',
+    'author_email': 'roman@optduty.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
