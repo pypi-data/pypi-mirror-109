474c30ed22a4a186b71f5e51b05347f44028b048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:03:21 +0000

    Add package mon-test/0.0.1/mon_test-0.0.1-py3-none-any.whl

---
+++ packages/mon-test/mon_test-0.0.1-py3-none-any.whl/mon_test/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+    Ce module est un test
+"""
+
+
+__version__ = "0.0.1"
+ 
+from mon_test.mon_test_script import mon_script_methode
+from mon_test.mon_test_script import favorite_meal
+from mon_test.mon_test_script import voiture_de_reve
+from mon_test.mon_test_script import list_de_toutou
+from mon_test.mon_test_script import associe
