9ce5250ad580f828403c6fd5cfd706186fe702b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:39 +0000

    Add package vaas-sdk-python/1.0.2/vaas-sdk-python-1.0.2.tar.gz

---
+++ packages/vaas-sdk-python/vaas-sdk-python-1.0.2.tar.gz/vaas-sdk-python-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vaas-sdk-python",
+    version="1.0.2",
+    description="vaas-sdk",
+    author="wangmq",
+    url="https://github.com/yilanyun/vaas-sdk-python.git",
+    packages=find_packages()
+)
