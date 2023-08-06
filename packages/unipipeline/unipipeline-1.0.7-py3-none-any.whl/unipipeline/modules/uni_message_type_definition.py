c347ae2f19f7f69700b382d82611fac4859a2b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:20 +0000

    Add package unipipeline/1.0.7/unipipeline-1.0.7-py3-none-any.whl

---
+++ packages/unipipeline/unipipeline-1.0.7-py3-none-any.whl/unipipeline/modules/uni_message_type_definition.py
@@ -0,0 +1,9 @@
+from typing import NamedTuple
+
+from unipipeline.modules.uni_module_definition import UniModuleDefinition
+
+
+class UniMessageTypeDefinition(NamedTuple):
+    name: str
+    type: UniModuleDefinition
+    version: int
