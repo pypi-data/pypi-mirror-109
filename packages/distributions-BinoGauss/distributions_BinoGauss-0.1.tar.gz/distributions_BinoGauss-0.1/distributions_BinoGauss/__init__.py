f37f789d5bfb0061ef8a35dc6b9449cf36c6ccf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:09 +0000

    Add package distributions-BinoGauss/0.1/distributions_BinoGauss-0.1.tar.gz

---
+++ packages/distributions-BinoGauss/distributions_BinoGauss-0.1.tar.gz/distributions_BinoGauss-0.1/distributions_BinoGauss/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
+
---
+++ packages/distributions-BinoGauss/distributions_BinoGauss-0.1.tar.gz/distributions_BinoGauss-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_BinoGauss',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_BinoGauss'],
+      author = 'Pongrak Keawsrinuan',
+      author_email = 'pongrak.k@hotmail.com',
+      zip_safe=False)
