d272617babc6230a250d93d13e710f650aa5a97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:48 +0000

    Add package merit/0.3/merit-0.3.tar.gz

---
+++ packages/merit/merit-0.3.tar.gz/merit-0.3/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'merit',
+  packages = ['merit'],
+  version = '0.3',
+  license='MIT',
+  description = 'A Python SDK for the Merit API.',
+  long_description = "file: README.md",
+  long_description_content_type = "text/markdown",
+  author = 'Taylor Robinson',
+  author_email = 'taylor.howard.robinson@gmail.com',
+  url = 'https://github.com/tayrobin/merit',
+  download_url = 'https://github.com/tayrobin/merit/archive/refs/tags/0.3.tar.gz',
+  keywords = ['Merit', 'API', 'SDK', 'Digital Credentials'],
+  install_requires=[
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
