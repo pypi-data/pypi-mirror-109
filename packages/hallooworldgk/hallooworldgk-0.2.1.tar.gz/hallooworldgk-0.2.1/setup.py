a58320f7842b7a643c64bceae181974cbb4f812c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:39 +0000

    Add package hallooworldgk/0.2.1/hallooworldgk-0.2.1.tar.gz

---
+++ packages/hallooworldgk/hallooworldgk-0.2.1.tar.gz/hallooworldgk-0.2.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.2.1'
+DESCRIPTION = 'A basic hello package'
+LONG_DESCRIPTION = 'A basic hello package long desc'
+
+# Setting up
+setup(
+    name="hallooworldgk",
+    version=VERSION,
+    author="Hew",
+    author_email="<hew92932@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    url="https://github.com/",
+    license="GPL 3",
+    install_requires=['PyQt5', 'librosa'],
+    keywords=['hello', 'world'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+
+    entry_points =
+    {   "console_scripts":
+        [
+            "halloo = libname:helloworld"
+            "halloo = libname:jah"
+        ]
+    }
+
+)
