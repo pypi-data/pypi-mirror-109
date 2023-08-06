70e4f948e22798d0f3f6c236dccb4c4b70b3a067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:05:04 +0000

    Add package upswingutil/1.0.5/upswingutil-1.0.5-py3-none-any.whl

---
+++ packages/upswingutil/upswingutil-1.0.5-py3-none-any.whl/upswingutil/schema/__init__.py
@@ -0,0 +1,7 @@
+from .property import Property, PropertyAirport, PropertyTransportation, PropertyCreditCardTypes, PropertyHotelAmenities, \
+    PropertyAccountsReceivableDetails
+from .auth import Token
+from .pubSub import ReservationMsg
+from .reservation import Reservation
+from .country import Country
+from .alvie import AlvieRating, Alvie
