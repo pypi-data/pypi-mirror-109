b4d929084b8c5e451e5ec57b4337d788b4022c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:21 +0000

    Add package DIRAC/7.3.0a11/DIRAC-7.3.0a11-py3-none-any.whl

---
+++ packages/DIRAC/DIRAC-7.3.0a11-py3-none-any.whl/DIRAC/Core/Utilities/Version.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+__RCSID__ = "$Id$"
+
+import importlib
+
+from DIRAC import S_OK
+from DIRAC.Core.Utilities.Extensions import extensionsByPriority
+
+
+def getCurrentVersion():
+  """ Get a string corresponding to the current version of the DIRAC package and all the installed
+      extension packages
+  """
+  for ext in extensionsByPriority():
+    try:
+      return S_OK(importlib.import_module(ext).version)
+    except (ImportError, AttributeError):
+      pass
+
+
+def getVersion():
+  """ Get a dictionary corresponding to the current version of the DIRAC package and all the installed
+      extension packages
+  """
+  vDict = {'Extensions': {}}
+  for ext in extensionsByPriority():
+    try:
+      version = importlib.import_module(ext).version
+    except (ImportError, AttributeError):
+      pass
+    if ext.endswith("DIRAC") and ext != "DIRAC":
+      ext = ext[:len("DIRAC")]
+    vDict['Extensions'][ext] = version
+  return S_OK(vDict)
