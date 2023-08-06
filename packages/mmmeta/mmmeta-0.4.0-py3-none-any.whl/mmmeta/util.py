561c51e0482ce42c5353154337492c8cb2676664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:55 +0000

    Add package mmmeta/0.4.0/mmmeta-0.4.0-py3-none-any.whl

---
+++ packages/mmmeta/mmmeta-0.4.0-py3-none-any.whl/mmmeta/util.py
@@ -0,0 +1,86 @@
+import os
+
+# from datetime import datetime
+from hashlib import sha1
+from pathlib import Path
+
+from banal import clean_dict
+
+BUF_SIZE = 1024 * 1024 * 16
+HASH_LENGTH = 40  # sha1
+
+
+def get_files(directory, condition=lambda x: True):
+    """
+    yield tuples of (filename, path) for files in given `directory`
+    that match `condition` (default: all) incl. subdirectories
+    """
+    return (
+        (os.path.splitext(f)[0], os.path.join(d, f))
+        for d, _, fnames in os.walk(directory)
+        for f in fnames
+        if condition(os.path.join(d, f))
+    )
+
+
+def cast(value):
+    if not isinstance(value, (str, float, int)):
+        return value
+    if isinstance(value, str):
+        value = value.strip()
+    try:
+        if float(value) == int(float(value)):
+            return int(value)
+        return float(value)
+    except (TypeError, ValueError):
+        return value
+        # FIXME
+        # try:
+        #     return datetime.fromisoformat(value)
+        # except ValueError:
+        #     return value
+
+
+def flatten_dict(d):
+    def items():
+        for key, value in d.items():
+            key = key.replace("-", "_")
+            if isinstance(value, dict):
+                for subkey, subvalue in flatten_dict(value).items():
+                    yield key + ":" + subkey, subvalue
+            else:
+                yield key, value
+
+    return dict(items())
+
+
+def robust_dict(d):
+    # no typing for better comparison performance
+    return {k: str(v) for k, v in clean_dict(flatten_dict(d)).items()}
+
+
+def ensure_path(file_path):
+    if file_path is None or isinstance(file_path, Path):
+        return file_path
+    return Path(file_path).resolve()
+
+
+def checksum(file_name):
+    """Generate a hash for a given file name."""
+    file_name = ensure_path(file_name)
+    if file_name is not None and file_name.is_file():
+        digest = sha1()
+        with open(file_name, "rb") as fh:
+            while True:
+                block = fh.read(BUF_SIZE)
+                if not block:
+                    break
+                digest.update(block)
+        return str(digest.hexdigest())
+
+
+def dict_is_subset(dict1, dict2, ignore=set()):
+    """
+    check if dict1 is contained in dict2 (including values)
+    """
+    return len(set(dict1.items()) - set(dict2.items()) - ignore) == 0
