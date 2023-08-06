c07204594025ac557d1bd51f83fc9e9103c074b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:26 +0000

    Add package dialogy/0.7.0/dialogy-0.7.0-py3-none-any.whl

---
+++ packages/dialogy/dialogy-0.7.0-py3-none-any.whl/dialogy/types/entity/__init__.py
@@ -0,0 +1,22 @@
+"""
+Module provides access to entity types.
+
+We are explicitly supporting entities known for Duckling, any new entity support
+will be added depending on open issues.
+"""
+
+from dialogy.types.entity.base_entity import BaseEntity, entity_synthesis
+from dialogy.types.entity.duration_entity import DurationEntity
+from dialogy.types.entity.keyword_entity import KeywordEntity
+from dialogy.types.entity.location_entity import LocationEntity
+from dialogy.types.entity.numerical_entity import NumericalEntity
+from dialogy.types.entity.people_entity import PeopleEntity
+from dialogy.types.entity.time_entity import TimeEntity
+from dialogy.types.entity.time_interval_entity import TimeIntervalEntity
+
+dimension_entity_map = {
+    "number": {"value": NumericalEntity},
+    "people": {"value": PeopleEntity},
+    "time": {"value": TimeEntity, "interval": TimeIntervalEntity},
+    "duration": {"value": DurationEntity},
+}
