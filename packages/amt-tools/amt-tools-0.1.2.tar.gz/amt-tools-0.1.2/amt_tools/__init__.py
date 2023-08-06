eb9c7727ea55faf6513f6f156f55d85540f5a316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:34 +0000

    Add package amt-tools/0.1.2/amt-tools-0.1.2.tar.gz

---
+++ packages/amt-tools/amt-tools-0.1.2.tar.gz/amt-tools-0.1.2/amt_tools/__init__.py
@@ -0,0 +1,28 @@
+"""
+Should be able to use the following import structures (e.g.):
+------------------------------------------------------------
+import amt_tools
+amt_tools.train()
+amt_tools.Estimator()
+amt_tools.Evaluator()
+------------------------------------------------------------
+from amt_tools import train
+train()
+------------------------------------------------------------
+from amt_tools.train import train
+train()
+"""
+
+from .evaluate import *
+from .train import *
+from .transcribe import *
+
+"""
+These are necessary in order to be able to access classes
+and functions in submodules using only 'import amt_tools'
+"""
+
+from . import datasets
+from . import features
+from . import models
+from . import tools
