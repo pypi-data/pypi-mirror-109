8dfbd8e14750b51ddba5f7750fbf55398d64bac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:09 +0000

    Add package alfred3-interact/0.1.9/alfred3_interact-0.1.9-py3-none-any.whl

---
+++ packages/alfred3-interact/alfred3_interact-0.1.9-py3-none-any.whl/alfred3_interact/__init__.py
@@ -0,0 +1,8 @@
+from .page import WaitingPage
+from .page import MatchingPage
+
+from .match import MatchMaker
+
+from .element import Chat
+from .element import ViewMembers
+from .element import ToggleMatchMakerActivation
