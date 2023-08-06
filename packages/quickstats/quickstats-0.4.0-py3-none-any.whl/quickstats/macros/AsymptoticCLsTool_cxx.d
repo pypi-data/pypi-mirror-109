bbe7f910335aab98cb4a64731a6578151bd7594d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:54 +0000

    Add package quickstats/0.4.0/quickstats-0.4.0-py3-none-any.whl

---
+++ packages/quickstats/quickstats-0.4.0-py3-none-any.whl/quickstats/macros/AsymptoticCLsTool_cxx.d
@@ -0,0 +1,23 @@
+
+# DO NOT DELETE
+
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/Repository/quickstats/quickstats/macros/AsymptoticCLsTool.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TMath.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/Rtypes.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/RtypesCore.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/ROOT/RConfig.hxx
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/RVersion.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/RConfigure.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/DllImport.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/snprintf.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/strlcpy.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TGenericClassInfo.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TSchemaHelper.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TIsAProxy.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TVirtualIsAProxy.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TMathBase.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TError.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/Math/ProbFuncMathCore.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/Math/QuantFuncMathCore.h
+quickstats/macros/AsymptoticCLsTool_cxx.so: /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/RVersion.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/RConfig.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TClass.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TDictAttributeMap.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TInterpreter.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TROOT.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TBuffer.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TMemberInspector.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TError.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/RtypesImp.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TIsAProxy.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TFileMergeInfo.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/include/TCollectionProxyInfo.h /afs/cern.ch/work/c/chlcheng/public/local/conda/miniconda/envs/ml-base/bin/rootcling
+AsymptoticCLsTool_cxx__ROOTBUILDVERSION= 6.22/06
