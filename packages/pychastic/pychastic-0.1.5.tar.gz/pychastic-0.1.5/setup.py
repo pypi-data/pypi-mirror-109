3c97bd8de7d02a607c9d3f4debeb539e25c095ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:37 +0000

    Add package pychastic/0.1.5/pychastic-0.1.5.tar.gz

---
+++ packages/pychastic/pychastic-0.1.5.tar.gz/pychastic-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='pychastic',
+      version='0.1.5',
+      description='Solvers for stochastic differential equations',
+      url='https://github.com/RadostW/stochastic',
+      author='Radost Waszkiewicz & Maciej Bartczak',
+      author_email='radost.waszkiewicz@gmail.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',  # This is important!
+      project_urls = {
+          'Documentation': 'https://pychastic.readthedocs.io',
+          'Source': 'https://github.com/RadostW/stochastic'
+      },
+      license='MIT',
+      packages=['pychastic'],
+      zip_safe=False)
