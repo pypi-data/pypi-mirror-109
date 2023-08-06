aef1159ca6a628e758d23035af953929bf2f4cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:19 +0000

    Add package pycohort/2.0/pycohort-2.0.tar.gz

---
+++ packages/pycohort/pycohort-2.0.tar.gz/pycohort-2.0/pycohort/__init__.py
@@ -0,0 +1,13 @@
+def calculate_mean(some_list):
+
+    """Function to calculate the mean of the data set.
+
+    Args: 
+        None
+
+    Returns: 
+        float: mean of the data set
+
+    """
+
+    return (1.0 * sum(some_list) / len(some_list))
---
+++ packages/pycohort/pycohort-2.0.tar.gz/pycohort-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pycohort',
+      version='2.0',
+      author='Yalim Demirkesen',
+      author_linkedin = 'https://www.linkedin.com/in/yalimdemirkesen/',
+      description='Visualizing the customer retention rates',
+      packages=['pycohort'],
+      zip_safe=False)
