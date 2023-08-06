38b24f1944f77e3aca8700f4ee009d3596b1b8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:31 +0000

    Add package bapy/1.0.9/bapy-1.0.9-py2.py3-none-any.whl

---
+++ packages/bapy/bapy-1.0.9-py2.py3-none-any.whl/bapy/scripts/secrets-pull.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+export starting="${BASH_SOURCE[0]}"; debug.sh starting
+[[ ! "${DEBUG-}" ]] || start="$( date +%s )"
+
+if isuserdarwin.sh; then
+  test -n "${GITHUB_SECRETS_PATH}" || { error.sh GITHUB_SECRETS_PATH 'not defined'; exit 1; }
+  cd "${GITHUB_SECRETS_PATH}" || { error.sh "${GITHUB_SECRETS_PATH}" 'not found'; exit 1; }
+  sudo chown -R "${USERNAME}":"$( id -g "${USERNAME}" )" "${USERNAME}/.ssh"
+  gpull.sh
+  cd - > /dev/null || exit 1
+fi
+unset starting
+[[ ! "${DEBUG-}" ]] || { end="$( date +%s )"; RUNTIME=$((end-start)); export RUNTIME; debug.sh RUNTIME; }
+unset start end RUNTIME
---
+++ packages/bapy/bapy-1.0.9-py2.py3-none-any.whl/bapy/scripts/secrets-push.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+export starting="${BASH_SOURCE[0]}"; debug.sh starting
+[[ ! "${DEBUG-}" ]] || start="$( date +%s )"
+
+if isuserdarwin.sh; then
+  test -n "${GITHUB_SECRETS_PATH}" || { error.sh GITHUB_SECRETS_PATH 'not defined'; exit 1; }
+  cd "${GITHUB_SECRETS_PATH}" || { error.sh "${GITHUB_SECRETS_PATH}" 'not found'; exit 1; }
+  sudo chown -R "${USERNAME}":"$( id -g "${USERNAME}" )" "${USERHOME}/.ssh"
+  gall.sh
+  cd - > /dev/null || exit 1
+fi
+
+unset starting
+[[ ! "${DEBUG-}" ]] || { end="$( date +%s )"; RUNTIME=$((end-start)); export RUNTIME; debug.sh RUNTIME; }
+unset start end RUNTIME
