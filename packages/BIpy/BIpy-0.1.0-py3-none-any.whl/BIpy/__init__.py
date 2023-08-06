6c4385f4a20616bf54e0a319946a0a4fd12e0f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:03 +0000

    Add package BIpy/0.1.0/BIpy-0.1.0-py3-none-any.whl

---
+++ packages/BIpy/BIpy-0.1.0-py3-none-any.whl/BIpy/__init__.py
@@ -0,0 +1,28 @@
+"""Python package to help build experiments at the Brain Institute. Currently it's use is to help using the BCI.
+
+Data Processing:
+data_processing.default_instructed_trigmap
+data_processing.default_free_trigmap
+data_processing.fc_c_cp_1through6
+data_processing.organize_xdf
+data_processing.get_sliding_window_partition
+data_processing.lowpass_filter
+data_processing.LowpassWrapper
+
+BCI:
+bci.classifier_process.run_classifier
+bci.classifier_process.ClassifierProcess
+
+bci.inlets.WindowInlet
+bci.inlets.ClassifierInlet
+
+bci.models.DummyClassifier
+bci.models.get_trained_CSP_LDA
+
+
+Other:
+electrode_info.csv
+electrode_info.json
+
+
+"""
