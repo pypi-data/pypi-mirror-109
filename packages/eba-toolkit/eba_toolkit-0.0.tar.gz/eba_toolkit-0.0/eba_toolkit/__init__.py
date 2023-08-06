8e649f7e471146f01b597cb436b5daca44d07abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:19 +0000

    Add package eba-toolkit/0.0/eba_toolkit-0.0.tar.gz

---
+++ packages/eba-toolkit/eba_toolkit-0.0.tar.gz/eba_toolkit-0.0/eba_toolkit/__init__.py
@@ -0,0 +1,6 @@
+from .eba_toolkit import *
+from .ephys import Ephys
+from .stim import Stim
+from .ecap import ECAP
+from .phys import Phys
+from .phys_response import PhysResponse
---
+++ packages/eba-toolkit/eba_toolkit-0.0.tar.gz/eba_toolkit-0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# TODO: find a way to include the neuroshare package from github
+# TODO: update requirements.txt
+version_reqs = ['pandas',
+                'scipy',
+                'matplotlib',
+                'seaborn',
+                'numba',
+                'pytest',
+                'tdt',
+                'mne',
+                'ipywidgets',
+                'cachey',
+                'cached_property',
+                'dask[complete]',
+                'ipympl',
+                'sphinx-rtd-theme',
+                'h5py',
+                'xlrd',
+                'openpyxl']
+
+setup(
+    name="eba_toolkit",
+    version="0.0",
+    author="James Trevathan & Stephan Blanz & Matthew Laluzerne",
+    author_email="james.trevathan@gmail.com, stephan.l.blanz@gmail.com",
+    packages=find_packages(),
+    install_requires=version_reqs,
+    url="https://github.com/ludwig-lab/eba-toolkit",
+    download_url="https://github.com/ludwig-lab/eba-toolkit/archive/refs/tags/v_0.0.tar.gz"
+)
