67a4177620c98408145353252d085fa868981a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:51 +0000

    Add package eea.dexterity.indicators/1.1/eea.dexterity.indicators-1.1.zip

---
+++ packages/eea.dexterity.indicators/eea.dexterity.indicators-1.1.zip/eea.dexterity.indicators-1.1/eea/dexterity/indicators/interfaces.py
@@ -0,0 +1,7 @@
+"""Module where all interfaces, events and exceptions live."""
+
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+
+class IEeaDexterityIndicatorsLayer(IDefaultBrowserLayer):
+    """Marker interface that defines a browser layer."""
