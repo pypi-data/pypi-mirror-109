5c10a3534298bdcb6609ca7b2c61277821bb5f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:34 +0000

    Add package lifeguard-rabbitmq/0.0.4/lifeguard-rabbitmq-0.0.4.tar.gz

---
+++ packages/lifeguard-rabbitmq/lifeguard-rabbitmq-0.0.4.tar.gz/lifeguard-rabbitmq-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lifeguard-rabbitmq",
+    version="0.0.4",
+    url="https://github.com/LifeguardSystem/lifeguard-rabbitmq",
+    author="Diego Rubin",
+    author_email="contact@diegorubin.dev",
+    license="GPL2",
+    scripts=[],
+    include_package_data=True,
+    description="Lifeguard integration with RabbitMQ",
+    install_requires=["lifeguard"],
+    classifiers=["Development Status :: 3 - Alpha"],
+    packages=find_packages(),
+)
