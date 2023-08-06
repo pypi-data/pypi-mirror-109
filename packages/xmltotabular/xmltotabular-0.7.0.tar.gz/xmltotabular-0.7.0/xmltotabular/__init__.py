1c068a3d20b49254f363c80ff88d70c0c95efae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:04 +0000

    Add package xmltotabular/0.7.0/xmltotabular-0.7.0.tar.gz

---
+++ packages/xmltotabular/xmltotabular-0.7.0.tar.gz/xmltotabular-0.7.0/xmltotabular/__init__.py
@@ -0,0 +1,5 @@
+from .xmlcollectiontotabular import XmlCollectionToTabular
+from .xmldoctotabular import XmlDocToTabular
+
+print("USING LOCAL XMLTOTABULAR!!")
+__all__ = [XmlCollectionToTabular, XmlDocToTabular]
