3f59065f4ac5a33c5793ad02117f2e6b9d250402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:25 +0000

    Add package timem/0.0.0/timem-0.0.0.tar.gz

---
+++ packages/timem/timem-0.0.0.tar.gz/timem-0.0.0/setup.py
@@ -0,0 +1,30 @@
+import re
+from setuptools import setup
+
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Topic :: Software Development
+Operating System :: POSIX
+Operating System :: Unix
+"""
+
+
+setup(
+    name="timem",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    description="A module for monitoring memory usage of a python program",
+    author="Tanay Karve",
+    url="https://github.com/tanaykarve/timem",
+    py_modules=["timem"],
+    install_requires=["psutil"],
+    python_requires=">=3.4",
+    classifiers=[_f for _f in CLASSIFIERS.split("\n") if _f],
+    license="BSD",
+)
