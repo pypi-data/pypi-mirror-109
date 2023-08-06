e7897739379bb075b8b4ff4a095b46bfa4ce2f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:01 +0000

    Add package lamberthub/0.1/lamberthub-0.1-py3-none-any.whl

---
+++ packages/lamberthub/lamberthub-0.1-py3-none-any.whl/lamberthub/__init__.py
@@ -0,0 +1,20 @@
+""" A collection of Lambert's problem solvers """
+
+from lamberthub.ecc_solvers.avanzini import avanzini2008
+from lamberthub.universal_solvers.arora import arora2013
+from lamberthub.universal_solvers.gooding import gooding1990
+from lamberthub.universal_solvers.izzo import izzo2015
+
+__version__ = "0.1"
+
+ALL_SOLVERS = [gooding1990, avanzini2008, arora2013, izzo2015]
+""" A list holding all lamberthub available solvers """
+
+ZERO_REV_SOLVERS = [gooding1990, avanzini2008, arora2013, izzo2015]
+""" A list holding all direct-revolution lamberthub solvers """
+
+MULTI_REV_SOLVERS = [gooding1990, izzo2015]
+""" A list holding all multi-revolution lamberthub solvers """
+
+ROBUST_SOLVERS = [gooding1990, izzo2015]
+""" A list holding all robust lamberthub solvers """
