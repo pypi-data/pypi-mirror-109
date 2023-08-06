c508f191c1fbe8a3a3857e0273a618e5ce82c09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:55:48 +0000

    Add package traval/0.1.0/traval-0.1.0-py3-none-any.whl

---
+++ packages/traval/traval-0.1.0-py3-none-any.whl/traval/__init__.py
@@ -0,0 +1,7 @@
+from . import plots, rulelib, ts_utils
+from .binary_classifier import BinaryClassifier
+from .detector import Detector
+from .plots import ComparisonPlots
+from .ruleset import RuleSet
+from .ts_comparison import SeriesComparison, SeriesComparisonRelative
+from .version import __version__
