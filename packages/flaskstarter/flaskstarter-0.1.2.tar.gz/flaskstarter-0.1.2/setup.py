2c6ed57e6220c50762d39b45d163af3a50fa0895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:54 +0000

    Add package flaskstarter/0.1.2/flaskstarter-0.1.2.tar.gz

---
+++ packages/flaskstarter/flaskstarter-0.1.2.tar.gz/flaskstarter-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+setup(
+  name='flaskstarter',
+  packages=['flaskstarter'],
+  version='0.1.2',
+  license='apache-2.0',
+  description='A Flask project start-up CLI to create a modular ready projects.',
+  author='Felipe Bastos Nunes',
+  author_email='felipe.bastosn@gmail.com',
+  url='https://github.com/felipebastos/flaskstart',
+  download_url='https://github.com/felipebastos/flaskstart/archive/refs/tags/v0.1.tar.gz',
+  keywords=['flask', 'cli', 'project'],
+  install_requires=['click',],
+  entry_points={
+        'console_scripts': [
+            'flaskstarter = flaskstarter.flaskstart_cli:init',
+        ],
+    },
+  classifiers=[
+    'Development Status :: 3 - Alpha',      #"3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
