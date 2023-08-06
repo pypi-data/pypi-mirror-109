1e5cf0750dbc7357180af1bc90373e9c148a2aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:46 +0000

    Add package cythontest-1/1.0.0/cythontest_1-1.0.0.tar.gz

---
+++ packages/cythontest-1/cythontest_1-1.0.0.tar.gz/cythontest_1-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cythontest_1',
+      version='1.0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['cythontest_1'],
+      author_email='xreflake@gmail.com',
+      zip_safe=False)
