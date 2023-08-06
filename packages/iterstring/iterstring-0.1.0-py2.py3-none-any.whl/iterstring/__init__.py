7c62dbc2784d60f9c24f5cd5bfd8b3a539ef31aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:55 +0000

    Add package iterstring/0.1.0/iterstring-0.1.0-py2.py3-none-any.whl

---
+++ packages/iterstring/iterstring-0.1.0-py2.py3-none-any.whl/iterstring/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for iterstring."""
+
+__author__ = """Brendan Strejcek"""
+__email__ = 'brendan@datagazing.com'
+__version__ = '0.1.0'
+
+from .iterstring import Istr, Listr, Distr
