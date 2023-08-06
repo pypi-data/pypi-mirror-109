33918d2226c6745f096750f5b0b61bf5b6254174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:59 +0000

    Add package notelink/0.0.5/notelink-0.0.5.tar.gz

---
+++ packages/notelink/notelink-0.0.5.tar.gz/notelink-0.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="notelink",
+    install_requires=[
+        "click>=8.0.1,<9.0.0",
+        "inquirer>=2.7.0,<3.0.0",
+        "configobj>=5.0.6,<6.0.0"
+    ],
+)
