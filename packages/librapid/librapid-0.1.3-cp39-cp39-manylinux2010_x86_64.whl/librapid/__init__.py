8662ed8e175e870195bdf175cad1c2750d62001f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:32 +0000

    Add package librapid/0.1.3/librapid-0.1.3-cp39-cp39-manylinux2010_x86_64.whl

---
+++ packages/librapid/librapid-0.1.3-cp39-cp39-manylinux2010_x86_64.whl/librapid/__init__.py
@@ -0,0 +1,18 @@
+import os
+import platform
+import distutils.sysconfig
+import shutil
+
+from . import progress
+
+try:
+	from librapid_ import *
+except ImportError as error:
+	print("Error while loading librapid core:", error)
+
+	if platform.system() == "Windows":
+		import win32api
+		print("Adding DLL path", os.path.join(distutils.sysconfig.get_python_lib(), "librapid", "blas"))
+		win32api.SetDllDirectory(os.path.join(distutils.sysconfig.get_python_lib(), "librapid", "blas"))
+	
+	from librapid_ import *
