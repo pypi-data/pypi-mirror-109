1f7ba5c6a74126d71af67a847b8666a5b04449f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:57 +0000

    Add package prefect/0.14.22/prefect-0.14.22.tar.gz

---
+++ packages/prefect/prefect-0.14.22.tar.gz/prefect-0.14.22/src/prefect/serialization/__init__.py
@@ -0,0 +1,9 @@
+import prefect.serialization.schedule
+import prefect.serialization.task
+import prefect.serialization.edge
+
+import prefect.serialization.flow
+import prefect.serialization.state
+import prefect.serialization.environment
+import prefect.serialization.storage
+import prefect.serialization.run_config
