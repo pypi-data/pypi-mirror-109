841da332b099089262bbf4f1674e249868183274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:45 +0000

    Add package seamm-dashboard/2021.6.11/seamm-dashboard-2021.6.11.tar.gz

---
+++ packages/seamm-dashboard/seamm-dashboard-2021.6.11.tar.gz/seamm-dashboard-2021.6.11/seamm_dashboard/routes/api/groups.py
@@ -0,0 +1,26 @@
+"""
+API endpoint for groups
+"""
+
+from flask_jwt_extended import jwt_required
+
+from seamm_dashboard import authorize
+from seamm_dashboard.models import Group, GroupSchema
+
+
+@jwt_required(optional=True)
+@authorize.has_role("admin", "group manager")
+def get_groups():
+
+    groups = Group.query.all()
+
+    # usernames = [ [ user.username for user in group.users ] for group in groups ]
+
+    group_schema = GroupSchema(many=True)
+
+    groups = group_schema.dump(groups)
+
+    # for i, group in enumerate(groups):
+    #    group["usernames"] = usernames[i]
+
+    return groups, 200
