89941635430e824665d0086aad184303de94bfe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:59:05 +0000

    Add package aiohttp-sqlalchemy/0.14.1/aiohttp_sqlalchemy-0.14.1-py3-none-any.whl

---
+++ packages/aiohttp-sqlalchemy/aiohttp_sqlalchemy-0.14.1-py3-none-any.whl/aiohttp_sqlalchemy/utils.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from aiohttp_sqlalchemy.constants import SA_DEFAULT_KEY
+
+if TYPE_CHECKING:
+    from aiohttp.web import Application, Request
+    from sqlalchemy import MetaData
+    from sqlalchemy.ext.asyncio import AsyncSession
+    from typing import Any
+
+
+async def sa_init_db(
+    app: 'Application',
+    metadata: 'MetaData',
+    key: str = SA_DEFAULT_KEY,
+) -> None:
+    session_factory = app.get(key)
+    session = session_factory()
+    async with session.bind.begin() as connection:
+        await connection.run_sync(metadata.create_all)
+
+
+def sa_session(request: 'Request', key: str = SA_DEFAULT_KEY) -> 'AsyncSession':
+    return request[key]
