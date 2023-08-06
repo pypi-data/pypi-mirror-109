460feb5e5058a6f9f766f35aa0ee596730f72a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:29 +0000

    Add package ndbd-probability/0.1/ndbd_probability-0.1.tar.gz

---
+++ packages/ndbd-probability/ndbd_probability-0.1.tar.gz/ndbd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ndbd_probability',
+      version='0.1',
+      description='Normal and Binomial distributions',
+      author = 'Nemanja Manojlovic',
+      author_email = 'manojlovic.developer@gmail.com',
+      packages=['ndbd_probability'],
+      zip_safe=False)
