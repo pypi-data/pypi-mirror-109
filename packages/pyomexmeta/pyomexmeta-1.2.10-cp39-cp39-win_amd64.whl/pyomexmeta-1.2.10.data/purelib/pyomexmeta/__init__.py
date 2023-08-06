91baa24d230d811051c513ce49ecba7b659ac042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:03 +0000

    Add package pyomexmeta/1.2.10/pyomexmeta-1.2.10-cp39-cp39-win_amd64.whl

---
+++ packages/pyomexmeta/pyomexmeta-1.2.10-cp39-cp39-win_amd64.whl/pyomexmeta-1.2.10.data/purelib/pyomexmeta/__init__.py
@@ -0,0 +1,25 @@
+from .pyomexmeta import (
+    PersonalInformation,
+    EnergyDiff,
+    PhysicalProcess,
+    _PropertyBearer,
+    PhysicalProperty,
+    RDF,
+    Editor,
+    PhysicalEntity,
+    SingularAnnotation,
+    OmexMetaException
+)
+from .pyomexmeta_api import PyOmexMetaAPI, get_version, eUriType, eXmlType
+
+__version__ = get_version()
+
+
+def run_tests():
+    import os
+    import unittest
+    loader = unittest.TestLoader()
+    start_dir = os.path.abspath(os.path.dirname(__file__))
+    suite = loader.discover(start_dir)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
