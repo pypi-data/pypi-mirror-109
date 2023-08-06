9150420ebc54f2613730f2f711f7ff9bf21eb7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:25 +0000

    Add package Corporal/0.1.1/Corporal-0.1.1.tar.gz

---
+++ packages/Corporal/Corporal-0.1.1.tar.gz/Corporal-0.1.1/corporal/emails.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8; -*-
+"""
+Custom email profiles
+"""
+
+from rattail.mail import Email
+
+# bring in some common config from rattail
+from rattail.emails import (ImporterEmail,
+                            # ProblemReportEmail,
+                            upgrade_failure,
+                            upgrade_success,
+                            user_feedback)
+
+
