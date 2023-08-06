20574255e0250c3a3289e1443c11397a9b54280e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:20 +0000

    Add package mon-premier-packageNT0102/0.0.1/mon_premier_packageNT0102-0.0.1-py3-none-any.whl

---
+++ packages/mon-premier-packageNT0102/mon_premier_packageNT0102-0.0.1-py3-none-any.whl/mon_test/mon_test_script.py
@@ -0,0 +1,17 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+def mon_script_methode():
+    print("Hello c'est moi le test, je fonctionne bien.")
+
+def favorite_meal():
+    print("la Pizza biensur")
+
+def voiture_de_reve():
+    print("Ford")
+ 
+
+
+if __name__ == '__main__':
+    voiture_de_reve()
