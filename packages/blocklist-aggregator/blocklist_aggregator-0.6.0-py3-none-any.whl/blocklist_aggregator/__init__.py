861c98f4ba3b388e942f292b2e10378bb982a1ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:37 +0000

    Add package blocklist-aggregator/0.6.0/blocklist_aggregator-0.6.0-py3-none-any.whl

---
+++ packages/blocklist-aggregator/blocklist_aggregator-0.6.0-py3-none-any.whl/blocklist_aggregator/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.6.0"
+
+from blocklist_aggregator.aggregator import fetch
+from blocklist_aggregator.aggregator import save_raw
+from blocklist_aggregator.aggregator import save_hosts
