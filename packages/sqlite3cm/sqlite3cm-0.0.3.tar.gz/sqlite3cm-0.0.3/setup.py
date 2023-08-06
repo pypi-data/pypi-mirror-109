b4cf026474f016a5f1b28cc59aa9fbac1b785124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:56 +0000

    Add package sqlite3cm/0.0.3/sqlite3cm-0.0.3.tar.gz

---
+++ packages/sqlite3cm/sqlite3cm-0.0.3.tar.gz/sqlite3cm-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+import setuptools
+
+CURR_DIR = pathlib.Path(__file__).parent.resolve()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Context manager for Sqlite databases'
+LONG_DESCRIPTION = (CURR_DIR / "README.MD").read_text(encoding='UTF-8')
+
+# Setting up
+setuptools.setup(
+    name="sqlite3cm",
+    version=VERSION,
+    author="Ahmed LAHRIZI",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/ahmedlahrizi/sqlite3_CM",
+    keywords=['python', 'sql', "sqlite", "3", "cm", "context", "manage", "database", "db"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
