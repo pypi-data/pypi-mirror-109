2b53fa0684149c38322f216e87cf5c073cf91169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:57:34 +0000

    Add package validit/1.0.0/validit-1.0.0.tar.gz

---
+++ packages/validit/validit-1.0.0.tar.gz/validit-1.0.0/validit/exceptions.py
@@ -0,0 +1,12 @@
+class ValidItError(Exception):
+    """ The base exception for this module. """
+
+
+class MissingExtras(ValidItError):
+    """ Raised when trying to use extra features without installing the
+    required packages (for example, trying to load YAML or TOML files without
+    installing required extra packages). """
+
+
+class InvalidTemplateConfiguration(ValidItError):
+    """ Raised when a template configuration is not valid. """
