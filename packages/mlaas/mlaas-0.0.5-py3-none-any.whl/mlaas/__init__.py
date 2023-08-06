187976912934488f80696a6c40619897b8d5a349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:43 +0000

    Add package mlaas/0.0.5/mlaas-0.0.5-py3-none-any.whl

---
+++ packages/mlaas/mlaas-0.0.5-py3-none-any.whl/mlaas/__init__.py
@@ -0,0 +1,4 @@
+from pkg.services.image_background_removal.end_point import background_removal 
+from pkg.services.speech2text.end_point import speech2text 
+from pkg.services.text2speech.end_point import text2speech 
+from pkg.services.translation.end_point import translation 
