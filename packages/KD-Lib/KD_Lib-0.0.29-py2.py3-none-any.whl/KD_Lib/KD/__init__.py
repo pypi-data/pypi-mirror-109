deb02b662247373de2e514dad9143156f3643a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:40 +0000

    Add package KD-Lib/0.0.29/KD_Lib-0.0.29-py2.py3-none-any.whl

---
+++ packages/KD-Lib/KD_Lib-0.0.29-py2.py3-none-any.whl/KD_Lib/KD/__init__.py
@@ -0,0 +1,20 @@
+from .vision import (
+    VanillaKD,
+    VirtualTeacher,
+    SelfTraining,
+    TAKD,
+    RKDLoss,
+    RCO,
+    NoisyTeacher,
+    SoftRandom,
+    MessyCollab,
+    MeanTeacher,
+    LabelSmoothReg,
+    ProbShift,
+    DML,
+    BANN,
+    CSKD,
+    Attention,
+)
+
+from .text import BERT2LSTM, get_essentials
