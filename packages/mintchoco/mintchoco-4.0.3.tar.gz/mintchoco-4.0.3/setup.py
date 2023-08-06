63f4090b60eac4b88ae93732bd36965e4a996613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:38 +0000

    Add package mintchoco/4.0.3/mintchoco-4.0.3.tar.gz

---
+++ packages/mintchoco/mintchoco-4.0.3.tar.gz/mintchoco-4.0.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools  # type: ignore
+from setuptools import setup  # type: ignore
+
+requirements = []
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="mintchoco",
+    author="Ryu ju heon",
+    author_email="SaidBySolo@gmail.com",
+    url="https://github.com/SaidBySolo/Mintchoco",
+    version="4.0.3",
+    long_description=open("README.md", "rt", encoding="UTF8").read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),  # type: ignore
+    description="Heliotrope python wrapper",
+    install_requires=requirements,
+    python_requires=">=3.6",
+)
