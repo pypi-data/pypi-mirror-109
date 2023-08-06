8a35150a4bcedbc9af8f9adab2de0d5df10c7fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:39 +0000

    Add package MediaPipeTools/1.1.2/MediaPipeTools-1.1.2.tar.gz

---
+++ packages/MediaPipeTools/MediaPipeTools-1.1.2.tar.gz/MediaPipeTools-1.1.2/MediaPipeTools/__init__.py
@@ -0,0 +1,4 @@
+from MediaPipeTools.TrackHandModule import HandTrack
+from MediaPipeTools.MeshFaceModule import MeshFace
+from MediaPipeTools.PoseTrackModule import PoseTrack
+from MediaPipeTools.VirtualBackground import VirtualBackground
