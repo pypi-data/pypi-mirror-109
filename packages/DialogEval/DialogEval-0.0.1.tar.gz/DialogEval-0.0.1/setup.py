711e1acbce1462fd796682579e3f690352dc3535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:43 +0000

    Add package DialogEval/0.0.1/DialogEval-0.0.1.tar.gz

---
+++ packages/DialogEval/DialogEval-0.0.1.tar.gz/DialogEval-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+# with open("requirements.txt") as f:
+#     required_packages = f.read().splitlines()
+
+setup(
+    name="DialogEval",
+    version="0.0.1",
+    author="Qingyang Wu",
+    author_email="wilwu@ucdavis.edu",
+    description="Fast Dialog Evaluation Kit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qywu/DialEval",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
