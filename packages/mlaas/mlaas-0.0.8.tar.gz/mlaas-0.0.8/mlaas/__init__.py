4ed0e4bc4eb55743e4abb671f5fabd2506b72056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:52 +0000

    Add package mlaas/0.0.8/mlaas-0.0.8.tar.gz

---
+++ packages/mlaas/mlaas-0.0.8.tar.gz/mlaas-0.0.8/mlaas/__init__.py
@@ -0,0 +1,28 @@
+from types import ModuleType
+from importlib import import_module
+
+import mlaas.services
+
+
+class LazyLoader(ModuleType):
+    def __init__(self, local_name, parent_module_globals, name):
+        self._local_name = local_name
+        self._parent_module_globals = parent_module_globals
+        super(LazyLoader, self).__init__(name)
+
+    def _load(self):
+        module = import_module(self.__name__)
+        self._parent_module_globals[self._local_name] = module
+        self.__dict__.update(module.__dict__)
+        return module
+
+    def __getattr__(self, item):
+        module = self._load()
+        return getattr(module, item)
+
+    def __dir__(self):
+        module = self._load()
+        return dir(module)
+
+
+services = LazyLoader('services', globals(), 'services.datasets')
