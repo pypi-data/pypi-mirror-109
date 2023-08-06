ff887afa9cc17a5e38eaa3c4faa7f9db12b5534c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:26 +0000

    Add package EmbedSeg/0.2.3/EmbedSeg-0.2.3.tar.gz

---
+++ packages/EmbedSeg/EmbedSeg-0.2.3.tar.gz/EmbedSeg-0.2.3/setup.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="EmbedSeg", 
+    version="0.2.3",
+    author="Manan Lalit, Pavel Tomancak, Florian Jug",
+    author_email="lalit@mpi-cbg.de",
+    description="EmbedSeg provides automatic detection and segmentation of objects in microscopy images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/juglab/EmbedSeg/",
+    project_urls={
+        "Bug Tracker": "https://github.com/juglab/EmbedSeg/issues",
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+          'matplotlib',
+          'numpy',
+          'scipy',
+          "tifffile==2021.4.8",
+          "numba",
+          "tqdm",
+          "jupyter",
+          "pandas",
+          "seaborn",
+          "scikit-image",
+          "colorspacious",
+          "itkwidgets",
+          "pytest"
+        ]
+)
+
