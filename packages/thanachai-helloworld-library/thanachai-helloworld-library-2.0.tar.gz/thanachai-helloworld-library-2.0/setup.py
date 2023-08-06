41cf9d686329e1fcf6d1612e6f5e31be0bb2d729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:01 +0000

    Add package thanachai-helloworld-library/2.0/thanachai-helloworld-library-2.0.tar.gz

---
+++ packages/thanachai-helloworld-library/thanachai-helloworld-library-2.0.tar.gz/thanachai-helloworld-library-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="thanachai-helloworld-library",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/lionants02/demo-helloworld-library',
+    author='Thanachai',
+    author_email='singmax161@gmail.com',
+)
