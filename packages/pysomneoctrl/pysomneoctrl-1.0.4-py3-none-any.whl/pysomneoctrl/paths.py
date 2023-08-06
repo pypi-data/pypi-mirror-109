c230a8830161a2f227d1858295090b23ea3bd729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:18 +0000

    Add package pysomneoctrl/1.0.4/pysomneoctrl-1.0.4-py3-none-any.whl

---
+++ packages/pysomneoctrl/pysomneoctrl-1.0.4-py3-none-any.whl/pysomneoctrl/paths.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: @pijiulaoshi
+"""
+
+# LOCATIONS
+
+# ALARMS
+ALARMS = "wualm"
+SET_ALARMS = f"{ALARMS}/prfwu"
+STATUS = f"{ALARMS}/aenvs"
+TIMES = f"{ALARMS}/aalms"
+
+# WIND DOWN
+WURLX = "wurlx"
+WUDSK = "wudsk"
+
+# AUDIO
+WUPLY = "wuply"
+WUFMR = "wufmr"
+WUFMP = "wufmp"
+FM_PRES = f"{WUFMP}/00"
+
+# LIGHT
+WULGT = "wulgt"
+# SENSORS
+WUSRD = "wusrd"
+# SETTINGS
+WUSTS = "wusts"
+# BEDTIME
+WUNGT = "wungt"
+
+
