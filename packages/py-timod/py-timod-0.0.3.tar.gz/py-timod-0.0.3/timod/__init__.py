93ef8ede2f8a5eb5650a91841348c2dd3c006c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:53 +0000

    Add package py-timod/0.0.3/py-timod-0.0.3.tar.gz

---
+++ packages/py-timod/py-timod-0.0.3.tar.gz/py-timod-0.0.3/timod/__init__.py
@@ -0,0 +1,25 @@
+from .ex import (
+    CustomError,
+    CancelledError,
+    OperationError,
+    NumArgumentsError,
+    TypeError,
+    ValueError,
+    OverflowError,
+    ZeroDivisionError,
+    MaxQuotaError,
+    AuthError,
+    ForbiddenError,
+    LookupError,
+    BadDataError,
+    SyntaxError,
+    NodeError,
+    AssertionError,
+)
+from .tihandler import TiHandler
+try:
+    from .util import start_module
+except ImportError:
+    pass  # importing msgpack might fail when importing from setup.py
+
+__version__ = "0.0.3"
