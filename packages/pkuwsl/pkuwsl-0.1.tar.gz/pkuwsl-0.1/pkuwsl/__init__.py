f2039a082ee3bf4453edc19ebe5687975f39a9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:55:59 +0000

    Add package pkuwsl/0.1/pkuwsl-0.1.tar.gz

---
+++ packages/pkuwsl/pkuwsl-0.1.tar.gz/pkuwsl-0.1/pkuwsl/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 15:50:02 2021
+
+Who would be so cruel to someone like you?
+No one but you
+Who would make the rules for the things that you do?
+No one but you
+I woke up this morning, looked at the sky
+I thought of all of the time passing by
+It didn't matter how hard we tried
+'Cause in the end
+
+@author: KING
+"""
+
+def jujube():
+    print ('吃枣')
+ 
+
+def pill():
+    print ('吃枣2')
