fc9ac3a462af684e5c8f39cb26601c9dfc32366e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:18 +0000

    Add package pyoauthbridge/1.0.19/pyoauthbridge-1.0.19.tar.gz

---
+++ packages/pyoauthbridge/pyoauthbridge-1.0.19.tar.gz/pyoauthbridge-1.0.19/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(
+   name='pyoauthbridge',
+   version='1.0.19',
+   description='pyoauthbridge is a official python library to communicate with Tradelab API',
+   long_description_content_type='text/x-rst',
+   long_description='A project by tradelab',
+   author='Biswajit',
+   author_email='biswajit@tradelab.com',
+   packages=["pyoauthbridge"]
+)
