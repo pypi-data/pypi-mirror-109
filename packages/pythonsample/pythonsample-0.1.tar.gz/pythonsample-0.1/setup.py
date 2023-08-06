0c237b32118420fa41530f8902864325b5fb0dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:21 +0000

    Add package pythonsample/0.1/pythonsample-0.1.tar.gz

---
+++ packages/pythonsample/pythonsample-0.1.tar.gz/pythonsample-0.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='pythonsample',
+    version='0.1', 
+    description = 'samplpe python',
+    author = 'Neethi',             
+    author_email = 'vinoth.royal6@gmail.com')
