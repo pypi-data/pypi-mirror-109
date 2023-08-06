5d23beaaf3182f8af72d67f88d1db985e08623f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:25 +0000

    Add package test-pipeline-pybuilder/0.0.1/test-pipeline-pybuilder-0.0.1.tar.gz

---
+++ packages/test-pipeline-pybuilder/test-pipeline-pybuilder-0.0.1.tar.gz/test-pipeline-pybuilder-0.0.1/setup.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from distutils.core import setup
+
+setup(name='test-pipeline-pybuilder',
+      version='0.0.1',
+      # py_modules='main',
+      author='Shaoyu Yang',
+      author_email='s.yang@pipelinesecurity.net',
+      url='https://github.com/pipelinelabo/pybuilder-investigation',
+      description='This is just a test for uploading')
