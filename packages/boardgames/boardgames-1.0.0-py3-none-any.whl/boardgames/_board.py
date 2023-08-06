a6bd597eff8add23662423e10570d19c0f8e31ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:39 +0000

    Add package boardgames/1.0.0/boardgames-1.0.0-py3-none-any.whl

---
+++ packages/boardgames/boardgames-1.0.0-py3-none-any.whl/boardgames/_board.py
@@ -0,0 +1,57 @@
+from typing import Generic, TypeVar, Union, overload
+
+from ._cell import Cell
+
+CT = TypeVar("CT", bound="Cell")
+
+
+__all__ = ("Board",)
+
+
+class Board(Generic[CT]):
+    def __init__(self, cell_type: type[CT], num_rows: int, num_cols: int) -> None:
+        self.num_rows: int = num_rows
+        self.num_cols: int = num_cols
+
+        self.grid: list[list[CT]] = [[] for _ in range(num_rows)]
+
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                self.grid[row].append(cell_type(self, row, col))
+
+    @overload
+    def __getitem__(self, index: tuple[slice, slice]) -> list[list[CT]]:
+        ...
+
+    @overload
+    def __getitem__(self, index: tuple[slice, int]) -> list[CT]:
+        ...
+
+    @overload
+    def __getitem__(self, index: tuple[int, slice]) -> list[CT]:
+        ...
+
+    @overload
+    def __getitem__(self, index: tuple[int, int]) -> CT:
+        ...
+
+    def __getitem__(self, index: tuple[Union[int, slice], Union[int, slice]]) -> Union[CT, list[CT], list[list[CT]]]:
+        row, col = index
+
+        if isinstance(row, slice):
+            rows = self.grid[row]
+            return [row[col] for row in rows]  # type: ignore
+
+        return self.grid[row][col]  # type: ignore
+
+    @property
+    def rows(self) -> list[list[CT]]:
+        return [self[row, :] for row in range(self.num_rows)]
+
+    @property
+    def cols(self) -> list[list[CT]]:
+        return [self[:, col] for col in range(self.num_cols)]
+
+    @property
+    def cells(self) -> list[CT]:
+        return [cell for row in self.grid for cell in row]
---
+++ packages/boardgames/boardgames-1.0.0-py3-none-any.whl/boardgames/_cell.py
@@ -0,0 +1,14 @@
+from typing import Any, TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from ._board import Board
+
+__all__ = ("Cell",)
+
+
+class Cell:
+    def __init__(self, board: Board[Any], row: int, col: int) -> None:
+        self.board: Board[Any] = board
+        self.row: int = row
+        self.col: int = col
