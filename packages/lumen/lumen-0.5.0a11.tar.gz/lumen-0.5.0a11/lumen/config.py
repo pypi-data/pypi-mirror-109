4923062877a8a89a95b48f49757f485ed59fd440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:52 +0000

    Add package lumen/0.5.0a11/lumen-0.5.0a11.tar.gz

---
+++ packages/lumen/lumen-0.5.0a11.tar.gz/lumen-0.5.0a11/lumen/config.py
@@ -0,0 +1,85 @@
+import importlib
+import os
+import sys
+
+import param as param
+import panel as pn
+
+from panel.widgets.indicators import Indicator
+from panel.template.base import BasicTemplate
+from panel.template import DefaultTheme, DarkTheme
+
+
+_INDICATORS = {k.lower(): v for k, v in param.concrete_descendents(Indicator).items()}
+
+_LAYOUTS = {
+    'accordion': pn.Accordion,
+    'column'   : pn.Column,
+    'grid'     : pn.GridBox,
+    'row'      : pn.Row,
+    'tabs'     : pn.Tabs
+}
+
+try:
+    _DEFAULT_LAYOUT = _LAYOUTS['flex'] = pn.FlexBox
+except Exception:
+    _DEFAULT_LAYOUT = pn.GridBox
+
+_TEMPLATES = {
+    k[:-8].lower(): v for k, v in param.concrete_descendents(BasicTemplate).items()
+}
+
+_THEMES = {'default': DefaultTheme, 'dark': DarkTheme}
+
+
+
+class _config(param.Parameterized):
+    """
+    Stores shared configuration for the entire Lumen application.
+    """
+
+    _root = param.String(default=None, doc="""
+      Root directory relative to which data and modules are loaded.""")
+
+    template_vars = param.Dict(default={}, doc="""
+      Template variables which may be referenced in a dashboard yaml
+      specification.""")
+
+    yamls = param.List(default=[], doc="""
+      List of yaml files currently being served.""")
+
+    @property
+    def root(self):
+        if self._root:
+            return self._root
+        return os.getcwd()
+
+    def load_local_modules(self):
+        """
+        Loads local modules containing custom components and templates.
+        """
+        modules = {}
+        for imp in ('filters', 'sources', 'transforms', 'template', 'views'):
+            path = os.path.join(self.root, imp+'.py')
+            if not os.path.isfile(path):
+                continue
+            spec = importlib.util.spec_from_file_location(f"local_lumen.{imp}", path)
+            modules[imp] = module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+        templates = param.concrete_descendents(BasicTemplate)
+        _TEMPLATES.update({
+            k[:-8].lower(): v for k, v in templates.items()
+        })
+        return modules
+
+    @property
+    def dev(self):
+        """
+        Whether the application was launched in development mode.
+        """
+        return '--dev' in sys.argv or '--autoreload' in sys.argv
+
+
+
+
+config = _config()
---
+++ packages/lumen/lumen-0.5.0a11.tar.gz/lumen-0.5.0a11/lumen/tests/test_dashboard.py
@@ -0,0 +1,12 @@
+import pathlib
+
+from lumen.dashboard import Dashboard
+from lumen.views import View
+
+def test_dashboard_with_local_view(set_root):
+    root = pathlib.Path(__file__).parent / 'sample_dashboard'
+    set_root(str(root))
+    dashboard = Dashboard(str(root / 'dashboard.yml'))
+    target = dashboard.targets[0]
+    view = View.from_spec(target.views[0], target.source, [])
+    assert isinstance(view, dashboard._modules['views'].TestView)
