418f07d1d2af7fb255f43f850be5e5e73dac4c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:21 +0000

    Add package BlaguesApi/1.0/BlaguesApi-1.0.tar.gz

---
+++ packages/BlaguesApi/BlaguesApi-1.0.tar.gz/BlaguesApi-1.0/BlaguesApi/__init__.py
@@ -0,0 +1,3 @@
+from BlaguesApi.Constants import JokeTypes, Types, logo
+from BlaguesApi.Jokes import Jokes
+from BlaguesApi.JokesAround import JokesAround
---
+++ packages/BlaguesApi/BlaguesApi-1.0.tar.gz/BlaguesApi-1.0/setup.py
@@ -0,0 +1,43 @@
+import pathlib
+
+from setuptools import setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='BlaguesApi',
+    packages=['BlaguesApi'],
+    version='1.0',
+    license='MIT',
+    description='Python API wrapper for BlaguesAPI.fr',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='coco875',
+    author_email='pereira.jannin@gmail.com',
+    url='https://github.com/coco875/BlagueAPI',
+    download_url='https://github.com/coco875/BlagueAPI/archive/refs/tags/v_09.tar.gz',
+    keywords=['API', 'Joke', 'French'],
+    install_requires=[
+        'jaro-winkler',
+        'requests',
+    ],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 4 - Beta',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    python_requires=">=3.6",
+)
