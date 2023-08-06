07b1910fa0f1836394c0b745cd127b06b278ec5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:56 +0000

    Add package novelWriter/1.4b1/novelWriter-1.4b1-py3-none-any.whl

---
+++ packages/novelWriter/novelWriter-1.4b1-py3-none-any.whl/nw/gui/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from nw.gui.doceditor import GuiDocEditor
+from nw.gui.docviewer import GuiDocViewer, GuiDocViewDetails
+from nw.gui.itemdetails import GuiItemDetails
+from nw.gui.mainmenu import GuiMainMenu
+from nw.gui.noveltree import GuiNovelTree
+from nw.gui.outline import GuiOutline
+from nw.gui.outlinedetails import GuiOutlineDetails
+from nw.gui.projdetails import GuiProjectDetails
+from nw.gui.projtree import GuiProjectTree
+from nw.gui.statusbar import GuiMainStatus
+from nw.gui.theme import GuiTheme
+
+__all__ = [
+    "GuiDocEditor",
+    "GuiDocViewDetails",
+    "GuiDocViewer",
+    "GuiItemDetails",
+    "GuiMainMenu",
+    "GuiMainStatus",
+    "GuiNovelTree",
+    "GuiOutline",
+    "GuiOutlineDetails",
+    "GuiProjectDetails",
+    "GuiProjectTree",
+    "GuiTheme",
+]
