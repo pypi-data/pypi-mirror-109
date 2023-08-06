1f28167185bf7b9e23d40a4bf6e461d757b85139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:31 +0000

    Add package jina/2.0.0rc5.dev81/jina-2.0.0rc5.dev81.tar.gz

---
+++ packages/jina/jina-2.0.0rc5.dev81.tar.gz/jina-2.0.0rc5.dev81/jina/parsers/client.py
@@ -0,0 +1,51 @@
+"""Module for argparse for Client"""
+from .helper import add_arg_group
+
+
+def mixin_client_cli_parser(parser):
+    """Add the arguments for the client to the parser
+
+    :param parser: the parser configure
+    """
+    gp = add_arg_group(parser, title='Client')
+
+    gp.add_argument(
+        '--request-size',
+        type=int,
+        default=100,
+        help='The number of Documents in each Request.',
+    )
+
+    gp.add_argument(
+        '--continue-on-error',
+        action='store_true',
+        default=False,
+        help='If set, a Request that causes error will be logged only without blocking the further '
+        'requests.',
+    )
+
+    gp.add_argument(
+        '--show-progress',
+        action='store_true',
+        default=False,
+        help='If set, client will show a progress bar on receiving every request.',
+    )
+
+    gp.add_argument(
+        '--return-results',
+        action='store_true',
+        default=False,
+        help='''
+This feature is only used for AsyncClient.
+
+If set, the results of all Requests will be returned as a list. This is useful when one wants 
+process Responses in bulk instead of using callback. 
+                    ''',
+    )
+
+    gp.add_argument(
+        '--restful',
+        action='store_true',
+        default=False,
+        help='If set, use RESTful interface instead of gRPC as the main interface',
+    )
