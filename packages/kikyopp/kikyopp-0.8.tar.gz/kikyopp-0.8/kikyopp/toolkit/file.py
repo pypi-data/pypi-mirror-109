33e0c15fb8570da3f7c07340b01538388b3febc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:22 +0000

    Add package kikyopp/0.8/kikyopp-0.8.tar.gz

---
+++ packages/kikyopp/kikyopp-0.8.tar.gz/kikyopp-0.8/kikyopp/toolkit/file.py
@@ -0,0 +1,19 @@
+import io
+from typing import List
+
+import fitz
+from fitz import Pixmap
+
+__all__ = [
+    'pdf_to_png',
+]
+
+
+def pdf_to_png(data: bytes) -> List[bytes]:
+    result = []
+    pdf = fitz.Document(stream=io.BytesIO(data), filetype='pdf')
+    for i, page in enumerate(pdf):
+        # 将每一页pdf读取为图片
+        img: Pixmap = page.getPixmap()
+        result.append(img.getImageData())
+    return result
