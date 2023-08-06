ce0ed7b3f054ed4dff9e0ee87afb70dbd881bde5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:58 +0000

    Add package pysatSeasons/0.1.3/pysatSeasons-0.1.3.tar.gz

---
+++ packages/pysatSeasons/pysatSeasons-0.1.3.tar.gz/pysatSeasons-0.1.3/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019, RS
+# Full license can be found in License.md
+# -----------------------------------------------------------------------------
+
+from setuptools import setup
+
+
+setup()
