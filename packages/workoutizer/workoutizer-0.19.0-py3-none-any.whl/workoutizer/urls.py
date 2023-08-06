6f6d203a48ffbf3b47dd898c7655b93670b6ead7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:08 +0000

    Add package workoutizer/0.19.0/workoutizer-0.19.0-py3-none-any.whl

---
+++ packages/workoutizer/workoutizer-0.19.0-py3-none-any.whl/workoutizer/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+# custom error handler
+handler404 = "wkz.views.custom_400_view"
+handler500 = "wkz.views.custom_500_view"
+handler403 = "wkz.views.custom_400_view"
+handler400 = "wkz.views.custom_400_view"
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("wkz.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
