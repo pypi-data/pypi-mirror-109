6c9c8f8533d4112219e97270bbf32485ccd5e5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:04:32 +0000

    Add package Inbreeding/0.1.1.3.post5/Inbreeding-0.1.1.3.post5-py3-none-any.whl

---
+++ packages/Inbreeding/Inbreeding-0.1.1.3.post5-py3-none-any.whl/inbreeding/__init__.py
@@ -0,0 +1,14 @@
+from .Inbreeding import Inbreeding
+from .MissingYOB import MissingYOB
+from .RelationAnimals import RelationAnimals
+
+"""
+The advice for this format in the __init__.py file was given in the following 
+thread: 
+https://stackoverflow.com/questions/45122/python-packages-import-by-class-not-file/45126#45126
+And even though it is focussed on Python2, it still seems legit in Python3
+"""
+
+__version__ = "0.0.1" 
+__author__ = "Thomas Rahimi"
+__credits__ = "FG Tierzucht, Universität Kassel"
