78c43053e6ed76f692aa9f3577d3a1e3e6601320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:18 +0000

    Add package libratom/0.5.3/libratom-0.5.3-py3-none-any.whl

---
+++ packages/libratom/libratom-0.5.3-py3-none-any.whl/libratom/scripts/download_edrm_zipped_files.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# pylint: disable=missing-docstring
+
+import logging
+from pathlib import Path
+
+import click
+import click_log
+
+from libratom.lib.download import download_files
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+# From conftest.py
+ENRON_DATASET_URL = "https://www.ibiblio.org/enron/RevisedEDRMv1_Complete"
+CACHED_ENRON_DATA_DIR = Path("/tmp/libratom/test_data/RevisedEDRMv1_Complete")
+
+# Set configuration on the root logger
+click_log.basic_config(logging.getLogger())
+
+
+def set_log_level_from_verbose(ctx, param, value):
+    if value > 1:
+        level = logging.DEBUG
+    elif value > 0:
+        level = logging.INFO
+    else:
+        # Default
+        level = logging.WARNING
+    logging.getLogger().setLevel(level)
+    return level
+
+
+@click.command(
+    context_settings=CONTEXT_SETTINGS,
+    help=f"Download edrm files into {CACHED_ENRON_DATA_DIR}/",
+)
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    callback=set_log_level_from_verbose,
+    help="Increase verbosity (can be repeated).",
+    expose_value=False,
+)
+def download_edrm_zipped_files() -> None:
+    """Download edrm files into CACHED_ENRON_DATA_DIR."""
+
+    names = [
+        "albert_meyers",
+        "andrea_ring",
+        "andrew_lewis",
+        "andy_zipper",
+        "chris_dorland",
+        "jason_wolfe",
+        "vkaminski",
+    ]
+
+    # CSV files to download
+    urls = [f"{ENRON_DATASET_URL}/{name}.zip" for name in names]
+
+    download_files(urls, CACHED_ENRON_DATA_DIR, dry_run=False)
+
+
+if __name__ == "__main__":
+    download_edrm_zipped_files()
