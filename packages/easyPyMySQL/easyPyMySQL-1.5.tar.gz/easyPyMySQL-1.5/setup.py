0f0aff3621ad49b522ba50e2a25fe1af72dba1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:52 +0000

    Add package easyPyMySQL/1.5/easyPyMySQL-1.5.tar.gz

---
+++ packages/easyPyMySQL/easyPyMySQL-1.5.tar.gz/easyPyMySQL-1.5/setup.py
@@ -0,0 +1,12 @@
+﻿from distutils.core import setup
+
+setup(
+      name="easyPyMySQL",
+      version="1.5",
+      description="简单易用的数据库ORM模块",
+      url='https://github.com/AkiYama-Ryou/easyPyMySQL',
+      author="ChenRuohan",
+      author_email='crh51306@gmail.com',
+      packages= ['easyPyMySQL'],
+
+      )
