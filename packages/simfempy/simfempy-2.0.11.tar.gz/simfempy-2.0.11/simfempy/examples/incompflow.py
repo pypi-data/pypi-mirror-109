be502387e3cda92f72436d287c2da0bed50ca9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:04 +0000

    Add package simfempy/2.0.11/simfempy-2.0.11.tar.gz

---
+++ packages/simfempy/simfempy-2.0.11.tar.gz/simfempy-2.0.11/simfempy/examples/incompflow.py
@@ -0,0 +1,126 @@
+assert __name__ == '__main__'
+# in shell
+import os, sys
+simfempypath = os.path.abspath(os.path.join(__file__, os.path.pardir, os.path.pardir, os.path.pardir, os.path.pardir,'simfempy'))
+sys.path.insert(0,simfempypath)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import pygmsh
+from simfempy.applications.stokes import Stokes
+from simfempy.applications.navierstokes import NavierStokes
+from simfempy.applications.problemdata import ProblemData
+from simfempy.meshes.simplexmesh import SimplexMesh
+from simfempy.meshes import plotmesh
+
+# ================================================================c#
+def main(testcase='drivenCavity'):
+    testcases = ['drivenCavity', 'backwardFacingStep', 'poiseuille']
+    # create mesh and data
+    if testcase=='drivenCavity':
+        mesh, data = drivenCavity(h=0.2, mu=0.00025)
+    elif testcase=='backwardFacingStep':
+        mesh, data = backwardFacingStep(h=0.1)
+    elif testcase=='poiseuille':
+        mesh, data = poiseuille(h=0.1)
+    else:
+        raise ValueError(f"test case must be in {testcases=}")
+   # plotmesh.meshWithBoundaries(mesh)
+    # create application
+    # stokes = Stokes(mesh=mesh, problemdata=data, linearsolver='iter_gmres_10')
+    # stokes = Stokes(mesh=mesh, problemdata=data, linearsolver='umf')
+    # stokes = NavierStokes(mesh=mesh, problemdata=data, linearsolver='iter_gmres')
+    # stokes = NavierStokes(mesh=mesh, problemdata=data, linearsolver='iter_gcrotmk')
+    stokes = NavierStokes(mesh=mesh, problemdata=data, linearsolver='umf')
+    result = stokes.solve()
+    print(f"{result.info['timer']}")
+    print(f"postproc:")
+    for p, v in result.data['global'].items(): print(f"{p}: {v}")
+    fig = plt.figure(figsize=(10, 8))
+    outer = gridspec.GridSpec(1, 3, wspace=0.2, hspace=0.2)
+    plotmesh.meshWithBoundaries(mesh, fig=fig, outer=outer[0])
+    plotmesh.meshWithData(mesh, data=result.data, title="Stokes", fig=fig, outer=outer[1])
+    plotmesh.meshWithData(mesh, title="Stokes", fig=fig, outer=outer[2],
+                          quiver_data={"V":list(result.data['point'].values())})
+    plt.show()
+
+
+# ================================================================c#
+def drivenCavity(h=0.1, mu=0.001):
+    with pygmsh.geo.Geometry() as geom:
+        ms = [h*v for v in [1.,1.,0.2,0.2]]
+        p = geom.add_rectangle(xmin=0, xmax=1, ymin=0, ymax=1, z=0, mesh_size=ms)
+        geom.add_physical(p.surface, label="100")
+        for i in range(len(p.lines)): geom.add_physical(p.lines[i], label=f"{1000 + i}")
+        mesh = geom.generate_mesh()
+    data = ProblemData()
+    # boundary conditions
+    # data.bdrycond.set("Dirichlet", [1000, 1001, 1002, 1003])
+    data.bdrycond.set("Dirichlet", [1001, 1002, 1003])
+    data.bdrycond.set("Navier", [1000])
+    data.bdrycond.fct[1002] = [lambda x, y, z: 1, lambda x, y, z: 0]
+    # parameters
+    data.params.scal_glob["mu"] = mu
+    #TODO pass ncomp with mesh ?!
+    data.ncomp = 2
+    return SimplexMesh(mesh=mesh), data
+
+
+# ================================================================ #
+def backwardFacingStep(h=0.2, mu=0.02):
+    with pygmsh.geo.Geometry() as geom:
+        X = []
+        X.append([-1.0, 1.0])
+        X.append([-1.0, 0.0])
+        X.append([0.0, 0.0])
+        X.append([0.0, -1.0])
+        X.append([3.0, -1.0])
+        X.append([3.0, 1.0])
+        p = geom.add_polygon(points=np.insert(np.array(X), 2, 0, axis=1), mesh_size=h)
+        geom.add_physical(p.surface, label="100")
+        for i in range(len(p.lines)): geom.add_physical(p.lines[i], label=f"{1000 + i}")
+        mesh = geom.generate_mesh()
+    data = ProblemData()
+    # boundary conditions
+    data.bdrycond.set("Dirichlet", [1000, 1001, 1002, 1003])
+    # data.bdrycond.set("Dirichlet", [1000, 1001, 1002, 1003, 1005])
+    data.bdrycond.set("Neumann", [1004])
+    data.bdrycond.set("Navier", [1005])
+    # data.bdrycond.fct[1000] = [lambda x, y, z: 1,  lambda x, y, z: 0]
+    data.bdrycond.fct[1000] = [lambda x, y, z: y*(1-y),  lambda x, y, z: 0]
+    # parameters
+    data.params.scal_glob["mu"] = mu
+    data.params.scal_glob["navier"] = 0.01
+    #TODO pass ncomp with mesh ?!
+    data.ncomp = 2
+    return SimplexMesh(mesh=mesh), data
+# ================================================================ #
+def poiseuille(h= 0.1, mu=0.02):
+    with pygmsh.geo.Geometry() as geom:
+        #ms = [h*v for v in [1.,1.,0.2,0.2]]
+        ms = h
+        p = geom.add_rectangle(xmin=-1.0, xmax=3.0, ymin=-1.0, ymax=1.0, z=0, mesh_size=ms)
+        geom.add_physical(p.surface, label="100")
+        for i in range(len(p.lines)): geom.add_physical(p.lines[i], label=f"{1000 + i}")
+        mesh = geom.generate_mesh()
+    data = ProblemData()
+   # boundary conditions
+    data.bdrycond.set("Dirichlet", [1003, 1002])
+    data.bdrycond.set("Neumann", [1001])
+    data.bdrycond.set("Navier", [1000])
+    data.bdrycond.fct[1003] = [lambda x, y, z:  1, lambda x, y, z: 0]
+    #--------------------------------------------------------------------------
+    #navier_slip_boundary
+    data.bdrycond.fct[1000] = { 'g': [lambda x, y, z:  1, lambda x, y, z:  0]}
+    #---------------------------------------------------------------------------
+    # parameters
+    data.params.scal_glob["mu"] = mu
+    data.params.scal_glob["navier"] = 1.01
+    #TODO pass ncomp with mesh ?!
+    data.ncomp = 2
+    return SimplexMesh(mesh=mesh), data
+
+# ================================================================c#
+# main(testcase='poiseuille')
+main(testcase='drivenCavity')
---
+++ packages/simfempy/simfempy-2.0.11.tar.gz/simfempy-2.0.11/simfempy/test/stokes_analytic.py
@@ -0,0 +1,59 @@
+import sys
+from os import path
+simfempypath = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.insert(0,simfempypath)
+
+import simfempy.meshes.testmeshes as testmeshes
+from simfempy.applications.stokes import Stokes
+import simfempy.applications.problemdata
+from simfempy.test.test_analytic import test_analytic
+
+
+
+#----------------------------------------------------------------#
+def test(dim, **kwargs):
+    exactsolution = kwargs.pop('exactsolution', 'Linear')
+    data = simfempy.applications.problemdata.ProblemData()
+    data.params.scal_glob['mu'] = kwargs.pop('mu', 1)
+    data.params.scal_glob['navier'] = kwargs.pop('navier', 1)
+    paramargs = {}
+    paramargs['dirichletmethod'] = kwargs.pop('dirichletmethod', ['strong','nitsche'])
+    if dim==2:
+        data.ncomp=2
+        createMesh = testmeshes.unitsquare
+        colors = [1000,1001,1002,1003]
+        colorsneu = [1001]
+        colorsnav = [1003]
+        colorsp = [1002]
+    else:
+        data.ncomp=3
+        createMesh = testmeshes.unitcube
+        colors = [100,101,102,103,104,105]
+        colorsneu = [103]
+        colorsnav = [105]
+        colorsp = [101]
+    colorsdir = [col for col in colors if col not in colorsnav and col not in colorsp and col not in colorsneu]
+    if 'strong' in paramargs['dirichletmethod']:
+        colorsdir.append(*colorsnav)
+        colorsnav=[]
+    data.bdrycond.set("Dirichlet", colorsdir)
+    data.bdrycond.set("Neumann", colorsneu)
+    data.bdrycond.set("Navier", colorsnav)
+    data.bdrycond.set("Pressure", colorsp)
+    data.postproc.set(name='bdrypmean', type='bdry_pmean', colors=colorsneu)
+    data.postproc.set(name='bdrynflux', type='bdry_nflux', colors=colorsdir)
+    linearsolver = kwargs.pop('linearsolver', 'iter')
+    applicationargs= {'problemdata': data, 'exactsolution': exactsolution, 'linearsolver': linearsolver}
+    # applicationargs['mode'] = 'newton'
+    return test_analytic(application=Stokes, createMesh=createMesh, paramargs=paramargs, applicationargs=applicationargs, **kwargs)
+
+
+
+#================================================================#
+if __name__ == '__main__':
+    # test(dim=2, exactsolution=[["x**2-y","-2*x*y+x**2"],"x*y"], dirichletmethod='nitsche', niter=6, plotsolution=False, linearsolver='iter_gcrotmk')
+    test(dim=3, exactsolution=[["x**2-y+2","-2*x*y+x**2","x+y"],"x*y*z"], dirichletmethod='nitsche', niter=5, plotsolution=False, linearsolver='iter_gcrotmk')
+    # test(dim=2, exactsolution=[["-y","x"],"10"], niter=3, dirichletmethod='nitsche', plotsolution=False, linearsolver='umf')
+    # test(dim=2, exactsolution=[["1","0"],"10"], niter=3, dirichletmethod='nitsche', plotsolution=True, linearsolver='umf')
+    # test(dim=3, exactsolution=[["-z","x","x+y"],"11"], niter=3, dirichletmethod=['nitsche'], plotsolution=False, linearsolver='umf')
+    # test(dim=2, exactsolution=[["0","1"],"1"], niter=2, h1=2)
