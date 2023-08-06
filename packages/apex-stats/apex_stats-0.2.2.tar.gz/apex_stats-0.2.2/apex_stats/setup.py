10d09c4d0206011a17f4fb30267d3fcba6c305d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:28 +0000

    Add package apex-stats/0.2.2/apex_stats-0.2.2.tar.gz

---
+++ packages/apex-stats/apex_stats-0.2.2.tar.gz/apex_stats-0.2.2/apex_stats/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+with open("pederas.md", "r") as f:
+  pederas = f.read()
+
+setup(
+  name = 'apex_stats',        
+  packages = ['apex_stats'],  
+  version = '0.2.2',     
+  license='MIT', 
+  description = 'API wrapper for https://apex.tracker.gg',   
+  author_email = 'yamozha16@protonmail.ch',  
+  url = 'https://github.com/yamozha/apex_stats',  
+  long_description=pederas,
+  long_description_content_type='text/markdown'
+
+)
