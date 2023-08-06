3459d90a25eaa851f6873816dca5f78fe6ae023d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:49 +0000

    Add package CAVA/1.3.4/CAVA-1.3.4.tar.gz

---
+++ packages/CAVA/CAVA-1.3.4.tar.gz/CAVA-1.3.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+with open(os.path.join(os.path.dirname(__file__), 'bin', 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+with open('requirements.txt') as requires_file:
+    requires = requires_file.read().split('\n')
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='CAVA',
+    version=version,
+    description='CAVA (Clinical Annotation of VAriants)',
+    url='https://github.com/Steven-N-Hart/CAVA',
+    author='Steven-N-Hart',
+    author_email='hart.steven@mayo.edu',
+    license='MIT',
+    packages=['cava_', 'ensembldb'],
+    scripts=[
+        'bin/CAVA.py',
+        'bin/EnsemblDB.py',
+        'bin/dbSNPDB.py',
+    ],
+    install_requires=requires,
+    zip_safe=False,
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
