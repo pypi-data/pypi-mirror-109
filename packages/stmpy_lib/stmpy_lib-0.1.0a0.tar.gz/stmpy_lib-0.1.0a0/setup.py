325bd0472921d97f61166bddd4c6778f1c940ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:00 +0000

    Add package stmpy_lib/0.1.0a0/stmpy_lib-0.1.0a0.tar.gz

---
+++ packages/stmpy_lib/stmpy_lib-0.1.0a0.tar.gz/stmpy_lib-0.1.0a0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'ecdsa>=0.10',
+    'six>=1.5.2',
+    'websocket-client==0.15.0'
+]
+
+
+setup(
+    name="stmpy_lib",
+    version="0.1.0a",
+    author="stm-open",
+    author_email="open@labs.stream",
+    url="https://github.com/stm-open/stmpy.git",
+    description="Python lib for the STM network",
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=install_requires,
+)
