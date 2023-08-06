79ed5f7fbf49ae94b054e9803b779ad2b25ecff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:39 +0000

    Add package jsm-user-services/1.0.0/jsm_user_services-1.0.0.tar.gz

---
+++ packages/jsm-user-services/jsm_user_services-1.0.0.tar.gz/jsm_user_services-1.0.0/jsm_user_services/exception.py
@@ -0,0 +1,24 @@
+class MissingRequiredConfiguration(Exception):
+    pass
+
+
+class IncorrectTypePermissionConfiguration(Exception):
+    pass
+
+
+class ApplicationSettingsNotFound(Exception):
+    """
+    Exception that indicates the application settings was not found through known methods.
+    """
+
+
+class RequestIDModuleNotFound(Exception):
+    """
+    Exception that indicates the request id module was not found through known methods.
+    """
+
+
+class DecoratorWrongUsage(Exception):
+    """
+    Generic exception when a decorator is used wrongly.
+    """
