af1e97a1dd0661ec79e8dea239a42ffba93f0a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:31 +0000

    Add package django-loader/0.0.2/django_loader-0.0.2-py3-none-any.whl

---
+++ packages/django-loader/django_loader-0.0.2-py3-none-any.whl/loader/__init__.py
@@ -0,0 +1,15 @@
+# ******************************************************************************
+#
+# django-loader, a configuration and secret loader for Django
+#
+# __init__.py:  loader module interface
+#
+# Copyright (C) 2021 Jeremy A Gray <gray@flyquackswim.com>.
+#
+# SPDX-License-Identifier: MIT
+#
+# ******************************************************************************
+#
+"""Loader module."""
+
+from loader import load_secrets
