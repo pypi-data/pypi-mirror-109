dc05d6907ec653e77604b6f8938a97675ff3d17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:31 +0000

    Add package compliance-trestle/0.18.0/compliance-trestle-0.18.0.tar.gz

---
+++ packages/compliance-trestle/compliance-trestle-0.18.0.tar.gz/compliance-trestle-0.18.0/scripts/gen_oscal.py
@@ -0,0 +1,124 @@
+# -*- mode:python; coding:utf-8 -*-
+
+# Copyright (c) 2020 IBM Corp. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Script to generate python models from oscal using datamodel-code-generator."""
+import re
+import sys
+from pathlib import Path
+from subprocess import CalledProcessError, check_call
+
+
+def load_git():
+    """Load git submodule for oscal."""
+    print('git add and update oscal modules')
+    try:
+        check_call('git submodule add https://github.com/usnistgov/OSCAL.git nist-source'.split())
+    except CalledProcessError:
+        # silently ignore already existing module
+        pass
+    # Add second module
+    try:
+        check_call('git submodule add https://github.com/usnistgov/oscal-content.git nist-content'.split())
+    except CalledProcessError:
+        # silently ignore already existing module
+        pass
+    try:
+        check_call('git submodule update --init'.split())
+    except CalledProcessError as error:
+        print(f'Error updating the oscal git submodule {error}')
+    try:
+        check_call('git submodule update --remote --merge'.split())
+    except CalledProcessError as error:
+        print(f'Error updating the oscal git submodule {error}')
+
+
+def generate_model(full_name, out_full_name):
+    """Generate a single model with datamodel-codegen."""
+    print(f'generate python model and apply fix_any: {str(full_name)} -> {str(out_full_name)}')
+    args = [
+        'datamodel-codegen',
+        '--disable-timestamp',
+        '--disable-appending-item-suffix',
+        '--input-file-type',
+        'jsonschema',
+        '--input',
+        str(full_name),
+        '--base-class',
+        'trestle.core.base_model.OscalBaseModel',
+        '--output',
+        str(out_full_name)
+    ]
+    try:
+        check_call(args)
+    except CalledProcessError as error:
+        print(f'Error calling datamodel-codegen for file {full_name} error {error}')
+
+
+def fix_schema(source_file, dest_file):
+    "Fix recent changes to nist schema."
+    kill_list = ['oscal-ap-oscal-metadata-', 'oscal-ar-oscal-metadata-', 'oscal-catalog-oscal-metadata-', 'oscal-component-definition-oscal-metadata-',
+                 'oscal-poam-metadata-', 'oscal-profile-oscal-metadata-', 'oscal-ar-oscal-assessment-common-', 'oscal-ap-oscal-assessment-common-',
+                 'oscal-poam-oscal-assessment-common-', 'oscal-metadata-', 'oscal-ap-', 'oscal-ar-', 'oscal-catalog-', 'oscal-poam-', 'oscal-profile-', 'oscal-ssp-', 'oscal-component-definition-']
+    with open(source_file, 'r', encoding='utf8') as src:
+        with open(dest_file, 'w', encoding='utf8') as dst:
+            for l in src.readlines():
+                for k in kill_list:
+                    l = l.replace(k, '')
+                dst.write(l)
+
+
+def generate_models():
+    """Generate all models including 3rd party."""
+    print('generating models')
+    out_dir = Path('trestle/oscal')
+    out_dir.mkdir(exist_ok=True, parents=True)
+    tmp_dir = out_dir / 'tmp'
+    tmp_dir.mkdir(exist_ok=True, parents=True)
+    out_init = out_dir / '__init__.py'
+    out_init.touch(exist_ok=True)
+
+    # ver_file = out_dir / 'b4_fix' / 'datamodel-codegen-version.txt'
+
+    # try:
+    #    check_call(f'datamodel-codegen --version >> {ver_file}'.split(), shell=True)
+    # except CalledProcessError as error:
+    #    print(f'Error calling datamodel-codegen for version: error {error}')
+
+    in_dir = Path('nist-source/json/schema')
+    for full_name in in_dir.glob('oscal_*_schema.json'):
+        fixed_name = tmp_dir / full_name.name
+        fix_schema(full_name, fixed_name)
+        try:
+            obj = re.search('oscal_(.+?)_schema.json', str(fixed_name)).group(1)
+        except AttributeError:
+            print(f'Warning: filename did not parse properly: {fixed_name}')
+            obj = None
+            continue
+        oscal_name = obj.replace('-', '_')
+        out_fname = oscal_name + '.py'
+        out_full_name = out_dir / out_fname
+        generate_model(fixed_name, out_full_name)
+    generate_model('3rd-party-schema-documents/IBM_target_schema_v1.0.0.json', out_dir / 'target.py')
+
+
+def main():
+    """Load git and generate models."""
+    load_git()
+    generate_models()
+    print('DONE')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/compliance-trestle/compliance-trestle-0.18.0.tar.gz/compliance-trestle-0.18.0/tests/trestle/tasks/tanium_to_oscal_test.py
@@ -0,0 +1,231 @@
+# -*- mode:python; coding:utf-8 -*-
+# Copyright (c) 2020 IBM Corp. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tanium to OSCAL task tests."""
+
+import configparser
+import os
+import pathlib
+import uuid
+from unittest.mock import Mock, patch
+
+import trestle.core.const as const
+import trestle.tasks.tanium_to_oscal as tanium_to_oscal
+import trestle.transforms.implementations.tanium as tanium
+from trestle.tasks.base_task import TaskOutcome
+
+uuid_mock1 = Mock(return_value=uuid.UUID('56666738-0f9a-4e38-9aac-c0fad00a5821'))
+uuid_mock2 = Mock(return_value=uuid.UUID('46aADFAC-A1fd-4Cf0-a6aA-d1AfAb3e0d3e'))
+
+def test_print_info(tmpdir):
+    """Test print_info call."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.print_info()
+    assert retval is None
+
+def test_simulate(tmpdir):
+    """Test simulate call."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.simulate()
+    assert retval == TaskOutcome.SIM_SUCCESS
+    assert len(os.listdir(str(tmpdir))) == 0
+    
+def test_simulate_no_config(tmpdir):
+    """Test simulate no config call."""
+    tgt = tanium_to_oscal.TaniumToOscal(None)
+    retval = tgt.simulate()
+    assert retval == TaskOutcome.SIM_FAILURE
+    assert len(os.listdir(str(tmpdir))) == 0
+
+def test_simulate_no_overwrite(tmpdir):
+    """Test simulate no overwrite call."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.SUCCESS
+    assert len(os.listdir(str(tmpdir))) == 1
+    section['output-overwrite'] = 'false'
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.simulate()
+    assert retval == TaskOutcome.SIM_FAILURE
+    assert len(os.listdir(str(tmpdir))) == 1
+
+def test_simulate_no_input_dir(tmpdir):
+    """Test simulate with no input dir call."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    config.remove_option('task.tanium-to-oscal', 'input-dir')
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.simulate()
+    assert retval == TaskOutcome.SIM_FAILURE
+    assert len(os.listdir(str(tmpdir))) == 0
+
+def test_simulate_no_ouput_dir(tmpdir):
+    """Test simulate with no output dir call."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    config.remove_option('task.tanium-to-oscal', 'output-dir')
+    section = config['task.tanium-to-oscal']
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.simulate()
+    assert retval == TaskOutcome.SIM_FAILURE
+    assert len(os.listdir(str(tmpdir))) == 0
+
+def test_simulate_bad_input_file(tmpdir):
+    """Test simulate with bad input file call."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    config.remove_option('task.tanium-to-oscal', 'input-dir')
+    config.set('task.tanium-to-oscal', 'input-dir', 'tests/data/tasks/tanium/input-bad')
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.simulate()
+    assert retval == TaskOutcome.SIM_FAILURE
+    assert len(os.listdir(str(tmpdir))) == 0
+
+@patch(target='uuid.uuid4', new=uuid_mock1)
+def test_execute(tmpdir):
+    """Test execute call."""
+    tanium.TaniumTransformer.set_timestamp('2021-02-24T19:31:13+00:00')
+    assert tanium.TaniumTransformer.get_timestamp() == '2021-02-24T19:31:13+00:00'
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.SUCCESS
+    assert len(os.listdir(str(tmpdir))) == 1
+    f_expected = pathlib.Path('tests/data/tasks/tanium/output/') / 'Tanium.oscal.json'
+    f_produced = tmpdir  / 'Tanium.oscal.json'
+    assert [row for row in open(f_produced, encoding=const.FILE_ENCODING)] == [row for row in open(f_expected, encoding=const.FILE_ENCODING)]
+
+def test_execute_no_config(tmpdir):
+    """Test execute no config call."""
+    tgt = tanium_to_oscal.TaniumToOscal(None)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.FAILURE
+    assert len(os.listdir(str(tmpdir))) == 0
+
+def test_execute_no_overwrite_dir(tmpdir):
+    """Test execute no overwrite directory call."""
+    tanium.TaniumTransformer.set_timestamp('2021-02-24T19:31:13+00:00')
+    execute_no_overwrite_dir_part1(tmpdir)
+    execute_no_overwrite_dir_part2(tmpdir)
+    f_expected = pathlib.Path('tests/data/tasks/tanium/output/') / 'Tanium.oscal.json'
+    f_produced = tmpdir  / 'Tanium.oscal.json'
+    assert [row for row in open(f_produced, encoding=const.FILE_ENCODING)] == [row for row in open(f_expected, encoding=const.FILE_ENCODING)]
+    
+@patch(target='uuid.uuid4', new=uuid_mock1)
+def execute_no_overwrite_dir_part1(tmpdir):
+    """Create expected output."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.SUCCESS
+    assert len(os.listdir(str(tmpdir))) == 1
+
+@patch(target='uuid.uuid4', new=uuid_mock2)
+def execute_no_overwrite_dir_part2(tmpdir):
+    """Attempt to overwrite."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    section = config['task.tanium-to-oscal']
+    section['output-overwrite'] = 'false'
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.FAILURE
+
+def test_execute_no_input_dir(tmpdir):
+    """Test execute with no input dir call."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    config.remove_option('task.tanium-to-oscal', 'input-dir')
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.FAILURE
+    assert len(os.listdir(str(tmpdir))) == 0
+
+def test_execute_no_ouput_dir(tmpdir):
+    """Test execute with no output dir call."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    config.remove_option('task.tanium-to-oscal', 'output-dir')
+    section = config['task.tanium-to-oscal']
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.FAILURE
+    assert len(os.listdir(str(tmpdir))) == 0
+
+def test_execute_bad_timestamp(tmpdir):
+    """Test execute with bad timestamp."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    section = config['task.tanium-to-oscal']
+    section['timestamp'] = str('bogus')
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.FAILURE
+    assert len(os.listdir(str(tmpdir))) == 0
+    
+@patch(target='uuid.uuid4', new=uuid_mock1)
+def test_execute_override_timestamp(tmpdir):
+    """Test execute override timestamp call."""
+    tanium.TaniumTransformer.set_timestamp('2020-02-24T19:31:13+00:00')
+    assert tanium.TaniumTransformer.get_timestamp() == '2020-02-24T19:31:13+00:00'
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/tanium/demo-tanium-to-oscal.config')
+    config.read(config_path)
+    section = config['task.tanium-to-oscal']
+    section['output-dir'] = str(tmpdir)
+    tgt = tanium_to_oscal.TaniumToOscal(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.SUCCESS
+    assert len(os.listdir(str(tmpdir))) == 1
+    f_expected = pathlib.Path('tests/data/tasks/tanium/output/') / 'Tanium.oscal.2020.json'
+    f_produced = tmpdir  / 'Tanium.oscal.json'
+    assert [row for row in open(f_produced, encoding=const.FILE_ENCODING)] == [row for row in open(f_expected, encoding=const.FILE_ENCODING)]
