45372473f7c68a85fb4350798efff5e5979d4292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:21 +0000

    Add package quizzes/0.6/quizzes-0.6.tar.gz

---
+++ packages/quizzes/quizzes-0.6.tar.gz/quizzes-0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(name="quizzes",
+      version="0.6",
+      description="package to generate MCQ questions with options and answer",
+      author="Vaibhav Agrawal",
+      packages=["quizzes"],
+      install_requires=[
+          'torch==1.8.1',
+          'transformers==4.4.2',
+          "numpy",
+          "sentencepiece",
+          "sense2vec",
+          "keybert",
+          "spacy",
+          "pke @ git+ssh://git@github.com/Vaibhavbrkn/pke.git#egg=pke"
+      ],
+      )
