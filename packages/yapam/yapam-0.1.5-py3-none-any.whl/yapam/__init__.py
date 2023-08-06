eeef211fb93e7130d71892c85390d83ba6ff3c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:27 +0000

    Add package yapam/0.1.5/yapam-0.1.5-py3-none-any.whl

---
+++ packages/yapam/yapam-0.1.5-py3-none-any.whl/yapam/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Yet another ammo maker for Yandex Tank.
+
+for additional info see README.md
+"""
+
+__version__ = '0.1.5'
+__all__ = [
+    'armory', 'config', 'phantom'
+]
+
+__author__ = 'Devyatkin Aleksei <yapam@devyatkin.dev>'
