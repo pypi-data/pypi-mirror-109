d70b355c8601c343e186fc98d5de8de36fd935e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:58 +0000

    Add package lucid-data/0.1.2/lucid-data-0.1.2.tar.gz

---
+++ packages/lucid-data/lucid-data-0.1.2.tar.gz/lucid-data-0.1.2/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="lucid-data",
+    version="0.1.2",
+    description="Scripts for processing, profiling, and publishing data",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liquidcarbon/lucid",
+    author="Alex Kislukhin",
+    author_email="liquidc@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["lucid"],
+    include_package_data=True,
+    install_requires=[
+        "bokeh",
+        "boto3",
+        "matplotlib",
+        "pandas",
+        "requests",
+    ],
+    python_requires=">=3.7",
+)
