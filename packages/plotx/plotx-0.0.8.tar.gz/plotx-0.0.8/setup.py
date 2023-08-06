21d297bdb63c525be18a8b37dc47b9214ca7a626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:37 +0000

    Add package plotx/0.0.8/plotx-0.0.8.tar.gz

---
+++ packages/plotx/plotx-0.0.8.tar.gz/plotx-0.0.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.8'
+DESCRIPTION = 'Shortcuts for matplotlib'
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'plotx.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Setting up
+setup(
+    name="plotx",
+    version=VERSION,
+    author="DaveAsator (Eugene Bolshakov)",
+    author_email="<omnispark@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy', 'matplotlib'],
+    keywords=['plot'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
