dd73c34cf01501b87b61c5ca77728bb5aae369db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:08 +0000

    Add package pypg/0.0.3/pypg-0.0.3-py3-none-any.whl

---
+++ packages/pypg/pypg-0.0.3-py3-none-any.whl/pypg/__init__.py
@@ -0,0 +1,35 @@
+"""
+PyPG: A library for PPG (PhotoPlethysmoGram) processing
+
+
+Documentation
+-----------
+Available in the docstrings for now; online soon.
+
+
+Modules
+-----------
+Using the modules requires an explicit import. For example:
+``import pypg.cycles``
+
+    ::
+
+    cycles
+    filters
+    plots
+
+
+Inspired By
+-----------
+SciPy: A scientific computing package for Python
+
+
+References
+----------
+https://github.com/scipy/scipy
+
+"""
+
+import pypg.cycles
+import pypg.filters
+import pypg.plots
