72ce17ecb81b4763d7219414c1c444304b9be217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:44 +0000

    Add package fidesctl/0.9.2/fidesctl-0.9.2.tar.gz

---
+++ packages/fidesctl/fidesctl-0.9.2.tar.gz/fidesctl-0.9.2/src/fidesctl/core/api.py
@@ -0,0 +1,96 @@
+"""A wrapper to make calling the API consistent across Fidesctl."""
+from typing import Dict
+
+import requests
+
+
+def generate_request_headers() -> Dict[str, str]:
+    """
+    Generate the headers for a request.
+    """
+    return {"Content-Type": "application/json", "user-id": "1"}
+
+
+def generate_object_url(
+    url: str, object_type: str = "", object_id: str = "", version: str = "v1"
+) -> str:
+    """
+    Generate an object's URL using a base url, the object type and a version.
+    """
+    return f"{url}/{version}/{object_type}/{object_id}"
+
+
+def find(url: str, object_type: str, object_key: str) -> requests.Response:
+    """
+    Get an object by its fidesKey.
+    """
+    object_url = generate_object_url(url, object_type)
+    find_url = f"{object_url}find/{object_key}"
+    headers = generate_request_headers()
+    return requests.get(find_url, headers=headers)
+
+
+def get(url: str, object_type: str, object_id: str) -> requests.Response:
+    """
+    Get an object by its id.
+    """
+    object_url = generate_object_url(url, object_type, object_id)
+    headers = generate_request_headers()
+    return requests.get(object_url, headers=headers)
+
+
+def create(url: str, object_type: str, json_object: str) -> requests.Response:
+    """
+    Create a new object.
+    """
+    payload = json_object
+    object_url = generate_object_url(url, object_type)
+    headers = generate_request_headers()
+    return requests.post(object_url, headers=headers, data=payload)
+
+
+def ping(url: str) -> requests.Response:
+    """
+    Pings the Server on the base url to make sure it's available.
+    """
+    return requests.get(url)
+
+
+def delete(url: str, object_type: str, object_id: str) -> requests.Response:
+    """
+    Delete an object by its id.
+    """
+    object_url = generate_object_url(url, object_type, object_id)
+    headers = generate_request_headers()
+    return requests.delete(object_url, headers=headers)
+
+
+def show(url: str, object_type: str) -> requests.Response:
+    """
+    Get a list of all of the objects of a certain type.
+    """
+    object_url = generate_object_url(url, object_type)
+    headers = generate_request_headers()
+    return requests.get(object_url, headers=headers)
+
+
+def update(
+    url: str, object_type: str, object_id: str, json_object: Dict
+) -> requests.Response:
+    """
+    Update an existing object.
+    """
+    object_url = generate_object_url(url, object_type, object_id)
+    payload = json_object
+    headers = generate_request_headers()
+    return requests.post(object_url, headers=headers, data=payload)
+
+
+def evaluate(url: str, json_object: str) -> requests.Response:
+    """
+    Evaluate a registry based on organizational policies.
+    """
+    url = f"{url}dry-run"
+    payload = json_object
+    headers = generate_request_headers()
+    return requests.post(url, headers=headers, data=payload)
---
+++ packages/fidesctl/fidesctl-0.9.2.tar.gz/fidesctl-0.9.2/src/fidesctl/core/models.py
@@ -0,0 +1,189 @@
+"""This module contains all of the models for the Fides API."""
+# pylint: skip-file
+from typing import Dict, List, Optional, Type, Union
+
+from pydantic import BaseModel, validator
+
+
+class DataCategory(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    fidesKey: str
+    name: str
+    clause: str
+    description: str
+
+
+class DataQualifier(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    fidesKey: str
+    name: str
+    clause: str
+    description: str
+
+
+class DatasetField(BaseModel):
+    name: str
+    description: str
+    dataCategories: Optional[List[str]]
+    dataQualifier: Optional[str]
+
+
+class DatasetTable(BaseModel):
+    name: str
+    description: str
+    fields: List[DatasetField]
+
+    @validator("fields")
+    def sort_list_objects(cls, v: List) -> List:
+        v.sort(key=lambda x: x.name)
+        return v
+
+
+class Dataset(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    fidesKey: str
+    name: str
+    description: str
+    location: str
+    datasetType: str
+    tables: List[DatasetTable]
+
+    @validator("tables")
+    def sort_list_objects(cls, v: List) -> List:
+        v.sort(key=lambda x: x.name)
+        return v
+
+
+class DataSubjectCategory(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    fidesKey: str
+    name: str
+    description: str
+
+
+class DataUse(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    fidesKey: str
+    name: str
+    clause: str
+    description: str
+
+
+class DataRule(BaseModel):
+    inclusion: str
+    values: List[str]
+
+
+class PolicyRule(BaseModel):
+    organizationId: int
+    policyId: int
+    fidesKey: str
+    name: str
+    description: str
+    dataCategories: DataRule
+    dataUses: DataRule
+    dataSubjectCategories: DataRule
+    dataQualifier: str
+    action: str
+
+
+class Policy(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    fidesKey: str
+    name: str
+    description: str
+    rules: List[PolicyRule]
+
+    @validator("rules")
+    def sort_list_objects(cls, v: List) -> List:
+        v.sort(key=lambda x: x.name)
+        return v
+
+
+class Registry(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    fidesKey: str
+    name: str
+    description: str
+
+
+class DataDeclaration(BaseModel):
+    dataCategories: List[str]
+    dataUse: str
+    dataQualifier: str
+    dataSubjectCategories: List[str]
+
+
+class System(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    registryId: int
+    fidesKey: str
+    fidesSystemType: str
+    name: str
+    description: str
+    declarations: Optional[List[DataDeclaration]]
+    systemDependencies: List[str]
+    datasets: List[str]
+
+
+class User(BaseModel):
+    id: Optional[int]
+    organizationId: int
+    fidesKey: str
+    userName: str
+    firstName: str
+    lastName: str
+    role: str
+
+
+class Organization(BaseModel):
+    id: Optional[int]
+    fidesKey: str
+    name: str
+    description: str
+
+
+# A mapping of object names to their Endpoints
+FidesTypes = Union[
+    Type[DataCategory],
+    Type[DataQualifier],
+    Type[Dataset],
+    Type[DataSubjectCategory],
+    Type[DataUse],
+    Type[Organization],
+    Type[Policy],
+    Type[Registry],
+    Type[System],
+]
+MODEL_DICT: Dict[str, FidesTypes] = {
+    "data-category": DataCategory,
+    "data-qualifier": DataQualifier,
+    "dataset": Dataset,
+    "data-subject-category": DataSubjectCategory,
+    "data-use": DataUse,
+    "organization": Organization,
+    "policy": Policy,
+    "registry": Registry,
+    "system": System,
+}
+MODEL_LIST = list(MODEL_DICT.keys())
+
+FidesModel = Union[
+    DataCategory,
+    DataQualifier,
+    Dataset,
+    DataSubjectCategory,
+    DataUse,
+    Organization,
+    Policy,
+    Registry,
+    System,
+]
