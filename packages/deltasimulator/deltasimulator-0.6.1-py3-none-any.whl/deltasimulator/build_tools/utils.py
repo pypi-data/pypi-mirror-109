fdb0827df4b2ad859e1ce54ec7b4a0ff33448e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:00 +0000

    Add package deltasimulator/0.6.1/deltasimulator-0.6.1-py3-none-any.whl

---
+++ packages/deltasimulator/deltasimulator-0.6.1-py3-none-any.whl/deltasimulator/build_tools/utils.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+import traceback
+
+
+log = logging.getLogger(__name__)
+
+
+async def wait(flag):
+    """Often, we want to sequence multiple actions that need to happen
+    after a single coro.
+
+    Wait allows you to purely block on a coro completing whilst discarding the
+    result, and allows this to happen multiple times.
+
+    Parameters
+    ----------
+    flag : Coroutine
+        The flag that we are waiting to terminate.    
+    """
+    fut = asyncio.ensure_future(flag)
+    try:
+        await fut
+    except RuntimeError:
+        log.debug(traceback.format_exc())
+        pass
+    return
+
+
+async def multiple_waits(flags):
+    """Same as :py:func:`wait`, but for waiting on multiple coroutines.
+
+    Parameters
+    ----------
+    flags : list
+        A list of :class:`Coroutine` objects to wait for.
+    """
+    for flag in flags:
+        await wait(flag)
+    return
