b4173aa5837653af497c9bac582445bfc5cfca7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:54 +0000

    Add package Docco/0.0.1/Docco-0.0.1.tar.gz

---
+++ packages/Docco/Docco-0.0.1.tar.gz/Docco-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Docco",
+    version="0.0.1",
+    author="Docco-Labs",
+    author_email="",
+    description="Work in progress - Docco",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/docco/docco-package",
+    project_urls={
+        "Bug Tracker": "https://github.com/docco/docco-package/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
