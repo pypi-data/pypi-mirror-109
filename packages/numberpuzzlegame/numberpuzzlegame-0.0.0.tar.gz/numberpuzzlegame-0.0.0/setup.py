cd331edf7011cf3986871bb978d0264fea1fd602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:24 +0000

    Add package numberpuzzlegame/0.0.0/numberpuzzlegame-0.0.0.tar.gz

---
+++ packages/numberpuzzlegame/numberpuzzlegame-0.0.0.tar.gz/numberpuzzlegame-0.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.0'
+DESCRIPTION = "'sliding' number puzzle game"
+
+
+# Setting up
+setup(
+    name="numberpuzzlegame",
+    author="Akhil Avvari",
+    author_email="<yellowloafofbread@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pygame'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
