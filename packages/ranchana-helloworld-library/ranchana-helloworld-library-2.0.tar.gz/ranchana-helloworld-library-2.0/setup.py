017a1213a3528e2c1ae865f8cf200c539b0d0a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:01 +0000

    Add package ranchana-helloworld-library/2.0/ranchana-helloworld-library-2.0.tar.gz

---
+++ packages/ranchana-helloworld-library/ranchana-helloworld-library-2.0.tar.gz/ranchana-helloworld-library-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ranchana-helloworld-library",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/nunranchana/demo-helloworld-library.git',
+    author='Ranchana',
+    author_email='imp.ranchana@gmail.com',
+)
