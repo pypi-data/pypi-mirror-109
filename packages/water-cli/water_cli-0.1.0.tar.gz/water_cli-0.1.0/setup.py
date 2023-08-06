7c43ec28c2463e61d47982cdf31e9ddaeda5fad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:53 +0000

    Add package water-cli/0.1.0/water_cli-0.1.0.tar.gz

---
+++ packages/water-cli/water_cli-0.1.0.tar.gz/water_cli-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['water_cli']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'water-cli',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'david',
+    'author_email': 'davidventura27@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
