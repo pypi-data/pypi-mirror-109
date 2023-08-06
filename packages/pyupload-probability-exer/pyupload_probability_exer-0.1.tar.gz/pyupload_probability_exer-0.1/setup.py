9f695247da93dcd2ca008140529c9aeb1e72fe0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:07 +0000

    Add package pyupload-probability-exer/0.1/pyupload_probability_exer-0.1.tar.gz

---
+++ packages/pyupload-probability-exer/pyupload_probability_exer-0.1.tar.gz/pyupload_probability_exer-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pyupload_probability_exer',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['pyupload_probability_exer'],
+      arthur='Fayez Abusharkh',
+      arthur_email='f_sharkh@hotmail.com',    
+      zip_safe=False)
