eee470bb4e75217ff52c4ab9e2ce05691182a1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:30 +0000

    Add package ordered-argparse/1.0.10/ordered_argparse-1.0.10.tar.gz

---
+++ packages/ordered-argparse/ordered_argparse-1.0.10.tar.gz/ordered_argparse-1.0.10/src/ordered_argparse/__init__.py
@@ -0,0 +1,8 @@
+from .ordered_argparse import *
+
+
+__author__ = "Sven Siegmund"
+__author_email__ = "sven.siegmund@gmail.com"
+__version__ = "1.0.10"
+__maintainer__ = "Sven Siegmund"
+__url__ = "https://github.com/Nagidal/ordered_argparse"
