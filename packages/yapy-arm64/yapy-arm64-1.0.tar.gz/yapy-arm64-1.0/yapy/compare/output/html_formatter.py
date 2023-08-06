ef5a988bddc104f4fdb9b3418789764cfaaf28cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:26 +0000

    Add package yapy-arm64/1.0/yapy-arm64-1.0.tar.gz

---
+++ packages/yapy-arm64/yapy-arm64-1.0.tar.gz/yapy-arm64-1.0/yapy/compare/output/html_formatter.py
@@ -0,0 +1,40 @@
+from yapy.compare.output.formatter import OutputFormatter
+
+
+class HtmlFormatter(OutputFormatter):
+
+    def __init__(self, plagiarism_compare):
+        super().__init__(plagiarism_compare)
+
+    def __draw_header(self):
+        headers = self.headers
+
+        header = '<th>Results</th>'
+
+        for element in headers:
+            header += f'<th>{element}</th>'
+
+        return f'<tr>{header}</tr>'
+
+    def __draw_row(self, row_id):
+        row_data = [self.plagiarism_compare.result[row_id][column]
+                    for column in range(0, len(self.plagiarism_compare.result))]
+
+        row = ''
+
+        for element in row_data:
+            row += f'<td>{element}</td>'
+
+        return f'<tr><th>{self.headers[row_id]}</th>{row}</tr>'
+
+    def format(self):
+        table = self.__draw_header()
+
+        for row in range(0, len(self.plagiarism_compare.result)):
+            table += self.__draw_row(row)
+
+        return f'<table>{table}</table>'
+
+    def format_suspicious(self):
+        # todo
+        return ""
