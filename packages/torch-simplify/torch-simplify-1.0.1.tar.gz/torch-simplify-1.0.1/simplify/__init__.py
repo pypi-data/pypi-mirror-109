5136884c054923f362621e0650417c8d5d5bc199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:32 +0000

    Add package torch-simplify/1.0.1/torch-simplify-1.0.1.tar.gz

---
+++ packages/torch-simplify/torch-simplify-1.0.1.tar.gz/torch-simplify-1.0.1/simplify/__init__.py
@@ -0,0 +1,24 @@
+from typing import List
+
+import torch
+import torch.nn as nn
+
+from . import layers
+from .fuse import fuse
+from .propagate import propagate_bias
+from .remove import remove_zeroed
+
+__version__ = "1.0.1"
+
+
+def simplify(model: nn.Module, x: torch.Tensor, pinned_out: List = None, bn_folding: List = None) -> nn.Module:
+    if bn_folding is None:
+        bn_folding = []
+    if pinned_out is None:
+        pinned_out = []
+    
+    fuse(model, bn_folding)
+    propagate_bias(model, x, pinned_out)
+    remove_zeroed(model, x, pinned_out)
+    
+    return model
