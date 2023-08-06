d165337e44512163a85bca0991941a7a2c6a0676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:40 +0000

    Add package notelink/0.0.2/notelink-0.0.2-py3-none-any.whl

---
+++ packages/notelink/notelink-0.0.2-py3-none-any.whl/src/core/helpers.py
@@ -0,0 +1,21 @@
+import click
+
+from src.core import NoteMe
+
+
+OPEN_BROWSER = 'open_browser'
+REMOVE = 'remove'
+
+
+def helper_for_action(note_me: NoteMe, link_chosen: str, action: str) -> None:
+    if action.lower() == OPEN_BROWSER:
+        click.launch(link_chosen)
+    elif action.lower() == REMOVE:
+        note_me.remove_link(link_chosen)
+
+
+def helper_for_hostname_action(note_me: NoteMe, link_chosen: str, action: str) -> None:
+    if action.lower() == OPEN_BROWSER:
+        click.launch('https://' + link_chosen)
+    elif action.lower() == REMOVE:
+        note_me.remove_hostname(link_chosen)
