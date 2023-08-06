4edc173943f6123053afa4da854080086150c048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:09 +0000

    Add package hapiclient/0.2.1/hapiclient-0.2.1.tar.gz

---
+++ packages/hapiclient/hapiclient-0.2.1.tar.gz/hapiclient-0.2.1/hapiclient/__init__.py
@@ -0,0 +1,10 @@
+# Allow "from hapiclient import hapi"
+from hapiclient.hapi import hapi
+
+# Allow "from hapiclient import hapitime2datetime"
+from hapiclient.hapitime import hapitime2datetime
+
+# Allow "from hapiclient import HAPIError"
+from hapiclient.util import HAPIError
+
+__version__ = '0.2.1'
