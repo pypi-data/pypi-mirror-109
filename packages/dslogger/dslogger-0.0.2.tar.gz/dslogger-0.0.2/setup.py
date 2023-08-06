992133622b2efe23bf87ed9d28b738ad3d075db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:46 +0000

    Add package dslogger/0.0.2/dslogger-0.0.2.tar.gz

---
+++ packages/dslogger/dslogger-0.0.2.tar.gz/dslogger-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dslogger',
+    version='0.0.2',
+    author='ice_coder',
+    author_email='1943158197@qq.com',
+    description='用于记录日志信息',
+    python_requires='>=3.6',
+    install_requires=['logger'],
+    license='MIT'
+)
