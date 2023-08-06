ce00c430c580b05db682c27e2e7eae909909ab9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:40 +0000

    Add package SecretF/1.8.12/SecretF-1.8.12.tar.gz

---
+++ packages/SecretF/SecretF-1.8.12.tar.gz/SecretF-1.8.12/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SecretF",
+    version="1.08.12",
+    author="Fedorenko Timur",
+    author_email="tim.fedorenko@mail.ru",
+    description="Secret File",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HardHook/SecretF",
+    classifiers=[
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python :: 3.9",
+    ],
+    package_dir={"": "scr"},
+    packages=setuptools.find_packages(where="scr"),
+)
