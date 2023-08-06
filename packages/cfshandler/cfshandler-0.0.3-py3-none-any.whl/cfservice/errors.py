9265f5a60e5021232befe3754f404817cf266761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:14 +0000

    Add package cfshandler/0.0.3/cfshandler-0.0.3-py3-none-any.whl

---
+++ packages/cfshandler/cfshandler-0.0.3-py3-none-any.whl/cfservice/errors.py
@@ -0,0 +1,14 @@
+class OperationError(Exception):
+    pass
+
+
+class ParameterError(Exception):
+    pass
+
+
+class ServiceError(Exception):
+    pass
+
+
+class ServiceUnavailable(Exception):
+    pass
