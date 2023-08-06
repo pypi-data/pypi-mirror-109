bd712bf4614314893819887506b32d130caad93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:36 +0000

    Add package hapiplot/0.0.1b6/hapiplot-0.0.1b6.tar.gz

---
+++ packages/hapiplot/hapiplot-0.0.1b6.tar.gz/hapiplot-0.0.1b6/setup.py
@@ -0,0 +1,30 @@
+import sys
+from setuptools import setup, find_packages
+
+#install_requires = ["matplotlib", "hapiclient @ git+https://github.com/hapi-server/client-python#egg=hapiclient"]
+install_requires = ["matplotlib", "hapiclient>0.1.7"]
+
+if len(sys.argv) > 1 and sys.argv[1] == 'develop':
+    install_requires.append("deepdiff<3.3.0")
+    if sys.version_info < (3, 6):
+        install_requires.append("pytest<5.0.0")
+    else:
+        # Should not be needed, as per
+        # https://docs.pytest.org/en/stable/py27-py34-deprecation.html
+        # Perhaps old version of pip causes this?
+        install_requires.append("pytest")
+
+# version is modified by misc/version.py (executed from Makefile). Do not edit.
+setup(
+    name='hapiplot',
+    version='0.0.1b6',
+    author='Bob Weigel',
+    author_email='rweigel@gmu.edu',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/hapiplot/',
+    license='LICENSE.txt',
+    description='Plot data from HAPI server',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    install_requires=install_requires
+)
