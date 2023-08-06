2269e3282cae9d6d4fa1191eba0be24ed88dc74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:50 +0000

    Add package jupyter-env-whitelist/0.3.2/jupyter_env_whitelist-0.3.2-py3-none-any.whl

---
+++ packages/jupyter-env-whitelist/jupyter_env_whitelist-0.3.2-py3-none-any.whl/jupyter_env_whitelist/whitelist.py
@@ -0,0 +1,37 @@
+import os
+
+default_whitelist = [
+    "PWD",
+    "KERNEL_USERNAME",
+    "KERNEL_HUB_USERNAME",
+    "HOME",
+    "LANG",
+    "SHLVL",
+    "KERNEL_LANGUAGE",
+    "TERM",
+    "CLICOLOR",
+    "PAGER",
+    "NO_PROXY",
+    "HTTP_PROXY",
+    "HTTPS_PROXY",
+    "LD_LIBRARY_PATH",
+    "REQUESTS_CA_BUNDLE",
+    "CURL_CA_BUNDLE",
+    "PATH",
+    "PYENV_ROOT",
+]
+
+
+def filter_env() -> None:
+    whitelist_str = os.environ.get("JUPYTER_ENV_WHITELIST", None)
+    if whitelist_str is not None:
+        whitelist = [s.strip().upper() for s in whitelist_str.split(",") if s]
+    else:
+        whitelist = default_whitelist
+
+    whitelist_extra_str = os.environ.get("JUPYTER_ENV_EXTRA_WHITELIST", "")
+    whitelist.extend([s.strip().upper() for s in whitelist_extra_str.split(",") if s])
+
+    for name in os.environ:
+        if name.upper() not in whitelist:
+            del os.environ[name]
