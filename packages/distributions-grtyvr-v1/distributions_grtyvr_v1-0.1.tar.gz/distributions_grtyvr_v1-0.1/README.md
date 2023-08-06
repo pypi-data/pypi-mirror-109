29698c97430fbaaccfc9b39f1c4004c1bd714130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:38 +0000

    Add package distributions-grtyvr-v1/0.1/distributions_grtyvr_v1-0.1.tar.gz

---
+++ packages/distributions-grtyvr-v1/distributions_grtyvr_v1-0.1.tar.gz/distributions_grtyvr_v1-0.1/README.md
@@ -0,0 +1,3 @@
+Simple example library to support two statistical distributions:  Gausian and Binomial
+
+Provides methods to print, add and plot the given distributions.
---
+++ packages/distributions-grtyvr-v1/distributions_grtyvr_v1-0.1.tar.gz/distributions_grtyvr_v1-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_grtyvr_v1',
+      version='0.1',
+      description='Statistical distributions',
+      packages=['distributions_grtyvr_v1'],
+      zip_safe=False)
