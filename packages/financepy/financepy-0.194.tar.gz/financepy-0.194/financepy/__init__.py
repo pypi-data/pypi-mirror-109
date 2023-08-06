422d3ead3e0f67092b6326e0f9e1bafa1c6a4fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:06 +0000

    Add package financepy/0.194/financepy-0.194.tar.gz

---
+++ packages/financepy/financepy-0.194.tar.gz/financepy-0.194/financepy/__init__.py
@@ -0,0 +1,11 @@
+cr = "\n"
+
+s = "####################################################################" + cr
+s += "# FINANCEPY BETA Version " + str('0.194') + " - This build:  12 Jun 2021 at 18:20 #" + cr
+s += "#      This software is distributed FREE & WITHOUT ANY WARRANTY    #" + cr
+s += "# For info and disclaimer - https://github.com/domokane/FinancePy  #" + cr
+s += "#      Send any bug reports or comments to quant@financepy.com     #" + cr
+s += "####################################################################"
+s += cr
+
+print(s)
