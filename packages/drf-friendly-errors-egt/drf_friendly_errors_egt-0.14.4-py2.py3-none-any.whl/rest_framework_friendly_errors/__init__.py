ea72cf80b30c6e0107973994598624197500f210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:46 +0000

    Add package drf-friendly-errors-egt/0.14.4/drf_friendly_errors_egt-0.14.4-py2.py3-none-any.whl

---
+++ packages/drf-friendly-errors-egt/drf_friendly_errors_egt-0.14.4-py2.py3-none-any.whl/rest_framework_friendly_errors/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "drf-friendly-errors"
+__version__ = "0.14.4"
+__author__ = "Mrityunjay Saxena"
+__contact__ = "mrityunjay.saxena@egyanamtech.com"
+__license__ = "MIT"
+
+
+# Version synonym
+VERSION = __version__
