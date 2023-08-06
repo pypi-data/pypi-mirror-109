3358cc30c01720c1a364bb5bfeb06ae91ca7a63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:05 +0000

    Add package pmcli/0.1.7/pmcli-0.1.7.tar.gz

---
+++ packages/pmcli/pmcli-0.1.7.tar.gz/pmcli-0.1.7/pmcli.py
@@ -0,0 +1,54 @@
+import cryptocode
+import click
+SYMBOLS = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890.,/\;:'"|_-=+!@#$%^&*<>? '''
+MAX_KEY_SIZE = len(SYMBOLS)
+def getKey():
+    key = 0
+    while True:
+        key = 13
+        if (key >= 1 and key <= MAX_KEY_SIZE):
+            return key
+def getTranslatedMessage(mode,message , key):
+    if mode [0] == 'd':
+        key = -key
+    translated = ''
+            
+    for symbol in message:
+        symbolIndex = SYMBOLS.find(symbol)
+        if symbolIndex == -1:
+            translated += symbol
+        else:
+            symbolIndex += key
+            if symbolIndex >= len(SYMBOLS):
+                symbolIndex -= len(SYMBOLS)
+            elif symbolIndex < 0:
+                symbolIndex += len(SYMBOLS)
+                    
+            translated += SYMBOLS[symbolIndex]
+    return translated
+
+@click.group()
+@click.version_option('0.1.7')
+def main():
+    """PM: Encrypt, decrypt and save your passwords"""
+    pass
+
+@main.command('encrypt', help='"pmcli encrypt" asks for your directory and file to save, then saves your encrypted password')
+@click.argument('text', nargs=1)
+def encrypt(text):
+    cce = getTranslatedMessage('e', text, 13)   
+    msg = cryptocode.encrypt(cce, "abcd")
+    dire = input('Enter directory and file name: ')
+    file = open(dire, 'w')
+    file.write(msg)
+
+@main.command('decrypt', help='"pmcli decrypt" asks for your directory and file where the password was saved an decrypts it')
+def decrypt():
+    dire = input('Enter directory and file name: ')
+    a = open(dire)
+    b = a.read()
+    dmsg = cryptocode.decrypt(b, 'abcd')
+    ccd = getTranslatedMessage('d', dmsg, 13) 
+    print(ccd)
+if __name__ == "__main__":
+    main()
---
+++ packages/pmcli/pmcli-0.1.7.tar.gz/pmcli-0.1.7/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pmcli']
+install_requires = \
+['click>=8.0.1,<9.0.0', 'cryptocode>=0.1,<0.2']
+
+entry_points = \
+{'console_scripts': ['pm = pmcli:main']}
+
+setup_kwargs = {
+    'name': 'pmcli',
+    'version': '0.1.7',
+    'description': 'A Command Line Interface to encrypt, decrypt and save passwords dor windows',
+    'long_description': "# PMCLI\n\n## A password encrypter and decrypter\n\n## Install\n\n#### Windows\n\npip install pmcli \n\n### How to use\n\n#### Windows\nOpen powershell and type pm\n\nEncrypting \n\nOpen Powershell and type pm. \nThen type pm encrypt\nThen it will ask for a directory. \nGive the directory with the file.txt\nNote: You don't have to make a file it makes on itself\nIt will add the file with the encrypted file\n\nDecrypting\nOpen Powershell and type pm. \nThen type pm decrypt\nThen it will ask for a directory. \nGive the directory with the file.txt\nThen it will display the decrypted text\n\n\n### Softwares used\n\nVisual Studio Code - https://github.com/Microsoft/vscode\n\nPython 3.9.1\n\n",
+    'author': 'Avanindra Chakraborty',
+    'author_email': 'avanindra.d2.chakraborty@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/AvanindraC/PMCLI',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
