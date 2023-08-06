da2d462e103437448809e8e4e3498ff23165ce73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:44 +0000

    Add package scodec/0.0.1/scodec-0.0.1-py3-none-any.whl

---
+++ packages/scodec/scodec-0.0.1-py3-none-any.whl/scodec/__init__.py
@@ -0,0 +1,13 @@
+"""
+Spatial Codec __init___
+===================================
+
+Logger initialization for spatial codec
+
+Copyright © 2021 LEAP. All Rights Reserved.
+"""
+
+# execute logger config
+from scodec.logs.config import config
+
+config()
