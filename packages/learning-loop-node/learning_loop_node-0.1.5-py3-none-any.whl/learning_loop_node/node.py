af213b517404f35d168f9fa9c477e9e6e694510f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:12 +0000

    Add package learning-loop-node/0.1.5/learning_loop_node-0.1.5-py3-none-any.whl

---
+++ packages/learning-loop-node/learning_loop_node-0.1.5-py3-none-any.whl/learning_loop_node/node.py
@@ -0,0 +1,105 @@
+from learning_loop_node.status import Status, State
+from fastapi import FastAPI
+import socketio
+import asyncio
+import asyncio
+import os
+from icecream import ic
+
+
+WEBSOCKET_BASE_URL_DEFAULT = 'ws://backend'
+BASE_PROJECT = 'demo'
+BASE_ORGANIZATION = 'zauberzeug'
+
+
+class Node(FastAPI):
+    name: str
+    uuid: str
+
+    def __init__(self, name: str, uuid: str):
+        super().__init__()
+        self.ws_url = os.environ.get('WEBSOCKET_BASE_URL', WEBSOCKET_BASE_URL_DEFAULT)
+        self.username = os.environ.get('USERNAME', None)
+        self.password = os.environ.get('PASSWORD', None)
+        self.organization = os.environ.get('ORGANIZATION', BASE_ORGANIZATION)
+        self.project = os.environ.get('PROJECT', BASE_PROJECT)
+
+        self.name = name
+        self.uuid = uuid
+
+        self.sio = socketio.AsyncClient(
+            reconnection_delay=0,
+            request_timeout=0.5,
+            # logger=True, engineio_logger=True
+        )
+        self.reset()
+
+        @self.sio.on('connect')
+        async def on_connect():
+            ic('received "on_connect" from constructor event.')
+            self.reset()
+            await self.update_state(State.Idle)
+
+        @self.sio.on('disconnect')
+        async def on_disconnect():
+            ic('received "on_disconnect" from constructor event.')
+            await self.update_state(State.Offline)
+
+        self.register_lifecycle_events()
+
+    def register_lifecycle_events(self):
+        @self.on_event("startup")
+        async def startup():
+            print('received "startup" event', flush=True)
+            await self.connect()
+
+        @self.on_event("shutdown")
+        async def shutdown():
+            print('received "shutdown" event', flush=True)
+            await self.sio.disconnect()
+
+    def reset(self):
+        self.status = Status(id=self.uuid, name=self.name)
+
+    async def connect(self):
+        try:
+            await self.sio.disconnect()
+        except:
+            pass
+
+        print('connecting to Learning Loop', flush=True)
+        try:
+            await self.sio.connect(f"{self.ws_url}", auth={'username': self.username, 'password': self.password}, socketio_path="/ws/socket.io")
+            print('my sid is', self.sio.sid, flush=True)
+            print('connected to Learning Loop', flush=True)
+        except socketio.exceptions.ConnectionError as e:
+            ic(e)
+            if 'Already connected' in str(e):
+                print('we are already connected')
+            else:
+                await asyncio.sleep(0.2)
+                await self.connect()
+
+    async def update_state(self, state: State):
+        self.status.state = state
+        if self.status.state != State.Offline:
+            await self.send_status()
+
+    async def update_status(self, new_status: Status):
+        self.status.id = new_status.id
+        self.status.name = new_status.name
+        self.status.uptime = new_status.uptime
+        self.status.latest_error = new_status.latest_error
+
+        if self.status.state != State.Offline:
+            self.status.state = State.Idle
+        await self.send_status()
+
+    async def send_status(self):
+        raise Exception("Override this in subclass")
+
+    @staticmethod
+    def create_project_folder(organization: str, project: str) -> str:
+        project_folder = f'/data/{organization}/{project}'
+        os.makedirs(project_folder, exist_ok=True)
+        return project_folder
