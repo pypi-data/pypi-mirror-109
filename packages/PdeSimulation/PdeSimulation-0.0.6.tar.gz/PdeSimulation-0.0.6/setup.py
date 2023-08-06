1c82aa6d076e2802656cc37288bce271ced05780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:06 +0000

    Add package PdeSimulation/0.0.6/PdeSimulation-0.0.6.tar.gz

---
+++ packages/PdeSimulation/PdeSimulation-0.0.6.tar.gz/PdeSimulation-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.6' 
+DESCRIPTION = 'Numerical simulation of some PDEs using finite difference methods'
+LONG_DESCRIPTION = 'pic16b'
+
+# Setting up
+setup(
+       
+        name="PdeSimulation", 
+        version=VERSION,
+        author="Yun",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
