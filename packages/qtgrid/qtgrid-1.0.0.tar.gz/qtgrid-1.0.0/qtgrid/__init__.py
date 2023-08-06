54957542baf6b536615ddd17297abae5fcf03b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:36 +0000

    Add package qtgrid/1.0.0/qtgrid-1.0.0.tar.gz

---
+++ packages/qtgrid/qtgrid-1.0.0.tar.gz/qtgrid-1.0.0/qtgrid/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# package qtgrid
+
+"""
+The *qtgrid* package only contains the `qtgrid.qtgrid` module.
+"""
+
+from qtgrid.qtgrid import Grid
+
+__version__ = '1.0.0'
+"""Version"""
