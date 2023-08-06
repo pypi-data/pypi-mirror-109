d0460d9885528af997a75aa7bda8eae825ea39f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:56 +0000

    Add package rexam-item-editor/0.2.0.3/rexam_item_editor-0.2.0.3-py3-none-any.whl

---
+++ packages/rexam-item-editor/rexam_item_editor-0.2.0.3-py3-none-any.whl/rexam_item_editor/__init__.py
@@ -0,0 +1,9 @@
+"""RExam Item Editor
+
+(c) Oliver Lindemann
+"""
+
+__version__ = '0.2.0.3'
+__author__ = 'Oliver Lindemann'
+APPNAME = "RExam Item Editor"
+WEBSITE = "https://github.com/lindemann09/rexam-item-editor"
