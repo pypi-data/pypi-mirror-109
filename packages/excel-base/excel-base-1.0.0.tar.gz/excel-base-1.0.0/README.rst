7ac6b796ef804a77a5278ae0f53dacb523afbc41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:09 +0000

    Add package excel-base/1.0.0/excel-base-1.0.0.tar.gz

---
+++ packages/excel-base/excel-base-1.0.0.tar.gz/excel-base-1.0.0/README.rst
@@ -0,0 +1,21 @@
+==========
+excel-base
+==========
+
+Excel Base
+
+Installation
+============
+
+::
+
+    pip install excel-base
+
+
+Usage
+=====
+
+::
+
+    from excel_base import as_xls, as_csv
+
---
+++ packages/excel-base/excel-base-1.0.0.tar.gz/excel-base-1.0.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+version = '1.0.0'
+
+
+setup(
+    name='excel-base',
+    version=version,
+    keywords='Excel Base',
+    description='Excel Base',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/Brightcells/excel-base',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=['excel_base'],
+    py_modules=[],
+    install_requires=['xlwt', 'pytz', 'screen', 'TimeConvert>=1.5.3'],
+
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
