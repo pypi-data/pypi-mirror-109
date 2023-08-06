88a4b7c9e984dec7c7020c2706e7a49176904779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:09 +0000

    Add package MarkovTextGeneration/0.1.4/MarkovTextGeneration-0.1.4.tar.gz

---
+++ packages/MarkovTextGeneration/MarkovTextGeneration-0.1.4.tar.gz/MarkovTextGeneration-0.1.4/README.md
@@ -0,0 +1,2 @@
+# MarkovTextGeneration
+PIP package for easy text generation with Markov Chains and NLTK
---
+++ packages/MarkovTextGeneration/MarkovTextGeneration-0.1.4.tar.gz/MarkovTextGeneration-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='MarkovTextGeneration',
+    version='0.1.4',    
+    description='Generating hallucinated text with Markov Chains',
+    long_description="Read the wiki at https://github.com/Sam-Nielsen-Dot/MarkovTextGeneration/wiki",
+    url='https://github.com/Sam-Nielsen-Dot/MarkovTextGeneration',
+    author='Sam Nielsen',
+    author_email='lenssimane@gmail.com',
+    license='MIT',
+    packages=['MarkovTextGeneration'],
+    install_requires=['markovify>=0.9.0',
+                    'nltk>=3.5',
+                    'spacy>=3.0.6'                  
+                      ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
