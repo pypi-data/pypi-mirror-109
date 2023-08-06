07dffc9e00ffd542e129d854e0e7b4fba18d4b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:51 +0000

    Add package jasper-engine/0.0.1/jasper-engine-0.0.1.tar.gz

---
+++ packages/jasper-engine/jasper-engine-0.0.1.tar.gz/jasper-engine-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from os import path
+
+DIR = path.dirname(path.abspath(__file__))
+INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
+
+with open(path.join(DIR, 'README.md')) as f:
+    README = f.read()
+
+setup(
+    name='jasper-engine',
+    packages=['jasper'],
+    description="A android reverse tool",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    install_requires=INSTALL_PACKAGES,
+    version='0.0.1',
+    url='http://github.com/vforkliu/jasper',
+    author='forkliu',
+    author_email='vforkliu@gmail.com',
+    keywords=['android', 'unicorn', 'emulator'],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pytest-sugar'
+    ],
+    package_data={
+        # include json and pkl files
+        '': ['*.json', 'models/*.pkl', 'models/*.json'],
+    },
+    include_package_data=True,
+    python_requires='>=3'
+)
