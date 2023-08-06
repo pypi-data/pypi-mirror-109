b41fda681957ef3e7a9bfae89e7614b61b4bd186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:01 +0000

    Add package latex2mathml/3.40.1/latex2mathml-3.40.1.tar.gz

---
+++ packages/latex2mathml/latex2mathml-3.40.1.tar.gz/latex2mathml-3.40.1/latex2mathml/exceptions.py
@@ -0,0 +1,30 @@
+class NumeratorNotFoundError(Exception):
+    pass
+
+
+class DenominatorNotFoundError(Exception):
+    pass
+
+
+class ExtraLeftOrMissingRightError(Exception):
+    pass
+
+
+class MissingSuperScriptOrSubscriptError(Exception):
+    pass
+
+
+class DoubleSubscriptsError(Exception):
+    pass
+
+
+class DoubleSuperscriptsError(Exception):
+    pass
+
+
+class NoAvailableTokensError(Exception):
+    pass
+
+
+class InvalidStyleForGenfracError(Exception):
+    pass
