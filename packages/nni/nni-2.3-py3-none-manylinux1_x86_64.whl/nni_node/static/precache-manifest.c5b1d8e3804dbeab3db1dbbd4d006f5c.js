cf346898d81ca9636dfe051eb62bd743378d93c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:09 +0000

    Add package nni/2.3/nni-2.3-py3-none-manylinux1_x86_64.whl

---
+++ packages/nni/nni-2.3-py3-none-manylinux1_x86_64.whl/nni_node/static/precache-manifest.c5b1d8e3804dbeab3db1dbbd4d006f5c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b2caae008927219dc87f383b39f7c2",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "b9dd32453aaf49e66f9a169e5f2b01e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "742e3ac827e9d006ecbfe39f281fc8a6",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "6a2df17bc72a1f55b012",
+    "url": "/static/css/4.1a3b413d.chunk.css"
+  },
+  {
+    "revision": "91fd7fd3c10520c24d57",
+    "url": "/static/css/5.5c750b80.chunk.css"
+  },
+  {
+    "revision": "10bb26d5f16b39e92539",
+    "url": "/static/css/6.c4ee7a17.chunk.css"
+  },
+  {
+    "revision": "648dbff4ef1921bb76f4",
+    "url": "/static/css/7.f68164e3.chunk.css"
+  },
+  {
+    "revision": "13d8d3434c7316be287f",
+    "url": "/static/css/8.a1143352.chunk.css"
+  },
+  {
+    "revision": "d23238329f22e0acd5ec",
+    "url": "/static/css/main.4e5b1ef0.chunk.css"
+  },
+  {
+    "revision": "282ca44c8d384e4eeb50",
+    "url": "/static/js/0.d6e703a3.chunk.js"
+  },
+  {
+    "revision": "798f0cd9e4b703be68b1",
+    "url": "/static/js/1.580a7766.chunk.js"
+  },
+  {
+    "revision": "5ecf1d353037b6b2d247",
+    "url": "/static/js/10.512f1e2c.chunk.js"
+  },
+  {
+    "revision": "6b57db6f61f1f3212572",
+    "url": "/static/js/11.19a610a9.chunk.js"
+  },
+  {
+    "revision": "6a2df17bc72a1f55b012",
+    "url": "/static/js/4.677eb894.chunk.js"
+  },
+  {
+    "revision": "91fd7fd3c10520c24d57",
+    "url": "/static/js/5.5ee2553b.chunk.js"
+  },
+  {
+    "revision": "10bb26d5f16b39e92539",
+    "url": "/static/js/6.958772fa.chunk.js"
+  },
+  {
+    "revision": "648dbff4ef1921bb76f4",
+    "url": "/static/js/7.c17127ab.chunk.js"
+  },
+  {
+    "revision": "13d8d3434c7316be287f",
+    "url": "/static/js/8.3c7ad669.chunk.js"
+  },
+  {
+    "revision": "bffb159b873e37d1cec1",
+    "url": "/static/js/9.9a8c0b24.chunk.js"
+  },
+  {
+    "revision": "d23238329f22e0acd5ec",
+    "url": "/static/js/main.d133d65a.chunk.js"
+  },
+  {
+    "revision": "c2dbc8bd06441e26b494",
+    "url": "/static/js/runtime~main.8c85195c.js"
+  },
+  {
+    "revision": "35bb8d560db5205616671eab8c4d0303",
+    "url": "/static/media/codicon.35bb8d56.ttf"
+  },
+  {
+    "revision": "e42a4d82a3e57fa49b196233949bc686",
+    "url": "/static/media/loading.e42a4d82.gif"
+  }
+]);
---
+++ packages/nni/nni-2.3-py3-none-manylinux1_x86_64.whl/nni_node/training_service/kubernetes/kubernetesConfig.js
@@ -0,0 +1,125 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const errors_1 = require("../../common/errors");
+class KubernetesClusterConfig {
+    constructor(apiVersion, storage, namespace) {
+        this.storage = storage;
+        this.apiVersion = apiVersion;
+        this.namespace = namespace;
+    }
+    get storageType() {
+        throw new errors_1.MethodNotImplementedError();
+    }
+}
+exports.KubernetesClusterConfig = KubernetesClusterConfig;
+class StorageConfig {
+    constructor(storage) {
+        this.storage = storage;
+    }
+}
+exports.StorageConfig = StorageConfig;
+class KubernetesClusterConfigNFS extends KubernetesClusterConfig {
+    constructor(apiVersion, nfs, storage, namespace) {
+        super(apiVersion, storage, namespace);
+        this.nfs = nfs;
+    }
+    get storageType() {
+        return 'nfs';
+    }
+    static getInstance(jsonObject) {
+        const kubernetesClusterConfigObjectNFS = jsonObject;
+        return new KubernetesClusterConfigNFS(kubernetesClusterConfigObjectNFS.apiVersion, kubernetesClusterConfigObjectNFS.nfs, kubernetesClusterConfigObjectNFS.storage, kubernetesClusterConfigObjectNFS.namespace);
+    }
+}
+exports.KubernetesClusterConfigNFS = KubernetesClusterConfigNFS;
+class KubernetesClusterConfigAzure extends KubernetesClusterConfig {
+    constructor(apiVersion, keyVault, azureStorage, storage, uploadRetryCount, namespace) {
+        super(apiVersion, storage, namespace);
+        this.keyVault = keyVault;
+        this.azureStorage = azureStorage;
+        this.uploadRetryCount = uploadRetryCount;
+    }
+    get storageType() {
+        return 'azureStorage';
+    }
+    static getInstance(jsonObject) {
+        const kubernetesClusterConfigObjectAzure = jsonObject;
+        return new KubernetesClusterConfigAzure(kubernetesClusterConfigObjectAzure.apiVersion, kubernetesClusterConfigObjectAzure.keyVault, kubernetesClusterConfigObjectAzure.azureStorage, kubernetesClusterConfigObjectAzure.storage, kubernetesClusterConfigObjectAzure.uploadRetryCount, kubernetesClusterConfigObjectAzure.namespace);
+    }
+}
+exports.KubernetesClusterConfigAzure = KubernetesClusterConfigAzure;
+class KubernetesClusterConfigPVC extends KubernetesClusterConfig {
+    constructor(apiVersion, pvc, storage, namespace) {
+        super(apiVersion, storage, namespace);
+        this.pvc = pvc;
+    }
+    get storageType() {
+        return 'pvc';
+    }
+    static getInstance(jsonObject) {
+        const kubernetesClusterConfigObjectPVC = jsonObject;
+        return new KubernetesClusterConfigPVC(kubernetesClusterConfigObjectPVC.apiVersion, kubernetesClusterConfigObjectPVC.pvc, kubernetesClusterConfigObjectPVC.storage, kubernetesClusterConfigObjectPVC.namespace);
+    }
+}
+exports.KubernetesClusterConfigPVC = KubernetesClusterConfigPVC;
+class KubernetesClusterConfigFactory {
+    static generateKubernetesClusterConfig(jsonObject) {
+        const storageConfig = jsonObject;
+        switch (storageConfig.storage) {
+            case 'azureStorage':
+                return KubernetesClusterConfigAzure.getInstance(jsonObject);
+            case 'pvc':
+                return KubernetesClusterConfigPVC.getInstance(jsonObject);
+            case 'nfs':
+            case undefined:
+                return KubernetesClusterConfigNFS.getInstance(jsonObject);
+            default:
+                throw new Error(`Invalid json object ${jsonObject}`);
+        }
+    }
+}
+exports.KubernetesClusterConfigFactory = KubernetesClusterConfigFactory;
+class NFSConfig {
+    constructor(server, path) {
+        this.server = server;
+        this.path = path;
+    }
+}
+exports.NFSConfig = NFSConfig;
+class PVCConfig {
+    constructor(path) {
+        this.path = path;
+    }
+}
+exports.PVCConfig = PVCConfig;
+class KeyVaultConfig {
+    constructor(vaultName, name) {
+        this.vaultName = vaultName;
+        this.name = name;
+    }
+}
+exports.KeyVaultConfig = KeyVaultConfig;
+class AzureStorage {
+    constructor(azureShare, accountName) {
+        this.azureShare = azureShare;
+        this.accountName = accountName;
+    }
+}
+exports.AzureStorage = AzureStorage;
+class KubernetesTrialConfigTemplate {
+    constructor(command, gpuNum, cpuNum, memoryMB, image, privateRegistryAuthPath) {
+        this.command = command;
+        this.gpuNum = gpuNum;
+        this.cpuNum = cpuNum;
+        this.memoryMB = memoryMB;
+        this.image = image;
+        this.privateRegistryAuthPath = privateRegistryAuthPath;
+    }
+}
+exports.KubernetesTrialConfigTemplate = KubernetesTrialConfigTemplate;
+class KubernetesTrialConfig {
+    constructor(codeDir) {
+        this.codeDir = codeDir;
+    }
+}
+exports.KubernetesTrialConfig = KubernetesTrialConfig;
