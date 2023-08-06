7b86f5f996d63513bbd793a0c27c9a7cbc5845b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:00 +0000

    Add package drf-ebs-utils/0.0.43/drf-ebs-utils-0.0.43.tar.gz

---
+++ packages/drf-ebs-utils/drf-ebs-utils-0.0.43.tar.gz/drf-ebs-utils-0.0.43/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drf-ebs-utils",
+    version="0.0.43",
+    author="Dorin Musteata",
+    author_email="dorin.musteata@ebs-integrator.com",
+    description="drf-ebs-utils is a package that offers service classes for notification-service, attachment-service, sso-service.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebs-integrator/drf-ebs-utils",
+    project_urls={
+        "Bug Tracker": "https://github.com/ebs-integrator/drf-ebs-utils/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
