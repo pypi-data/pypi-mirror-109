809fe96f9437d08fca1f1f7fb5b1f69eb7446ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:05:12 +0000

    Add package my-pdf2text/1.0.0/my_pdf2text-1.0.0.tar.gz

---
+++ packages/my-pdf2text/my_pdf2text-1.0.0.tar.gz/my_pdf2text-1.0.0/my_pdf2text/pdf2text.py
@@ -0,0 +1,17 @@
+""" This module converts PDF file t0 Text """
+
+
+class Converter:
+    """ A simple converted to convert PDF to text"""
+    def convert(self, path):
+        """ Convert the pdf to text format
+
+        :parameter
+        Path: PDF file path
+
+        :returns
+        str: The content of the PDF file as text string
+
+        """
+        print("my_pdf2text")
+
