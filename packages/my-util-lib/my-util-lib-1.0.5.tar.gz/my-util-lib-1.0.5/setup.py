d4cf05023c90bd3c0aba4d135d7380f5f5911297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:27 +0000

    Add package my-util-lib/1.0.5/my-util-lib-1.0.5.tar.gz

---
+++ packages/my-util-lib/my-util-lib-1.0.5.tar.gz/my-util-lib-1.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+"""
+reference:
+https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f
+"""
+setup(
+    name='my-util-lib',  # name can use '-', but src/my_util_lib should use '_' instead, otherwise cannot be imported
+    packages=find_packages(where='src', include=['my-util-lib'], exclude=[]),
+    package_dir={
+        '': 'src',
+    },
+    version='1.0.5',
+    description='My util library in Python',
+    long_description='My util library in Python (long description)',
+    author='Me',
+    author_email="me@test.com",
+    url="http://localhost",
+    license='MIT',
+    install_requires=[
+        'numpy>=1.19'
+    ],  # any dependencies that this library needs go here
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest==6.2.4'],
+    # test_suite='tests',
+)
