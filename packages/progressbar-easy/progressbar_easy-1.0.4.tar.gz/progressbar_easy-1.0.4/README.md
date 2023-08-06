abb89c29762f0833ea8af21c2600c8717670ecfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:52 +0000

    Add package progressbar-easy/1.0.4/progressbar_easy-1.0.4.tar.gz

---
+++ packages/progressbar-easy/progressbar_easy-1.0.4.tar.gz/progressbar_easy-1.0.4/README.md
@@ -0,0 +1,24 @@
+# Progress Bar
+This is a simple progress bar that allows for easy use to display progress.
+
+# FEATURES
+- Timer to estimate time remaining
+- Timer adjusts to rates
+- Choice of length of bar
+- Choice of character bar is made of
+- Option for item count or percentage completed (or both)
+
+# Usage
+Imported with `from progressbar import ProgressBar`
+
+Initialize object with `bar = ProgressBar(number of iterations)`
+
+Use in loop. Inside loop at the end, put `bar.update()` or `bar += 1`.
+Prefer `bar.update()`
+
+## Or
+Use as an iterator object
+```python
+for i in ProgressBar(range(10)):
+    ...
+```
---
+++ packages/progressbar-easy/progressbar_easy-1.0.4.tar.gz/progressbar_easy-1.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='progressbar_easy',
+    version='1.0.4',
+    description='A simple progressbar to track progress with built in-timer that estimates remaining time',
+    #long_description=open('README.md', 'r').read() + '\n\n' + open('CHANGELOG.md', 'r').read(),
+    url='',
+    author='Braxton Brown',
+    author_email='braxton.brown@outlook.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='progressbar, progress bar, progressbar_easy, easy progressbar, easy progress bar',
+    packages=find_packages(),
+    install_requires=['']
+)
