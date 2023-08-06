a4a75936c874ca24ebd69299ecf848a6a9f8bc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:23 +0000

    Add package coolors/0.1.3/coolors-0.1.3.tar.gz

---
+++ packages/coolors/coolors-0.1.3.tar.gz/coolors-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['coolors']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyppeteer>=0.2.5,<0.3.0']
+
+setup_kwargs = {
+    'name': 'coolors',
+    'version': '0.1.3',
+    'description': 'generates 5 random colors by scraping coolors.co',
+    'long_description': 'A simple package to gen a color scheme by scraping coolors.co\n\nto use import coolor function and run\n',
+    'author': 'Arjun Gandhi',
+    'author_email': 'account@arjugandhi.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/coolors/coolors-0.1.3.tar.gz/coolors-0.1.3/src/coolors/main.py
@@ -0,0 +1,21 @@
+import asyncio
+from pyppeteer import launch
+import json
+
+def generate():
+    loop = asyncio.get_event_loop()
+    r = loop.run_until_complete(_main())
+    result = r.split('/')[-1]
+    results = result.split('-')
+
+    return results
+
+async def _main():
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+    await page.goto('https://coolors.co/generate',{'waitUntil' : 'networkidle2'})
+    js = "() => { return window.location.href }"
+    result = await page.evaluate(js)
+    await browser.close()
+
+    return result
