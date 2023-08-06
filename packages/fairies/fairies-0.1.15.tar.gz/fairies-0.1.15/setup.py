1e49671842edec8b70413beb96bf00fc56e6e325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:03:24 +0000

    Add package fairies/0.1.15/fairies-0.1.15.tar.gz

---
+++ packages/fairies/fairies-0.1.15.tar.gz/fairies-0.1.15/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fairies',
+    version='0.1.15',
+    description=(
+        'utils for huahua and xingxing and shengsheng'
+    ),
+    author='liyuhao',
+    author_email='1241225413@qq.com',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    url='https://github.com/hgliyuhao/flower',
+    install_requires=[
+        'pdfminer3k',
+        'requests',
+        'xlrd==1.2.0',
+        'jieba',
+        'xlwt',
+        'xlutils',
+        'beautifulsoup4',
+        'matplotlib'
+    ],
+)
