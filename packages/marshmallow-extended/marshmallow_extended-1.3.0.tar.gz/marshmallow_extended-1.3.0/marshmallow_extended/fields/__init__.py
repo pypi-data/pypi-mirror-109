b6353951f7531d3f62cd0670b61d78ef3744a148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:50 +0000

    Add package marshmallow-extended/1.3.0/marshmallow_extended-1.3.0.tar.gz

---
+++ packages/marshmallow-extended/marshmallow_extended-1.3.0.tar.gz/marshmallow_extended-1.3.0/marshmallow_extended/fields/__init__.py
@@ -0,0 +1,10 @@
+from marshmallow.fields import *
+from .instance import Instance
+from .active import Active
+from .email import Email
+
+
+__all__ = ["Field", "Raw", "Nested", "Mapping", "Dict", "List", "Tuple", "String", "UUID", "Number", "Integer",
+           "Decimal", "Boolean", "Float", "DateTime", "NaiveDateTime", "AwareDateTime", "Time", "Date", "TimeDelta",
+           "Url", "URL", "Email", "IP", "IPv4", "IPv6", "Method", "Function", "Str", "Bool", "Int", "Constant",
+           "Pluck", "Instance", "Active", "Email"]
