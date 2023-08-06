defa04f94292e9faf08eaa5c9b71ea9c450509ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:55 +0000

    Add package Unalix/0.7/Unalix-0.7-py3-none-any.whl

---
+++ packages/Unalix/Unalix-0.7-py3-none-any.whl/unalix/types/__init__.py
@@ -0,0 +1,29 @@
+from .objects import Dict, List
+from .patterns import Pattern, Patterns
+from .rulesets import Ruleset, Rulesets
+from .body_redirects import BodyRedirect, BodyRedirects
+from .domains import Domains
+from .urls import URL, URL_TYPES
+
+
+__all__ = [
+    "Dict",
+    "List",
+    "Pattern",
+    "Patterns",
+    "Ruleset",
+    "Rulesets",
+    "BodyRedirect",
+    "BodyRedirects",
+    "Domains",
+    "URL",
+    "URL_TYPES"
+]
+
+__locals = locals()
+
+for __name in __all__:
+    try:
+        setattr(__locals[__name], "__module__", "unalix.types")
+    except AttributeError:
+        pass
