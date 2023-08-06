dccb783f36dd8a7472baf5997b79dae822014f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:10 +0000

    Add package pnrw/0.1.0/pnrw-0.1.0.tar.gz

---
+++ packages/pnrw/pnrw-0.1.0.tar.gz/pnrw-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pnrw',
+    version='0.1.0',
+    description='Python Nanocurrency RPC Wrapper',
+    author='MiranDaniel',
+    author_email='mirandaniel@protonmail.com',
+    license='License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    packages=['pnrw'],
+    install_requires=[],
+
+    classifiers=[
+        'Environment :: Console',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
