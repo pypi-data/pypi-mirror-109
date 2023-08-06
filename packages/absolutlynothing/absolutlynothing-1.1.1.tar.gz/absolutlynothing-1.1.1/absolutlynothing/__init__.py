0c6fa6f913bfeaa664797c42b61c44714fabf43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:34 +0000

    Add package absolutlynothing/1.1.1/absolutlynothing-1.1.1.tar.gz

---
+++ packages/absolutlynothing/absolutlynothing-1.1.1.tar.gz/absolutlynothing-1.1.1/absolutlynothing/__init__.py
@@ -0,0 +1,13 @@
+from tkinter import*
+
+print("I TOLD YOU THIS IS NOTHING!")
+
+def Nothing():
+    window = Tk()
+    window.title("Nothing")
+    canvas = Canvas(bg="red")
+    canvas.pack()
+    lable = Label(text="I TOLD YOU THIS IS NOTHING!")
+    lable.pack()
+    window.mainloop()
+Nothing()
