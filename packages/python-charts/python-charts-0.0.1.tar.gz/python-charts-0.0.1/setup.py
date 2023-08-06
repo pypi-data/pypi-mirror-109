02eb4e1c61b951fed0d14362a22dc43d9d00af64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:56 +0000

    Add package python-charts/0.0.1/python-charts-0.0.1.tar.gz

---
+++ packages/python-charts/python-charts-0.0.1.tar.gz/python-charts-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+        name = 'python-charts',
+        version = '0.0.1',
+        py_modules = ['python-charts'],
+        author = 'jack.253png',
+        author_email= 'guoxiuchen20170402@163.com',
+        url = 'https://github.com',
+        descriptioin = 'Pycharts',
+        packages=['python-charts'],
+        install_requires=[
+        'pyecharts'])
