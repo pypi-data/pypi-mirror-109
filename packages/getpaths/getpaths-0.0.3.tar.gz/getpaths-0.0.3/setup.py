f40e8747608945e3199a3b7619d5c9af782e1473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:51 +0000

    Add package getpaths/0.0.3/getpaths-0.0.3.tar.gz

---
+++ packages/getpaths/getpaths-0.0.3.tar.gz/getpaths-0.0.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3' 
+DESCRIPTION = 'Pathing Library'
+
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+
+# Setting up
+setup(
+        name="getpaths", 
+        version=VERSION,
+        author="Jv Kyle Eclarin",
+        author_email="<jvykleeclarin@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=[],
+        
+        keywords=['python', 'pathing', 'path'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
