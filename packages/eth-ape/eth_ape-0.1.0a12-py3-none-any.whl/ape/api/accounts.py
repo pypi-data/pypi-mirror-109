e1b0eb83fec7063d6ce03efd899e91bf412f4615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:06 +0000

    Add package eth-ape/0.1.0a12/eth_ape-0.1.0a12-py3-none-any.whl

---
+++ packages/eth-ape/eth_ape-0.1.0a12-py3-none-any.whl/ape/api/accounts.py
@@ -0,0 +1,142 @@
+from pathlib import Path
+from typing import TYPE_CHECKING, Iterator, Optional, Type
+
+from eth_account.datastructures import SignedMessage  # type: ignore
+from eth_account.datastructures import SignedTransaction
+from eth_account.messages import SignableMessage  # type: ignore
+
+from .base import abstractdataclass, abstractmethod
+
+if TYPE_CHECKING:
+    from ape.managers.networks import NetworkManager
+
+
+@abstractdataclass
+class AddressAPI:
+    network_manager: Optional["NetworkManager"] = None
+
+    @property
+    def _provider(self):
+        if not self.network_manager:
+            raise Exception("Not wired correctly")
+
+        if not self.network_manager.active_provider:
+            raise Exception("Not connected to any network!")
+
+        return self.network_manager.active_provider
+
+    @property
+    @abstractmethod
+    def address(self) -> str:
+        ...
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.address}>"
+
+    def __str__(self) -> str:
+        return self.address
+
+    @property
+    def nonce(self) -> int:
+        return self._provider.get_nonce(self.address)
+
+    @property
+    def balance(self) -> int:
+        return self._provider.get_balance(self.address)
+
+    @property
+    def code(self) -> bytes:
+        # TODO: Explore caching this (based on `self.provider.network` and examining code)
+        return self._provider.get_code(self.address)
+
+    @property
+    def codesize(self) -> int:
+        return len(self.code)
+
+    @property
+    def is_contract(self) -> bool:
+        return len(self.code) > 0
+
+
+# NOTE: AddressAPI is a dataclass already
+class AccountAPI(AddressAPI):
+    container: "AccountContainerAPI"
+
+    @property
+    def alias(self) -> Optional[str]:
+        """
+        Override with whatever alias might want to use, if applicable
+        """
+        return None
+
+    @abstractmethod
+    def sign_message(self, msg: SignableMessage) -> Optional[SignedMessage]:
+        ...
+
+    @abstractmethod
+    def sign_transaction(self, txn: dict) -> Optional[SignedTransaction]:
+        ...
+
+
+@abstractdataclass
+class AccountContainerAPI:
+    data_folder: Path
+    account_type: Type[AccountAPI]
+
+    @property
+    @abstractmethod
+    def aliases(self) -> Iterator[str]:
+        ...
+
+    @abstractmethod
+    def __len__(self) -> int:
+        ...
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[AccountAPI]:
+        ...
+
+    def __getitem__(self, address: str) -> AccountAPI:
+        for account in self.__iter__():
+            if account.address == address:
+                return account
+
+        raise IndexError(f"No local account {address}.")
+
+    def append(self, account: AccountAPI):
+        if not isinstance(account, self.account_type):
+            raise Exception("Not the right type for this container")
+
+        if account.address in self:
+            raise Exception("Account already in container")
+
+        if account.alias and account.alias in self.aliases:
+            raise Exception("Alias already in use")
+
+        self.__setitem__(account.address, account)
+
+    def __setitem__(self, address: str, account: AccountAPI):
+        raise NotImplementedError("Must define this method to use `container.append(acct)`")
+
+    def remove(self, account: AccountAPI):
+        if not isinstance(account, self.account_type):
+            raise Exception("Not the right type for this container")
+
+        if account.address not in self:
+            raise Exception("Account not in container")
+
+        if account.alias and account.alias in self.aliases:
+            raise Exception("Alias already in use")
+
+        self.__delitem__(account.address)
+
+    def __delitem__(self, address: str):
+        raise NotImplementedError("Must define this method to use `container.remove(acct)`")
+
+    def __contains__(self, address: str) -> bool:
+        try:
+            self.__getitem__(address)
+            return True
+
+        except IndexError:
+            return False
---
+++ packages/eth-ape/eth_ape-0.1.0a12-py3-none-any.whl/ape/api/providers.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+from . import networks
+from .base import abstractdataclass, abstractmethod
+
+
+@abstractdataclass
+class ProviderAPI:
+    """
+    A Provider must work with a particular Network in a particular Ecosystem
+    """
+
+    name: str  # Plugin name
+    network: networks.NetworkAPI
+    config: dict
+    data_folder: Path
+    request_header: str
+
+    @abstractmethod
+    def connect(self):
+        ...
+
+    @abstractmethod
+    def disconnect(self):
+        ...
+
+    @abstractmethod
+    def update_settings(self, new_settings: dict):
+        ...
+
+    @abstractmethod
+    def get_balance(self, address: str) -> int:
+        ...
+
+    @abstractmethod
+    def get_code(self, address: str) -> bytes:
+        ...
+
+    @abstractmethod
+    def get_nonce(self, address: str) -> int:
+        ...
+
+    @abstractmethod
+    def transfer_cost(self, address: str) -> int:
+        ...
+
+    @property
+    @abstractmethod
+    def gas_price(self):
+        ...
+
+    @abstractmethod
+    def send_transaction(self, data: bytes) -> bytes:
+        ...
