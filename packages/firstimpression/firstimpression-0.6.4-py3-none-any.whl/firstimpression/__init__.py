c398043e402af458a2fce2016d43c6075453a43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:56 +0000

    Add package firstimpression/0.6.4/firstimpression-0.6.4-py3-none-any.whl

---
+++ packages/firstimpression/firstimpression-0.6.4-py3-none-any.whl/firstimpression/__init__.py
@@ -0,0 +1,13 @@
+from firstimpression import api
+from firstimpression import file
+from firstimpression import rss
+from firstimpression import scala
+from firstimpression import text
+from firstimpression import time
+from firstimpression import xml
+
+from firstimpression import constants
+
+from os.path import join
+
+file.create_directories([join(constants.LOCAL_INTEGRATED_FOLDER, api_name) for api_name in constants.APIS])
