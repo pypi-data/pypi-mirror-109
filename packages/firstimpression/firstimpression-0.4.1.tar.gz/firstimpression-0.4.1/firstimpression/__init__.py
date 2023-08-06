941da231c45fa7df6fb500fcf8ae537cdabb477c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:32 +0000

    Add package firstimpression/0.4.1/firstimpression-0.4.1.tar.gz

---
+++ packages/firstimpression/firstimpression-0.4.1.tar.gz/firstimpression-0.4.1/firstimpression/__init__.py
@@ -0,0 +1,11 @@
+from .file import *
+from .html import *
+from .nu_nl import *
+from .rss import *
+from .text import *
+from .bcc import *
+from .scalascript import *
+from .ns import *
+from .traffic import *
+
+create_directories([local_integrated_folder])
