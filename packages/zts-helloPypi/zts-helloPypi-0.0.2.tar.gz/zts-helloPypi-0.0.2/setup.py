ff5beb5647de25e9ca49338acb333fb20c2fbe45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:04:49 +0000

    Add package zts-helloPypi/0.0.2/zts-helloPypi-0.0.2.tar.gz

---
+++ packages/zts-helloPypi/zts-helloPypi-0.0.2.tar.gz/zts-helloPypi-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zts-helloPypi',
+    version='0.0.2',
+    description='a test for packaging',
+    license='MIT License',
+    url='https://github.com/',
+    author='zts2125',
+    author_email='786566148@qq.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)
