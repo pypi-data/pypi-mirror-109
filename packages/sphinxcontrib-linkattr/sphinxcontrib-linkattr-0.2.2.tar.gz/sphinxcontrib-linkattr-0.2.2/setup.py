a323951e8b7aa10f7bc2dc8f69953165a9beafc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:29 +0000

    Add package sphinxcontrib-linkattr/0.2.2/sphinxcontrib-linkattr-0.2.2.tar.gz

---
+++ packages/sphinxcontrib-linkattr/sphinxcontrib-linkattr-0.2.2.tar.gz/sphinxcontrib-linkattr-0.2.2/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+try:
+    with open("README.md") as f:
+        readme_md = f.read()
+except IOError:
+    readme_md = ""
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="sphinxcontrib-linkattr",
+    version="0.2.2",
+    url="https://github.com/tatsushi-ikeda/sphinxcontrib-linkattr",
+    download_url="http://pypi.python.org/pypi/sphinxcontrib-linkattr",
+    license="MIT",
+    author="tatsushi-ikeda",
+    author_email="ikeda.tatsushi.37u@kyoto-u.jp",
+    maintainer="tatsushi-ikeda",
+    maintainer_email="ikeda.tatsushi.37u@kyoto-u.jp",
+    description='Sphinx "linkattr" extension',
+    long_description=readme_md,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Utilities",
+    ],
+    platforms="any",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=_requires_from_file("requirements.txt"),
+    namespace_packages=["sphinxcontrib"],
+)
