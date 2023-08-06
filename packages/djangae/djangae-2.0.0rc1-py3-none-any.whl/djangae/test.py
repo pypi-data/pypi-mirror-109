ffa177898a22d70c64fa5d3a4686c39d08569598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:18 +0000

    Add package djangae/2.0.0rc1/djangae-2.0.0rc1-py3-none-any.whl

---
+++ packages/djangae/djangae-2.0.0rc1-py3-none-any.whl/djangae/test.py
@@ -0,0 +1,43 @@
+
+from django.test.runner import DiscoverRunner
+from djangae.tasks.test import (
+    TaskFailedBehaviour,
+    TaskFailedError,
+    TestCaseMixin,
+)
+from django import test
+from django.core.cache import cache
+from djangae.sandbox import start_emulators, stop_emulators, wipe_cloud_storage
+
+TaskFailedError = TaskFailedError
+TaskFailedBehaviour = TaskFailedBehaviour
+
+
+class TestEnvironmentMixin(object):
+    def setUp(self):
+        cache.clear()
+        super().setUp()
+
+
+class CloudStorageTestCaseMixin(object):
+    def setUp(self):
+        wipe_cloud_storage()
+        super().setUp()
+
+
+class TestCase(TestEnvironmentMixin, TestCaseMixin, test.TestCase):
+    pass
+
+
+class TransactionTestCase(TestEnvironmentMixin, TestCaseMixin, test.TransactionTestCase):
+    pass
+
+
+class AppEngineDiscoverRunner(DiscoverRunner):
+    def setup_test_environment(self, **kwargs):
+        start_emulators(persist_data=False)
+        super().setup_test_environment(**kwargs)
+
+    def teardown_test_environment(self, **kwargs):
+        super().teardown_test_environment(**kwargs)
+        stop_emulators()
