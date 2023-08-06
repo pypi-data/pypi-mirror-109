f0290d7e382753c10fcda816ccb258199eff6c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:02:40 +0000

    Add package binary-fractions/0.0.3/binary_fractions-0.0.3.tar.gz

---
+++ packages/binary-fractions/binary_fractions-0.0.3.tar.gz/binary_fractions-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+classifiers = []
+
+setup(
+    name="binary_fractions",
+    version="0.0.3",
+    description="A package to handle binary fractions",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jonny-exe/binary-fractions",
+    author="Jonny-exe",
+    author_email="",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="binary fractions binary-fractions",
+    install_requires=[""],
+    packages=["binary_fractions"]
+)
