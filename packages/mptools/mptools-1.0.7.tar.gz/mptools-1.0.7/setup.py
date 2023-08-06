eb8adb67b7f3849cefa961f765a78e02d65d8ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:17 +0000

    Add package mptools/1.0.7/mptools-1.0.7.tar.gz

---
+++ packages/mptools/mptools-1.0.7.tar.gz/mptools-1.0.7/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mptools",
+    version="1.0.7",
+    author="Manuele Pesenti",
+    author_email="manuele@inventati.org",
+    description="A very custom collection of development shared libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/manuelep/mptools",
+    project_urls={
+        "Bug Tracker": "https://github.com/manuelep/mptools/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "py4web",
+        "pandas",
+        "matplotlib",
+        "numpy",
+        "diskcache",
+        "pytopojson"
+    ]
+)
