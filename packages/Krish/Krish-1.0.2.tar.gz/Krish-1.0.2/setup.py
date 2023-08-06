fc35fa2d18d2487f62e2e6e156a6e5e7db36160e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:29 +0000

    Add package Krish/1.0.2/Krish-1.0.2.tar.gz

---
+++ packages/Krish/Krish-1.0.2.tar.gz/Krish-1.0.2/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+#README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Krish",
+    version="1.0.2",
+    description="Meet A new A.I. - Krish",
+    long_description_content_type="text/markdown",
+    author="Mausam Kaushal",
+    author_email="tbot1738@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["v1"],
+    include_package_data=True,
+    install_requires=["pyttsx3","speechrecognition","wikipedia",
+    "youtube-search-python","pafy"],
+    entry_points={
+        "console_scripts": [
+            "Krish=v1.__main__:main",
+        ]
+    },
+)
