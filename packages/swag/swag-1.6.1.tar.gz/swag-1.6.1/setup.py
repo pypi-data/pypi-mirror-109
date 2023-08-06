b40003cd8e6f81f4b6973ff8b901f1c34de0a4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package swag/1.6.1/swag-1.6.1.tar.gz

---
+++ packages/swag/swag-1.6.1.tar.gz/swag-1.6.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['swag']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'swag',
+    'version': '1.6.1',
+    'description': 'Swag up your shell output with escape code magic!',
+    'long_description': None,
+    'author': '4thel00z',
+    'author_email': '4thel00z@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
