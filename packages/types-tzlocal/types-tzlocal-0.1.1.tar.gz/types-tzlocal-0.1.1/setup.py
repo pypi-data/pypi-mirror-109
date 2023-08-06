5c0d9b9f709dc6ed590e9b7dfa7fd293572abe75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:53 +0000

    Add package types-tzlocal/0.1.1/types-tzlocal-0.1.1.tar.gz

---
+++ packages/types-tzlocal/types-tzlocal-0.1.1.tar.gz/types-tzlocal-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-tzlocal"
+description = "Typing stubs for tzlocal"
+long_description = '''
+## Typing stubs for tzlocal
+
+This is an auto-generated PEP 561 type stub package for `tzlocal` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `tzlocal`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/tzlocal. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `64f463172b27d7321461890dd3edf416a103ace7`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.1.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=['types-pytz'],
+      packages=['tzlocal-stubs'],
+      package_data={'tzlocal-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
