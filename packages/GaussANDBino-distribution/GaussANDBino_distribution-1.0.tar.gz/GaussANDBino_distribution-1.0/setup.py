c55b2c1066a139f62f488f58d03c4c64199c56fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:09 +0000

    Add package GaussANDBino-distribution/1.0/GaussANDBino_distribution-1.0.tar.gz

---
+++ packages/GaussANDBino-distribution/GaussANDBino_distribution-1.0.tar.gz/GaussANDBino_distribution-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GaussANDBino_distribution',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['GaussANDBino_distribution'],
+      zip_safe=False)
