db3c5871c8f2846bef7726370ca73f30a0e36298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:14 +0000

    Add package evspy/0.1.2/evspy-0.1.2.tar.gz

---
+++ packages/evspy/evspy-0.1.2.tar.gz/evspy-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+# load the README file and use it as the long_description for PyPI
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+setup(name='evspy',
+      version='0.1.2',
+      description='Modelling of consolidation with creep and swelling of soils',
+      long_description=readme(),
+      url='http://github.com/thomasvergote/evspy',
+      author='Thomas Vergote',
+      author_email='thomas@inferensics.be',
+      license='GPL v3',
+      packages=['evspy'],
+      keywords=['engineering', 'geotechnical'],
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
