a23e2ea1ee1d5452820f94be13039426fb1d899c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:42 +0000

    Add package QtRangeSlider/0.1.5rc0/QtRangeSlider-0.1.5rc0.tar.gz

---
+++ packages/QtRangeSlider/QtRangeSlider-0.1.5rc0.tar.gz/QtRangeSlider-0.1.5rc0/examples/basic.py
@@ -0,0 +1,13 @@
+from qtrangeslider import QRangeSlider
+from qtrangeslider.qtcompat.QtCore import Qt
+from qtrangeslider.qtcompat.QtWidgets import QApplication
+
+app = QApplication([])
+
+slider = QRangeSlider(Qt.Horizontal)
+slider = QRangeSlider(Qt.Horizontal)
+
+slider.setValue((20, 80))
+slider.show()
+
+app.exec_()
