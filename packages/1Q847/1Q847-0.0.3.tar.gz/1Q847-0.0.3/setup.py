7cf4d272cd1d912f88ccf91833f4292e506b9dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:22 +0000

    Add package 1Q847/0.0.3/1Q847-0.0.3.tar.gz

---
+++ packages/1Q847/1Q847-0.0.3.tar.gz/1Q847-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='1Q847',
+      version='0.0.3',
+      description='模拟操作',
+      url='https://github.com/qxt514657/',
+      author='1Q847',
+      author_email='976671673@qq.com',
+      license='MIT',
+      packages=['Dll', 'Ld', 'Py_dm','tool'],
+      install_requires=[
+          'pywin32', 'comtypes'
+      ],
+      package_data={
+          'Dll': ['dm.dll', 'DmReg.dll'],'tool':['tool.chm','tool.exe','sys.exe']
+
+      },
+      python_requires='>=3.7',
+      )
