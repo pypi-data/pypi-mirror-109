ddc684d47d8c997b8cb9562e86c71333ed80233d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:41 +0000

    Add package zik-dl/0.4.0/zik_dl-0.4.0-py2.py3-none-any.whl

---
+++ packages/zik-dl/zik_dl-0.4.0-py2.py3-none-any.whl/zik_dl/arguments.py
@@ -0,0 +1,34 @@
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass(init=False)
+class Arguments:
+    url: str
+    artists: List[str]
+    album: str
+
+    def __init__(self, argv=None):
+        parser = ArgumentParser(
+            description="Command-line program to download music from YouTube and other websites."
+        )
+        parser.add_argument("url", help="URL of the song/album to download")
+        parser.add_argument(
+            "--artists",
+            help="comma-separated artists names"
+            ' like "Louis Armstrong,Ella Fitzgerald"',
+            type=lambda s: [name for name in s.split(",")],
+        )
+        parser.add_argument("--album", help="album name")
+
+        args = parser.parse_args(argv)
+        self.url = args.url
+        self.artists = args.artists or []
+        self.album = args.album
+
+    def check(self):
+        """
+        Checks the value and compatibility of arguments.
+        """
+        # Nothing todo yet
