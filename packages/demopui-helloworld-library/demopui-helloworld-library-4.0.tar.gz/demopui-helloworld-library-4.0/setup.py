3e9f7d73217ed0a56954cd544faf99c7e11fbc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:04 +0000

    Add package demopui-helloworld-library/4.0/demopui-helloworld-library-4.0.tar.gz

---
+++ packages/demopui-helloworld-library/demopui-helloworld-library-4.0.tar.gz/demopui-helloworld-library-4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="demopui-helloworld-library",
+    version='4.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='Somkiat',
+    author_email='somkiat.p@gmail.com',
+)
