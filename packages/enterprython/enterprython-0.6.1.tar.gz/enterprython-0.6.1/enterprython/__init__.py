44d2d3d10011e553d796907b060b45130b70ccd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:12 +0000

    Add package enterprython/0.6.1/enterprython-0.6.1.tar.gz

---
+++ packages/enterprython/enterprython-0.6.1.tar.gz/enterprython-0.6.1/enterprython/__init__.py
@@ -0,0 +1,15 @@
+from ._inject import add_value
+from ._inject import add_values
+from ._inject import assemble
+from ._inject import component
+from ._inject import factory
+from ._inject import set_values
+from ._inject import set_values_from_config
+from ._inject import value
+
+name = "enterprython"
+
+__author__ = "Tobias Hermann"
+__copyright__ = "Copyright 2018, Tobias Hermann"
+__email__ = "editgym@gmail.com"
+__license__ = "MIT"
