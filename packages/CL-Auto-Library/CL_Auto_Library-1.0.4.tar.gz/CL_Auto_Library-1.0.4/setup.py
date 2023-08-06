ae5cba69b6d89bf856020564ef4fb351087a93b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:05 +0000

    Add package CL-Auto-Library/1.0.4/CL_Auto_Library-1.0.4.tar.gz

---
+++ packages/CL-Auto-Library/CL_Auto_Library-1.0.4.tar.gz/CL_Auto_Library-1.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CL_Auto_Library", # A custom Library
+    version="1.0.4",
+    author="sieqqc",
+    author_email="sieqqc@gmail.com",
+    description="A custom Library",
+    long_description="A custom Library",
+    long_description_content_type="text/markdown",
+    url="http://10.21.37.110/corp_sieq/Partner%20Automation%20Testing%20Trial/_git/Custom_Library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
