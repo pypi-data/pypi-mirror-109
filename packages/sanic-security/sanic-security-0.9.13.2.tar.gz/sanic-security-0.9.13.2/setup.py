f0de9a3bdb2eacdf496d8600ae1cf441f6083e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:23 +0000

    Add package sanic-security/0.9.13.2/sanic-security-0.9.13.2.tar.gz

---
+++ packages/sanic-security/sanic-security-0.9.13.2.tar.gz/sanic-security-0.9.13.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as rm:
+    long_description = rm.read()
+
+with open("requirements.txt", "r") as rq:
+    requirements = [r.strip() for r in rq.readlines()]
+
+setup(
+    name='sanic-security',
+    version='0.9.13.2',
+    packages=setuptools.find_packages(),
+    url='https://github.com/sunset-developer/sanic-security',
+    license='GNU General Public License v3.0',
+    author='sunset-developer',
+    author_email='aidanstewart@sunsetdeveloper.com',
+    include_package_data=True,
+    description='A powerful, simple, and async security library for Sanic.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements
+)
