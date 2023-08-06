71be9ddce7e18ee85d91cb0354efa9e982f5b351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:24 +0000

    Add package performance-logger/1.0.1/performance_logger-1.0.1.tar.gz

---
+++ packages/performance-logger/performance_logger-1.0.1.tar.gz/performance_logger-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'performance_logger',
+    version          = '1.0.1',
+    description      = 'check performance of the func using decorator',
+    author           = 'Sean Kim',
+    author_email     = 'ddhyun93@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['logger'],
+    long_description = open('README.md').read(),
+    long_description_content_type='text/markdown',
+	include_package_data=True,
+	packages=find_packages(),
+    keywords         = ['performance_logger'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
