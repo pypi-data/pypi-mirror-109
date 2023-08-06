52033daa80e0c1513cd33111b0e3c0d485ed4776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:36 +0000

    Add package poetry-my-first-test/1.4.1/poetry-my-first-test-1.4.1.tar.gz

---
+++ packages/poetry-my-first-test/poetry-my-first-test-1.4.1.tar.gz/poetry-my-first-test-1.4.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_my_first_test', 'poetry_my_first_test.views']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.1,<9.0.0', 'requests>=2.25.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['my-cmd = poetry_my_first_test.cli:cli']}
+
+setup_kwargs = {
+    'name': 'poetry-my-first-test',
+    'version': '1.4.1',
+    'description': '',
+    'long_description': None,
+    'author': 'puntonim',
+    'author_email': 'puntonim@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
