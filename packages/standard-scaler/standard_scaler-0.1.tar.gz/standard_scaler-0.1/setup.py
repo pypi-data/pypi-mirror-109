576aeeac1f5434e0e87377c0a9a0c5194caf9214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:10 +0000

    Add package standard-scaler/0.1/standard_scaler-0.1.tar.gz

---
+++ packages/standard-scaler/standard_scaler-0.1.tar.gz/standard_scaler-0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'standard_scaler',
+  packages = ['standard_scaler'],
+  version = '0.1',     
+  license='MIT',    
+  description = 'An alternative to scikit-learn standard scaler', 
+  author = 'Hart Massie-Keller',
+  author_email = 'hartktraveller@gmail.com', 
+  url = 'https://github.com/hartktmk/standard_scaler',
+  download_url = 'https://github.com/hartktmk/standard_scaler/archive/refs/tags/0.1.tar.gz',
+  keywords = ['standard', 'scaler', 'scaling','transform','ml','ai'],
+  install_requires=[ 
+          'numpy',
+          'json',
+      ]
+)
