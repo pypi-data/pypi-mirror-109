9f06c5505801cf58dd75acec2afc87809a21e48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:20 +0000

    Add package testindatadev/0.0.47/testindatadev-0.0.47.tar.gz

---
+++ packages/testindatadev/testindatadev-0.0.47.tar.gz/testindatadev-0.0.47/testindatadev/dataset/metadata.py
@@ -0,0 +1,11 @@
+import json
+
+class MetaData():
+    def __init__(self, metadata):
+        # 数据格式检查
+        if not type(metadata) is dict:
+            raise Exception(f"metadata must be a dict, {type(metadata)} gavin")
+        self.meta = metadata
+
+    def ToString(self):
+        return json.dumps(self.meta)
