478c42db95e9bfde27ac795bd77d9b503b341fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:36 +0000

    Add package understory-db/0.0.8/understory-db-0.0.8.tar.gz

---
+++ packages/understory-db/understory-db-0.0.8.tar.gz/understory-db-0.0.8/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['understory']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pendulum>=2.1.2,<3.0.0',
+ 'redis>=3.5.3,<4.0.0',
+ 'sh>=1.14.2,<2.0.0',
+ 'understory-fx>=0.0.5,<0.0.6']
+
+setup_kwargs = {
+    'name': 'understory-db',
+    'version': '0.0.8',
+    'description': 'Tools for metamodern data management',
+    'long_description': '# understory-db\nTools for metamodern data management\n',
+    'author': 'Angelo Gladding',
+    'author_email': 'angelo@lahacker.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
