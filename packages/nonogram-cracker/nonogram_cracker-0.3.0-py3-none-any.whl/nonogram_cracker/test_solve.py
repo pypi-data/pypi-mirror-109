77a3eef1eef392fd9c3f00448d19499a5c1ba914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:42 +0000

    Add package nonogram-cracker/0.3.0/nonogram_cracker-0.3.0-py3-none-any.whl

---
+++ packages/nonogram-cracker/nonogram_cracker-0.3.0-py3-none-any.whl/nonogram_cracker/test_solve.py
@@ -0,0 +1,9 @@
+import json
+from .solve import solve
+
+def test_solve_puzzle_1():
+  puzzle = json.load(open('../../sample_data/puzzle1.json'))
+  expected = json.load(open('../../sample_data/puzzle1.solution.json', encoding='utf-8'))
+  print(expected)
+  actual_solution = solve(puzzle)
+  assert actual_solution == expected['solution']
