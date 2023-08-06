2dd259b9537f81f0808092c897aece30fc454a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:05 +0000

    Add package fai-helloworld-library/2.0/fai-helloworld-library-2.0.tar.gz

---
+++ packages/fai-helloworld-library/fai-helloworld-library-2.0.tar.gz/fai-helloworld-library-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fai-helloworld-library",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='Thasanee K.',
+    author_email='tasanee.fai@gmail.com',
+)
