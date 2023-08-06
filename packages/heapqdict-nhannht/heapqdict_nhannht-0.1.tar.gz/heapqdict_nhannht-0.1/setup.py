7cf25ab11c4aa7e91e3f3cf33454f8bc5c084a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:40 +0000

    Add package heapqdict-nhannht/0.1/heapqdict_nhannht-0.1.tar.gz

---
+++ packages/heapqdict-nhannht/heapqdict_nhannht-0.1.tar.gz/heapqdict_nhannht-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='heapqdict_nhannht',
+      version='0.1',
+      description='wrapper of heapq that work on dict',
+      packages=['HeapqDict'],
+      zip_safe=False)
+ 
