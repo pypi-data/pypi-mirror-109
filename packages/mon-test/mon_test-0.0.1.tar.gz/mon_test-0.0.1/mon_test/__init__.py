5b854c41f07e928d218c3e8c5a5d0223dbd833a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:02:43 +0000

    Add package mon-test/0.0.1/mon_test-0.0.1.tar.gz

---
+++ packages/mon-test/mon_test-0.0.1.tar.gz/mon_test-0.0.1/mon_test/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+    Ce module est un test
+"""
+
+
+__version__ = "0.0.1"
+ 
+from mon_test.mon_test_script import mon_script_methode
+from mon_test.mon_test_script import favorite_meal
+from mon_test.mon_test_script import voiture_de_reve
---
+++ packages/mon-test/mon_test-0.0.1.tar.gz/mon_test-0.0.1/mon_test/mon_test_script.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+    usage:
+    >>> from mon_test import mon_script_methode , favorite_meal, voiture_de_reve
+    >>> mon_script_methode()
+    >>> favorite_meal()
+    >>> voiture_de_reve()
+"""
+
+
+__all__ = ['mon_script_methode', 'favorite_meal', 'voiture_de_reve']
+
+def mon_script_methode():
+    print("Hello c'est moi le test, je fonctionne bien.")
+
+def favorite_meal():
+    print("la Pizza biensur")
+
+def voiture_de_reve():
+    print("Ford")
+ 
+def list_de_toutou():
+    print("Pomsky","husky", "malamute","Berger finois laponie")
+ 
+
+
+if __name__ == '__main__':
+    voiture_de_reve()
