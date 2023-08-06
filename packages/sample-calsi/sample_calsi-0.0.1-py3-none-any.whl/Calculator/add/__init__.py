9839767bf5d06f2b9992847e32ca1de94173e3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:49 +0000

    Add package sample-calsi/0.0.1/sample_calsi-0.0.1-py3-none-any.whl

---
+++ packages/sample-calsi/sample_calsi-0.0.1-py3-none-any.whl/Calculator/add/__init__.py
@@ -0,0 +1,17 @@
+class cal():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return a + b
+
+
+a = float(input("a:"))
+b = float(input("b:"))
+obj = cal(a, b)
+print(obj.add())
+
+
+def add():
+    return None
