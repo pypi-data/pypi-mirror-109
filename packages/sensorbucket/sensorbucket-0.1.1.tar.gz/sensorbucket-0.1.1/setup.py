88ce71a61388cb1468d3d86334b0d5dc8486e3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:25 +0000

    Add package sensorbucket/0.1.1/sensorbucket-0.1.1.tar.gz

---
+++ packages/sensorbucket/sensorbucket-0.1.1.tar.gz/sensorbucket-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sensorbucket',
+    version='0.1.1',
+    author='Tim van Osch',
+    author_email='timvosch@pollex.nl',
+    packages=['sensorbucket'],
+    url='http://pypi.python.org/pypi/Sensorbucket/',
+    license='LICENSE',
+    description='Provides interaction with the Sensorbucket API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
