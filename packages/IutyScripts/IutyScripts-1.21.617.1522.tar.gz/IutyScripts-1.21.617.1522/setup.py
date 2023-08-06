d534386f563f79c275e025656357755ff2603724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:38 +0000

    Add package IutyScripts/1.21.617.1522/IutyScripts-1.21.617.1522.tar.gz

---
+++ packages/IutyScripts/IutyScripts-1.21.617.1522.tar.gz/IutyScripts-1.21.617.1522/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup,find_packages
+
+name="IutyScripts"
+version = '1.21.0617.1522'
+
+author = "Iuty"
+author_email = "dfdfggg@126.com"
+
+packages = [
+        "IutyScripts.coding",
+        "IutyScripts.file",
+        "IutyScripts.entry",
+        "IutyScripts.image",
+    ]
+
+
+#data_files = [(r"D:/templates/","IutyScripts/templates/"),]
+#package_dir = {'templates': "IutyScripts/templates"}
+scripts = []
+install_requires = ["IutyLib"]
+
+entry_points={
+        'console_scripts': [
+            'ipb = IutyScripts.entry.ipb:main',
+            'ifs = IutyScripts.entry.ifs:main',
+            'iat = IutyScripts.entry.iat:main',
+            
+            'imgex = IutyScripts.entry.imgex:main',
+        ]
+    }
+
+
+
+
+
+setup(
+    name=name,
+    version= version,
+    author = author,
+    author_email = author_email,
+    #data_files = data_files,
+    packages=packages,
+    #package_dir=package_dir,
+    entry_points=entry_points,
+    install_requires = install_requires
+)
