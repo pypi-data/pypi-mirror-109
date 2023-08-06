e1b0a704911db8cf9c33a9a95216332eb3ed17ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:26 +0000

    Add package banddownfolder/0.1/banddownfolder-0.1.tar.gz

---
+++ packages/banddownfolder/banddownfolder-0.1.tar.gz/banddownfolder-0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='banddownfolder',
+    version='0.1',
+    description='Downfold Hamiltonian',
+    author='Xu He',
+    author_email='mailhexu@gmail.com',
+    license='GPLv3',
+    packages=find_packages(),
+    package_data={},
+    install_requires=['numpy', 'scipy',  'matplotlib', 'ase', 'numba', 
+        'netcdf4',
+        'jupyter',
+        ],
+    scripts=['scripts/phonopy_to_netcdf.py'
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ])
