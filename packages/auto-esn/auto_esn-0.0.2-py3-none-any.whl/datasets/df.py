48982f6320a0792ce5f6fc7a2fe71f06891bb8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:04:45 +0000

    Add package auto-esn/0.0.2/auto_esn-0.0.2-py3-none-any.whl

---
+++ packages/auto-esn/auto_esn-0.0.2-py3-none-any.whl/datasets/df.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+MackeyGlass = pd.read_csv("auto_esn/datasets/mg10.csv")
+Sunspot = pd.read_csv("auto_esn/datasets/sunspot.csv")
+SunspotSmooth = pd.read_csv("auto_esn/datasets/sunspotSmooth.csv")
