885ecdbd14023f918f87711cfa7b03e019d0c5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:37 +0000

    Add package penis/420.6969/penis-420.6969.tar.gz

---
+++ packages/penis/penis-420.6969.tar.gz/penis-420.6969/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+	name='penis',
+	version='420.6969',
+	description='This is prokect penis for the pp',
+	author='asarma0804',
+	packages=setuptools.find_packages(),
+	url='http://steam.com',
+	license='HOT POTATO LICENSE',
+	project_urls={
+		'TheHub':'https://pornhub.com/'
+			
+	}
+		
+)
