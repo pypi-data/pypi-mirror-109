0fd35ed7dc04230b7a6ae67217f3ca62758235db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:03:18 +0000

    Add package txtbcGen/0.0.3/txtbcGen-0.0.3-py3-none-any.whl

---
+++ packages/txtbcGen/txtbcGen-0.0.3-py3-none-any.whl/txtbcGen/reactions/MichaelisMenten.py
@@ -0,0 +1,35 @@
+from Reactions import Reactions
+
+class MichaelisMenten(Reactions):
+
+    def __init__(self, forwardSpecie: str, backwardSpecie: str, Vmax_f: float = 10, Km_b: float = 100, Vmax_b: float = 10, Km_f: float = 100):
+
+        super().__init__(forwardSpecie, backwardSpecie)
+        self.Vmax_f = Vmax_f
+        self.Km_f = Km_f 
+        self.Vmax_b = Vmax_b
+        self.Km_b = Km_b
+        self.type = "MichaelisMenten"
+
+    def computeForward(self, forwardValues: list):
+
+        return (self.Vmax_f * forwardValues[0]) / (self.Km_f + forwardValues[0])
+
+    def computeBackward(self, backwardValues: list):
+
+        return (self.Vmax_b * backwardValues[0]) / (self.Km_b + backwardValues[0])
+
+    def getEqHeaderStr(self, index):
+        return "{forward} <=> {backward} :R{i}".format(forward=self.fs[0], backward=self.bs[0], i=index)
+
+    def getForwardEqStr(self, index):
+        
+        return "Vmax{id}f * {fs} / (Km{id}f + {fs})".format(id=str(index), fs=self.fs[0])
+
+    def getBackwardEqStr(self, index):
+        
+        return "Vmax{id}b * {bs} / (Km{id}b + {bs})".format(id=str(index), bs=self.bs[0])
+
+    def getParams(self, index):
+
+        return {"Vmax{id}f".format(id=index): self.Vmax_f, "Vmax{id}b".format(id=index): self.Vmax_b, "Km{id}f".format(id=index): self.Km_f, "Km{id}b".format(id=index): self.Km_b}
