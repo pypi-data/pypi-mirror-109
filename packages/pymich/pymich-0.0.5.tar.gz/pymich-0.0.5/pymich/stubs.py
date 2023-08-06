8e06f0bf17c7ecb71400b55da3093776728aeccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:29 +0000

    Add package pymich/0.0.5/pymich-0.0.5.tar.gz

---
+++ packages/pymich/pymich-0.0.5.tar.gz/pymich-0.0.5/pymich/stubs.py
@@ -0,0 +1,19 @@
+import pymich.compiler
+
+from typing import List
+SENDER = "tz1"
+address = str
+Contract = List
+nat = int
+AMOUNT = 0
+BALANCE = 0
+
+
+def contract(a: address):
+    pass
+
+def transaction(a, b, c):
+    pass
+
+def mutez(amount: int):
+    pass
