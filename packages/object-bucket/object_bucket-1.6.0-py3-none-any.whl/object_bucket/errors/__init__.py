f1909c77e71dc543506fbb42d577f4e8a200ac11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:26 +0000

    Add package object-bucket/1.6.0/object_bucket-1.6.0-py3-none-any.whl

---
+++ packages/object-bucket/object_bucket-1.6.0-py3-none-any.whl/object_bucket/errors/__init__.py
@@ -0,0 +1,3 @@
+from .bucket_error import DropletDoesNotExistsError  # noqa: F401
+from .bucket_error import DropletExistsError  # noqa: F401
+from .bucket_error import DropletTypeError  # noqa: F401
