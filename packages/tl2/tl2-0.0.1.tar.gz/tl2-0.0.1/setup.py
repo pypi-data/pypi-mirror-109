a9e63b50ecbaf2dc011ad3b3e4cd4eea7d656422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:46 +0000

    Add package tl2/0.0.1/tl2-0.0.1.tar.gz

---
+++ packages/tl2/tl2-0.0.1.tar.gz/tl2-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tl2",
+    version="0.0.1",
+    author="Peterou",
+    author_email="pengzhoucv@gmail.com",
+    description="A personal package for research",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PeterouZh/tl2",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        # 'Django >= 1.11, != 1.11.1, <= 2',
+        'easydict'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
