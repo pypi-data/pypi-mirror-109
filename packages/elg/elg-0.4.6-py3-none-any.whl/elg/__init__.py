1797eda05770f9d4f4e27e2f52ea8319f5fd3501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:39 +0000

    Add package elg/0.4.6/elg-0.4.6-py3-none-any.whl

---
+++ packages/elg/elg-0.4.6-py3-none-any.whl/elg/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.6"
+
+from .authentication import Authentication
+from .benchmark import Benchmark
+from .catalog import Catalog
+from .corpus import Corpus
+from .entity import Entity
+from .flask_service import FlaskService
+from .pipeline import Pipeline
+from .provider import Provider
+from .service import Service
