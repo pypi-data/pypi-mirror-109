09a3bcd9e808f3f899dc94323be08504e6f69542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:57:37 +0000

    Add package discord-command/0.0.1/discord_command-0.0.1.tar.gz

---
+++ packages/discord-command/discord_command-0.0.1.tar.gz/discord_command-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discord_command",
+    version="0.0.1",
+    author="Takana",
+    author_email="thetakana68@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thetakana/discord_command",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
