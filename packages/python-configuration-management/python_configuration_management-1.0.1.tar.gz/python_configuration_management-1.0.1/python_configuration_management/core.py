923609cf130f78acce461db6ff682baadfe3e9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:31 +0000

    Add package python-configuration-management/1.0.1/python_configuration_management-1.0.1.tar.gz

---
+++ packages/python-configuration-management/python_configuration_management-1.0.1.tar.gz/python_configuration_management-1.0.1/python_configuration_management/core.py
@@ -0,0 +1,27 @@
+import os
+
+from python_configuration_management.utils import (
+    load_env,
+    normalize_config_data,
+)
+from python_configuration_management.yml_utils import yml_to_dict
+
+
+def get_config(environment: str, use_dotenv=False, use_secrets=True):
+    if use_secrets:
+        if use_dotenv:
+            load_env(environment)
+        else:
+            assert os.getenv("ENC_KEY"), "ENC_KEY not present in environment variables"
+
+    config = yml_to_dict(environment)
+    normalized_config = normalize_config_data(config)
+
+    return {**normalized_config}
+
+
+def inject_config(environment: str, settings_module, use_dotenv=False, use_secrets=True):
+    config = get_config(environment, use_dotenv=use_dotenv, use_secrets=use_secrets)
+
+    for key, value in config.items():
+        setattr(settings_module, key, value)
