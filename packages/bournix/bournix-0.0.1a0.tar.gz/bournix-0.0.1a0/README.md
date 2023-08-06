9c31d52c87102c95b58cd95527c1c78422724ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:55:19 +0000

    Add package bournix/0.0.1a0/bournix-0.0.1a0.tar.gz

---
+++ packages/bournix/bournix-0.0.1a0.tar.gz/bournix-0.0.1a0/README.md
@@ -0,0 +1,2 @@
+# bournix-on-chain
+Bournix team utils for django
---
+++ packages/bournix/bournix-0.0.1a0.tar.gz/bournix-0.0.1a0/setup.py
@@ -0,0 +1,27 @@
+import io
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+with io.open("src/bournix/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+setup(
+    name="bournix",
+    version=version,
+    description="Bournix team utils for django",
+    long_description=readme,
+    author="Mahdi Zarrintareh",
+    author_email="mza2rintareh@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=[],
+    extras_require={},
+    entry_points={},
+)
