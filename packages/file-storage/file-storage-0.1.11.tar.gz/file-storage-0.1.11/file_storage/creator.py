15cd574f243d24b42ba3000a914c87c6297ad546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:58 +0000

    Add package file-storage/0.1.11/file-storage-0.1.11.tar.gz

---
+++ packages/file-storage/file-storage-0.1.11.tar.gz/file-storage-0.1.11/file_storage/creator.py
@@ -0,0 +1,16 @@
+from .entities import Type, Key
+
+from .storage import Storage
+from .ftp import FtpServer
+from .s3 import S3
+from .disk import Disk
+
+def new_storage_by_type(storage_type: Type, key: Key) -> Storage:
+    if storage_type == Type.FTP:
+        return FtpServer(key.url, key.username, key.password)
+    if storage_type == Type.S3:
+        return S3(key.url, key.username, key.password)
+    if storage_type == Type.DISK:
+        return Disk()
+        
+    return None
