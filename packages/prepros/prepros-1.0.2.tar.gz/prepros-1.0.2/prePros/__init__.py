d1ca40ae11d3aa2473eb1eca478d4d0cfbabd2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:30 +0000

    Add package prepros/1.0.2/prepros-1.0.2.tar.gz

---
+++ packages/prepros/prepros-1.0.2.tar.gz/prepros-1.0.2/prePros/__init__.py
@@ -0,0 +1,6 @@
+from .pros import pros
+from .imageTransform import imageTransform
+from .imageFilter import imageFilter
+from .edgeDetection import edgeDetection
+from .dataAugmentation import dataAugmenation
+
---
+++ packages/prepros/prepros-1.0.2.tar.gz/prepros-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (
+    name = "prepros",
+    version = "1.0.2",
+    description = "prePros",
+    packages = ['prePros'],
+    zip_safe = False
+)
