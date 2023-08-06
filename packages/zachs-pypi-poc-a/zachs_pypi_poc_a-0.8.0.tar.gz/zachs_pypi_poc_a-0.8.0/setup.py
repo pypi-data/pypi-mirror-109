5b04dc76a4be5df2b2d80b3028e8fa899cbaa358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:14 +0000

    Add package zachs-pypi-poc-a/0.8.0/zachs_pypi_poc_a-0.8.0.tar.gz

---
+++ packages/zachs-pypi-poc-a/zachs_pypi_poc_a-0.8.0.tar.gz/zachs_pypi_poc_a-0.8.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='zachs_pypi_poc_a',
+      version='0.8.0',
+      description='Pypi proof of concept module',
+      author='Zach Harris',
+      author_email='zharris@talend.com',
+      license='MIT',
+      packages=['zachs_pypi_poc_a'],
+      zip_safe=False)
