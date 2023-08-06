1db781cdfcbb5b8b87847f25840567a7e1a45490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:31 +0000

    Add package smartystreets-python-sdk/4.8.4/smartystreets_python_sdk-4.8.4.tar.gz

---
+++ packages/smartystreets-python-sdk/smartystreets_python_sdk-4.8.4.tar.gz/smartystreets_python_sdk-4.8.4/smartystreets_python_sdk/us_street/analysis.py
@@ -0,0 +1,18 @@
+class Analysis:
+    def __init__(self, obj):
+        """
+        See "https://smartystreets.com/docs/cloud/us-street-api#analysis"
+        """
+        self.dpv_match_code = obj.get('dpv_match_code', None)
+        self.dpv_footnotes = obj.get('dpv_footnotes', None)
+        self.cmra = obj.get('dpv_cmra', None)
+        self.vacant = obj.get('dpv_vacant', None)
+        self.active = obj.get('active', None)
+        self.dpv_no_stat = obj.get('dpv_no_stat', None)
+        self.is_ews_match = obj.get('ews_match', DeprecationWarning)
+        self.footnotes = obj.get('footnotes', None)
+        self.lacs_link_code = obj.get('lacslink_code', None)
+        self.lacs_link_indicator = obj.get('lacslink_indicator', None)
+        self.is_suite_link_match = obj.get('suitelink_match', None)
+        self.match_mode = obj.get('match_mode', None)
+        self.match_details = obj.get('match_details', None)
