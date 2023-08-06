bd62cca3bb7f7fdef227b7d0370446210bed54c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:05 +0000

    Add package net-templates/0.1.4/net_templates-0.1.4.tar.gz

---
+++ packages/net-templates/net_templates-0.1.4.tar.gz/net_templates-0.1.4/tests/tests_ios/test_IosAaaMethods.py
@@ -0,0 +1,73 @@
+import unittest
+from tests.tests_ios.BaseTemplateTestIos import BaseTemplateTestIos
+from net_models.models.services.vi.AaaMethods import *
+
+
+class TestIosAaaAction(BaseTemplateTestIos):
+
+    TEMPLATE_NAME = 'ios_aaa_action_list'
+
+    def test_01(self):
+        test_cases = [
+            {
+                "test_name": "Test-Local-01",
+                "data": {
+                    "params": [
+                        {
+                            "action": "local"
+                        }
+                    ]
+                },
+                "result": (
+                    "local"
+                )
+            },
+            {
+                "test_name": "Test-Group-Local-01",
+                "data": {
+                    "params": [
+                        {
+                            "action": "group",
+                            "group": "TACACS-GROUP"
+                        },
+                        {
+                            "action": "local"
+                        }
+                    ]
+                },
+                "result": (
+                    "group TACACS-GROUP local"
+                )
+            }
+        ]
+        super().common_testbase(test_cases=test_cases)
+
+
+class TestIosAaaAuthentication(BaseTemplateTestIos):
+
+    TEST_CLASS = IosAaaAuthentication
+    TEMPLATE_NAME = 'ios_aaa_authentication_methods'
+
+    def test_01(self):
+        test_cases = self.get_test_cases_from_resources()
+        super().common_testbase(test_cases=test_cases)
+
+
+class TestIosAaaAuthorization(BaseTemplateTestIos):
+
+    TEST_CLASS = IosAaaAuthorization
+    TEMPLATE_NAME = 'ios_aaa_authorization'
+
+    def test_01(self):
+        test_cases = self.get_test_cases_from_resources()
+        super().common_testbase(test_cases=test_cases)
+
+
+class TestIosAaaAccounting(BaseTemplateTestIos):
+
+    TEST_CLASS = IosAaaAccounting
+    TEMPLATE_NAME = 'ios_aaa_accounting'
+
+    def test_01(self):
+        test_cases = self.get_test_cases_from_resources()
+        super().common_testbase(test_cases=test_cases)
