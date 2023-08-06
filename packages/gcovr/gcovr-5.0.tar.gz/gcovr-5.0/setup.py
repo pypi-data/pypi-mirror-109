67b71c86cfc361e66ff6a7efe6201b385b89290e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:45 +0000

    Add package gcovr/5.0/gcovr-5.0.tar.gz

---
+++ packages/gcovr/gcovr-5.0.tar.gz/gcovr-5.0/setup.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+
+#  ************************** Copyrights and license ***************************
+#
+# This file is part of gcovr 5.0, a parsing and reporting tool for gcov.
+# https://gcovr.com/en/stable
+#
+# _____________________________________________________________________________
+#
+# Copyright (c) 2013-2021 the gcovr authors
+# Copyright (c) 2013 Sandia Corporation.
+# This software is distributed under the BSD License.
+# Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+# the U.S. Government retains certain rights in this software.
+# For more information, see the README.rst file.
+#
+# ****************************************************************************
+
+"""
+Script to generate the installer for gcovr.
+"""
+
+from runpy import run_path
+from setuptools import setup, find_packages
+
+
+version = run_path('./gcovr/version.py')['__version__']
+
+setup(name='gcovr',
+      version=version,
+      platforms=["any"],
+      python_requires='>=3.6',
+      packages=find_packages(include=['gcovr*'], exclude=['gcovr.tests']),
+      install_requires=[
+          'jinja2',
+          'lxml',
+          'pygments'
+      ],
+      package_data={
+          'gcovr': ['templates/*.css', 'templates/*.html'],
+      },
+      entry_points={
+          'console_scripts': [
+              'gcovr=gcovr.__main__:main',
+          ],
+      },
+      )
