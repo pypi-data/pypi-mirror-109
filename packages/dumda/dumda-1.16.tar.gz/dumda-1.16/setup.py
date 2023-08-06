e7cfed646117f1e054430f812854b56a0829c6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:32 +0000

    Add package dumda/1.16/dumda-1.16.tar.gz

---
+++ packages/dumda/dumda-1.16.tar.gz/dumda-1.16/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='dumda',
+      version='1.16',
+      description='generate highly customizable dummy data for data science testing',
+      keywords='data science python',
+      url='https://github.com/oliverbdot/dumda',
+      author='Oliver B.',
+      author_email='oliverbcontact@gmail.com',
+      license='MIT',
+      packages=['dumda'],
+      include_package_data=True,
+      zip_safe=False)
