82a7ed3d52dd4f6a2c11bb92e266659aea370463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:04 +0000

    Add package segregation/2.0.0/segregation-2.0.0-py3-none-any.whl

---
+++ packages/segregation/segregation-2.0.0-py3-none-any.whl/segregation/tests/test_distance_decay_isolation.py
@@ -0,0 +1,17 @@
+import unittest
+from libpysal.examples import load_example
+import geopandas as gpd
+import numpy as np
+from segregation.singlegroup import DistanceDecayIsolation
+
+
+class Distance_Decay_Isolation_Tester(unittest.TestCase):
+    def test_Distance_Decay_Isolation(self):
+        s_map = gpd.read_file(load_example("Sacramento1").get_path("sacramentot2.shp"))
+        df = s_map[['geometry', 'HISP', 'TOT_POP']]
+        index = DistanceDecayIsolation(df, 'HISP', 'TOT_POP')
+        np.testing.assert_almost_equal(index.statistic, 0.14913778285850937)
+
+
+if __name__ == '__main__':
+    unittest.main()
