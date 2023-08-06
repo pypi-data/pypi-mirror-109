0355e9a333e4812f42febff2eee4bceb5d97326d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:19 +0000

    Add package maxembeds/0.0.1/maxembeds-0.0.1.tar.gz

---
+++ packages/maxembeds/maxembeds-0.0.1.tar.gz/maxembeds-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (
+    name = "maxembeds",
+    version = "0.0.1",
+    description = "Awesome EmbedBuilder for discord.py",
+    py_modules = ["MaxEmbeds"],
+    package_dir = {"": "src"}
+)
