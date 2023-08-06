dab7066fa691e68b1305c26761da6ae5cad4a134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:04 +0000

    Add package blip-sdk/1.0.2/blip_sdk-1.0.2-py3-none-any.whl

---
+++ packages/blip-sdk/blip_sdk-1.0.2-py3-none-any.whl/blip_sdk/extensions/artificial_intelligence/__init__.py
@@ -0,0 +1,7 @@
+from .ai_model import *
+from .ai_analytics import *
+from .content_assistant import *
+from .entities import *
+from .intents import *
+from .word_set import *
+from .ai_extension import AiExtension
