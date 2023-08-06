b8a928222969bd0375092257f08b2bd2248868a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:59:03 +0000

    Add package DPPMyLib/2.0/DPPMyLib-2.0.tar.gz

---
+++ packages/DPPMyLib/DPPMyLib-2.0.tar.gz/DPPMyLib-2.0/DPPMyLib/apiHelper.py
@@ -0,0 +1,15 @@
+def print_hi(minPop, countryID):
+    import requests
+
+    url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
+
+    querystring = {"countryIds":countryID,"minPopulation": minPop}
+
+    headers = {
+        'x-rapidapi-key': "c94b07d5fcmsh192324421c65896p1d93d1jsna064f463a452",
+        'x-rapidapi-host': "wft-geo-db.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
