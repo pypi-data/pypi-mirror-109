073b9fcd42105733c94158e918e6e124896d6f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:50 +0000

    Add package gamgee/0.0.4/gamgee-0.0.4-py3-none-any.whl

---
+++ packages/gamgee/gamgee-0.0.4-py3-none-any.whl/gamgee/errors.py
@@ -0,0 +1,39 @@
+import json
+from typing import Optional
+
+class SamError(Exception): pass
+
+
+class HttpError(SamError): 
+    status_code: int = None
+    default_message: str = None
+
+    def json(self, message: Optional[str] = None):
+        return {
+            "status_code": self.status_code,
+            "body": json.dumps({
+                "success": False,
+                "message": self.default_message if message is None else message
+            })
+        }
+
+
+class RequestParseError(HttpError): 
+    status_code = 400
+    default_message = "Error parsing request."
+
+
+class AuthenticationError(HttpError): 
+    status_code = 401
+    default_message = "Unable to authenticate."
+
+
+class AuthorizationError(HttpError): 
+    status_code = 403
+    default_message = "Unauthorized."
+
+class InternalServerError(HttpError): 
+    status_code = 500
+    default_message = "Internal server error."
+
+
