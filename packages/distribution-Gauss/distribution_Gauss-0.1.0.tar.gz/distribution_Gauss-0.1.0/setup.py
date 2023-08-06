84da8eff043b20208f42c34c5cddeeabd010dd8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:36 +0000

    Add package distribution-Gauss/0.1.0/distribution_Gauss-0.1.0.tar.gz

---
+++ packages/distribution-Gauss/distribution_Gauss-0.1.0.tar.gz/distribution_Gauss-0.1.0/setup.py
@@ -0,0 +1,16 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name='distribution_Gauss',
+      version='0.1.0',
+      description='Gaussian distributions',
+      packages=['distribution_Gauss'],
+      author = 'Pongrak Keawsrinuan',
+      author_email = 'pongrak.k@hotmail.com',
+      zip_safe=False)
