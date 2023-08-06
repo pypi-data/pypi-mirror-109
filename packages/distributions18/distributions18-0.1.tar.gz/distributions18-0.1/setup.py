9cd5c363bf3ece3ee97ecfd0caf9a9b8321b9990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:50 +0000

    Add package distributions18/0.1/distributions18-0.1.tar.gz

---
+++ packages/distributions18/distributions18-0.1.tar.gz/distributions18-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions18',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions18'],
+      author = 'Andrew Alarcon',
+      zip_safe=False)
