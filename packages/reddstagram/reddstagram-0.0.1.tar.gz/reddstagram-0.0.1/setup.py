4a6e2d81d37cac1a3a774f22de39ed2e933517d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:01 +0000

    Add package reddstagram/0.0.1/reddstagram-0.0.1.tar.gz

---
+++ packages/reddstagram/reddstagram-0.0.1.tar.gz/reddstagram-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Automated Reddit to Instagram'
+LONG_DESCRIPTION = 'A package for automated Reddit subforum scraping and uploading to Instagram.'
+
+setup(
+    name="reddstagram",
+    version=VERSION,
+    author="FOGT Solutions",
+    author_email="<contact@fogt-solutions.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pillow', 'pathlib', 'instabot', 'praw', 'wget'],
+    keywords=['python', 'reddit', 'instagram'],
+)
