ae3d51c3ae806165b59e05943abc71c0f435481b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:52 +0000

    Add package quakeio/0.0.2/quakeio-0.0.2-py3-none-any.whl

---
+++ packages/quakeio/quakeio-0.0.2-py3-none-any.whl/quakeio/__init__.py
@@ -0,0 +1,48 @@
+# Claudio Perez
+__version__ = "0.0.2"
+
+from pathlib import Path
+
+from . import (
+    csmip,
+    # nga,
+    eqsig,
+    basic_formats
+)
+
+
+FILE_TYPES = {}
+FILE_TYPES.update(csmip.FILE_TYPES)
+FILE_TYPES.update(eqsig.FILE_TYPES)
+FILE_TYPES.update(basic_formats.FILE_TYPES)
+
+DEFAULT_TYPES = {
+    ".zip": "csmip.zip",
+    ".v2": "csmip.v2",
+    ".json": "json.record",
+}
+
+
+def read(read_file, *args, **kwds):
+    """
+    Generic ground motion reader
+    """
+    if "input_format" in kwds and kwds["input_format"]:
+        typ = kwds["input_format"]
+    else:
+        try:
+            typ = DEFAULT_TYPES[Path(read_file).suffix]
+        except KeyError:
+            raise ValueError("Unable to deduce input format")
+    return FILE_TYPES[typ]["read"](read_file, *args, **kwds)
+
+
+def write(write_file, ground_motion, write_format=None, *args, **kwds):
+    if write_format:
+        typ = write_format
+    else:
+        try:
+            typ = DEFAULT_TYPES[Path(write_file).suffix]
+        except KeyError:
+            raise ValueError("Unable to deduce output format")
+    FILE_TYPES[typ]["write"](write_file, ground_motion, *args, **kwds)
