bbe258a1004e886efb9805fae9c06ec7a5d8e78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:13 +0000

    Add package stl-helloworld-library/2.0/stl-helloworld-library-2.0.tar.gz

---
+++ packages/stl-helloworld-library/stl-helloworld-library-2.0.tar.gz/stl-helloworld-library-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="stl-helloworld-library",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='Suparat',
+    author_email='suparat.tapthong@gmail.com',
+)
