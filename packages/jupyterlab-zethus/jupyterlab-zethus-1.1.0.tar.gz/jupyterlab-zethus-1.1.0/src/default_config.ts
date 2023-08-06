8e88ccbf571e49baf4b8ca459c73a0a82058c2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:54 +0000

    Add package jupyterlab-zethus/1.1.0/jupyterlab-zethus-1.1.0.tar.gz

---
+++ packages/jupyterlab-zethus/jupyterlab-zethus-1.1.0.tar.gz/jupyterlab-zethus-1.1.0/src/default_config.ts
@@ -0,0 +1,51 @@
+export const default_config = (defaultROSEndpoint: string) => {
+  return `{
+        "panels": {
+          "sidebar": {
+            "display": true,
+            "collapsed": false
+          },
+          "header": {
+            "display": true
+          },
+          "info": {
+            "display": true,
+            "collapsed": true
+          }
+        },
+        "ros": {
+          "endpoint": "${defaultROSEndpoint}"
+        },
+        "infoTabs": [],
+        "visualizations": [],
+        "globalOptions": {
+          "display": true,
+          "backgroundColor": {
+            "display": true,
+            "value": 15790320
+          },
+          "fixedFrame": {
+            "display": true,
+            "value": "world"
+          },
+          "grid": {
+            "display": true,
+            "size": 30,
+            "divisions": 30,
+            "color": 11184810,
+            "centerlineColor": 7368816
+          }
+        },
+        "tools": {
+          "mode": "controls",
+          "controls": {
+            "display": false,
+            "enabled": true
+          },
+          "measure": {
+            "display": false
+          },
+          "custom": []
+        }
+      }`;
+};
