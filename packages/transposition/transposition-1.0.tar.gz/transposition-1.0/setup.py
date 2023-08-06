ef655cf693e5f5def9bb3801f9c8d1484edb1c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:48 +0000

    Add package transposition/1.0/transposition-1.0.tar.gz

---
+++ packages/transposition/transposition-1.0.tar.gz/transposition-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='transposition',
+    version='1.0',
+    license='MIT',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    packages=['solution'],
+    include_package_data=True,
+    zip_safe=False
+)
