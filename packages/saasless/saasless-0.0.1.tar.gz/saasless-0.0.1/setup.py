63380a8d7fab9dfccbf51d60ef8883776daf70e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:32 +0000

    Add package saasless/0.0.1/saasless-0.0.1.tar.gz

---
+++ packages/saasless/saasless-0.0.1.tar.gz/saasless-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+NAME='saasless'
+VERSION='0.0.1'
+
+packages = find_packages(exclude=["instance","tests.*", "tests", "requirements", "deploy","docs"])
+
+setup(
+    name=NAME,
+    version=VERSION,
+    entry_points={'console_scripts': [
+        f'{NAME}={NAME}:cli',
+        # [[[cog 
+        # if values.extensions['desktop.py'].enabled:
+        #   p("f'{NAME}-ui={NAME}:ui',\n")
+        # ]]]
+        # [[[end]]]        
+        ]},
+    python_requires=">3.7.0",
+    packages=packages,
+    extras_require={},
+    include_package_data=True,
+    install_requires=[],
+    test_suite="tests",
+)
