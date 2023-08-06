3dab72f804ea03eb9816bad279240fce4be56ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:55 +0000

    Add package beniutils/0.0.36/beniutils-0.0.36.tar.gz

---
+++ packages/beniutils/beniutils-0.0.36.tar.gz/beniutils-0.0.36/setup.py
@@ -0,0 +1,24 @@
+version = "0.0.36"
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "beniutils",
+    version = version,
+    keywords="beni",
+    description = "utils library for Beni",
+    license = "MIT License",
+    url = "https://pypi.org/project/beniutils/",
+    author = "Beni",
+    author_email = "benimang@126.com",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = [
+        "aiohttp>=3.6.2",
+        "aiofiles>=0.4.0",
+        "xlrd>=1.2.0",
+        "selenium>=3.141.0",
+        "selenium-wire>=4.3.0",
+    ],
+)
