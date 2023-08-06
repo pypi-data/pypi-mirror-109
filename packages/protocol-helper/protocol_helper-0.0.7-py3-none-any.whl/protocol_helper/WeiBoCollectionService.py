a04a7cca28e0612c81bc566bdd676cec891ee1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:36 +0000

    Add package protocol-helper/0.0.7/protocol_helper-0.0.7-py3-none-any.whl

---
+++ packages/protocol-helper/protocol_helper-0.0.7-py3-none-any.whl/protocol_helper/WeiBoCollectionService.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author:LeisureMan
+# email:LeisureMam@gmail.com
+# datetime:2021-06-11 16:34
+# software: PyCharm
+from abc import ABC
+
+from . import RequestBase
+
+
+class WeiBoCollectionService(RequestBase, ABC):
+
+    def __init__(self):
+        super(WeiBoCollectionService, self).__init__()
+
+    def get_fans(self, url):
+        """
+        获取粉丝数据
+        Args:
+            url:
+
+        Returns:
+
+        """
+
+    def get_article(self, url):
+        """
+        获取文章数据
+        Args:
+            url:
+
+        Returns:
+
+        """
