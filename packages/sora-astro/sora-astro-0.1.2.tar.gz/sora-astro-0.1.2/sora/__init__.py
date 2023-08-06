c4d723cd8024bd84b97ad46597ff445babc001be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:17 +0000

    Add package sora-astro/0.1.2/sora-astro-0.1.2.tar.gz

---
+++ packages/sora-astro/sora-astro-0.1.2.tar.gz/sora-astro-0.1.2/sora/__init__.py
@@ -0,0 +1,7 @@
+from .ephem import EphemKernel, EphemPlanete, EphemJPL
+from .observer import Observer
+from .star import Star
+from .lightcurve import LightCurve
+from .occultation import Occultation
+
+__version__ = '0.1.2'
