d14fa7cc7329293b5b45f6c409223addbda9dfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:03 +0000

    Add package py-entangle/0.2.1/py_entangle-0.2.1-py3-none-any.whl

---
+++ packages/py-entangle/py_entangle-0.2.1-py3-none-any.whl/entangle/examples/example5.py
@@ -0,0 +1,59 @@
+# pylint: disable=locally-disabled, multiple-statements, no-value-for-parameter, unexpected-keyword-arg, invalid-name, too-many-function-args, unused-import, missing-function-docstring
+"""
+TBD
+"""
+import json
+from entangle.logging.debug import logging
+from entangle.process import process
+from entangle.http import request
+from entangle.workflow import workflow
+from entangle.scheduler import scheduler
+
+scheduler_config = {'cpus': 3,
+                    'impl': 'entangle.scheduler.DefaultScheduler'}
+
+
+@scheduler(**scheduler_config)
+@process
+@request(url='https://datausa.io/api/data', method='GET')
+def mydata(data):
+    data = json.loads(data)
+    print('My function got the data! ', data)
+    return int(data['data'][0]['Year'])
+
+
+@scheduler(**scheduler_config)
+@process
+def two():
+    return 2
+
+
+@scheduler(**scheduler_config)
+@process
+def add(a, b):
+    v = int(a) + int(b)
+    print("ADD: *"+str(v)+"*")
+    return v
+
+
+@scheduler(**scheduler_config)
+@workflow
+def workflow1():
+    return add(
+        mydata(drilldowns='Nation', measures='Population'),
+        two()
+    )
+
+
+@scheduler(**scheduler_config)
+@workflow
+def workflow2(value):
+    return add(
+        value(),
+        two()
+    )
+
+
+result = workflow2(workflow1)
+
+print(result())
---
+++ packages/py-entangle/py_entangle-0.2.1-py3-none-any.whl/entangle/examples/listexample.py
@@ -0,0 +1,53 @@
+# pylint: disable=locally-disabled, multiple-statements, no-value-for-parameter, invalid-name, too-many-function-args, unused-import, missing-function-docstring
+"""
+TBD
+"""
+import threading
+import time
+from entangle.logging.info import logging
+from entangle.dataflow import thread
+from entangle.dataflow import process
+from entangle.dataflow import dataflow
+
+
+def triggered(func, result):
+    print("triggered: {} {}".format(func.__name__, result))
+
+
+@dataflow(callback=triggered)
+@thread
+def printx(x):
+    print('printx: {}'.format(threading.current_thread().name))
+    return "  X: {}".format(x)
+
+
+@dataflow(callback=triggered)
+@thread
+def printy(y):
+    print('printy: {}'.format(threading.current_thread().name))
+    return "Y: {}".format(y)
+
+
+@dataflow(callback=triggered)
+@thread
+def printz(z):
+    print('printz: {}'.format(threading.current_thread().name))
+    return "     Z: {}".format(z)
+
+
+@dataflow(callback=triggered, maxworkers=5)
+def emit(a):
+    print('emit: {}'.format(threading.current_thread().name))
+    return a+"!"
+
+flow = emit(
+    lambda x: [printy(
+        printz(
+            printx()
+        )
+        ) for _ in range(0, 10)]
+)
+
+flow('emit')
+
+time.sleep(2)
