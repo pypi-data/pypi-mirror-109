138adca7a3c1426d697ef6ae3804a64d61d92fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:14 +0000

    Add package testfoopublish/0.0.3/testfoopublish-0.0.3.tar.gz

---
+++ packages/testfoopublish/testfoopublish-0.0.3.tar.gz/testfoopublish-0.0.3/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='testfoopublish',
+    version='0.0.3',
+    long_description='# testfoopublish\n\n[![PyPI - Version](https://img.shields.io/pypi/v/testfoopublish.svg)](https://pypi.org/project/testfoopublish)\n[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/testfoopublish.svg)](https://pypi.org/project/testfoopublish)\n\n-----\n\n**Table of Contents**\n\n- [Installation](#installation)\n- [License](#license)\n\n## Installation\n\n```console\npip install testfoopublish\n```\n\n## License\n\n`testfoopublish` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.\n',
+    author='U.N. Owen',
+    author_email='void@some.where',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=[
+        'testfoopublish',
+        'tests',
+    ],
+)
