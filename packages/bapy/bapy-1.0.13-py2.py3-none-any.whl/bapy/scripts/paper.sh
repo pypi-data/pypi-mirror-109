0f2a54e2ae9f80fcfd861254792ed8c6e2738b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:54 +0000

    Add package bapy/1.0.13/bapy-1.0.13-py2.py3-none-any.whl

---
+++ packages/bapy/bapy-1.0.13-py2.py3-none-any.whl/bapy/scripts/paper.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+test -n "${DARWIN}" || exit
+export starting="${BASH_SOURCE[0]}"; debug.sh starting
+
+function paperchoice() {
+  local starting
+  export starting="${FUNCNAME[0]}"; debug.sh starting
+  python -c "import random; import glob; print(random.choice(glob.glob('${FILES}/Wall Papers/*')))"
+}
+
+if isuserdarwin.sh; then
+  screens="$(wallpaper get --screen all | wc -l | xargs)"
+  wallpaper set "$(paperchoice)" --scale auto --screen main
+  ((screens -= 1))
+  until [[ "${screens}" -eq 0 ]]; do
+    wallpaper set "$(paperchoice)" --scale auto --screen "${screens}"
+    ((screens -= 1))
+  done
+fi
+[[ ! "${TIMES-}" ]] || { export end="${starting}"; debug.sh end; trap times EXIT; }
+unset end starting
