b4c22ed07733b624613e5937e05dc2144d0a9c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:30 +0000

    Add package scrapy-scraping-link/0.0.5/scrapy-scraping-link-0.0.5.tar.gz

---
+++ packages/scrapy-scraping-link/scrapy-scraping-link-0.0.5.tar.gz/scrapy-scraping-link-0.0.5/scrapy_scraping_link/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['ScrapingLinkRequest', 'ScrapingLinkMiddleware', 'ScrapingLinkSpider']
+
+from scrapy_scraping_link.request import ScrapingLinkRequest
+from scrapy_scraping_link.middleware import ScrapingLinkMiddleware
+from scrapy_scraping_link.spider import ScrapingLinkSpider
