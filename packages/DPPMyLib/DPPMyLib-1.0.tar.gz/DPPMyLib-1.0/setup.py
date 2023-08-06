52ffe15c8a21732e43b4e0ec2e1c5d01dcf4414a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:55:53 +0000

    Add package DPPMyLib/1.0/DPPMyLib-1.0.tar.gz

---
+++ packages/DPPMyLib/DPPMyLib-1.0.tar.gz/DPPMyLib-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DPPMyLib',
+    version='1.0',
+    author='Yan Machulski',
+    author_email='janmaczulski@gmail.com',
+    packages=find_packages(),
+)
