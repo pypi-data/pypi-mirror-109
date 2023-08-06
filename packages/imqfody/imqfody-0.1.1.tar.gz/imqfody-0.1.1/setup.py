67603f8249e13acd4c35192d697d2a741694eec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:10 +0000

    Add package imqfody/0.1.1/imqfody-0.1.1.tar.gz

---
+++ packages/imqfody/imqfody-0.1.1.tar.gz/imqfody-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['imqfody']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'imqfody',
+    'version': '0.1.1',
+    'description': 'Python wrapper for IntelMQ Fody API',
+    'long_description': None,
+    'author': '3c7',
+    'author_email': '3c7@posteo.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
