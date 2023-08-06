5d71fe22adeb8670a054664733cbec6c5c430ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:07 +0000

    Add package bapy/1.0.15/bapy-1.0.15.tar.gz

---
+++ packages/bapy/bapy-1.0.15.tar.gz/bapy-1.0.15/bapy/scripts/secrets-push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+export starting="${BASH_SOURCE[0]}"; debug.sh starting
+
+if isuserdarwin.sh; then
+  test -n "${GITHUB_SECRETS_PATH}" || { error.sh GITHUB_SECRETS_PATH 'not defined'; exit 1; }
+  cd "${GITHUB_SECRETS_PATH}" || { error.sh "${GITHUB_SECRETS_PATH}" 'not found'; exit 1; }
+  sudo chown -R "${USERNAME}":"$( id -g "${USERNAME}" )" "${USERHOME}/.ssh"
+  gall.sh
+  cd - > /dev/null || exit 1
+fi
+[[ ! "${TIMES-}" ]] || { export end="${starting}"; debug.sh end; trap times EXIT; }
+unset end
+
+unset starting
