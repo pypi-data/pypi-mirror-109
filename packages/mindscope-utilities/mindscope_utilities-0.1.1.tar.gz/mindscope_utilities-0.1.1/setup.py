ef09048ecf824f075b2a7838f7c3dc6f4e360ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:14 +0000

    Add package mindscope-utilities/0.1.1/mindscope_utilities-0.1.1.tar.gz

---
+++ packages/mindscope-utilities/mindscope_utilities-0.1.1.tar.gz/mindscope_utilities-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='mindscope_utilities',
+    version='0.1.1',
+    description='Utilities for loading, manipulating and visualizing data from the Allen Institute Mindscope program',
+    url='https://github.com/AllenInstitute/mindscope_utilities',
+    author='Allen Institute',
+    author_email='marinag@alleninstitute.org, iryna.yavorska@alleninstitute.org, kater@alleninstitute.org, dougo@alleninstitute.org,',
+    license='Allen Institute',
+    install_requires=[
+        'flake8',
+        'pytest',
+        'allensdk==2.11.2',
+    ],
+)
