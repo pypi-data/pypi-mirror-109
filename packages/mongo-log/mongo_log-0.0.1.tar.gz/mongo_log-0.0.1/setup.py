a8dad91b1c29a8951be6b2edb1ede69cde930be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:50 +0000

    Add package mongo-log/0.0.1/mongo_log-0.0.1.tar.gz

---
+++ packages/mongo-log/mongo_log-0.0.1.tar.gz/mongo_log-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mongo_log",
+    version="0.0.1",
+    author="Steven Wang",
+    author_email="brightstar8284@icloud.com",
+    description="use mongo db to log",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StevenLianaL/mongo_log",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
