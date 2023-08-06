583b7271ca0efc2044a61f760b9ad8e885e51efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:52 +0000

    Add package valvepcf/1.0.0/valvepcf-1.0.0.tar.gz

---
+++ packages/valvepcf/valvepcf-1.0.0.tar.gz/valvepcf-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name='valvepcf',
+    packages=['valvepcf'],
+    version='1.0.0',
+    license='gpl-3.0',
+    description='A library to parse .pcf files used by the source engine.',
+    author='Maxime Dupuis',
+    author_email='mdupuis@hotmail.ca',
+    url='https://github.com/pySourceSDK/ValvePCF',
+    download_url='https://github.com/pySourceSDK/ValveBSP/archive/v1.0.0.tar.gz',
+    keywords=['pcf', 'source', 'sourcesdk', 'hammer', 'valve'],
+    install_requires=['construct', 'future'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
