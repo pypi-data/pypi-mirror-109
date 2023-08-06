c367b0d4e0eb1bfe27e4cc6f09079673106e7a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:46 +0000

    Add package veracore-api-client/0.0.3a2/veracore_api_client-0.0.3a2.tar.gz

---
+++ packages/veracore-api-client/veracore_api_client-0.0.3a2.tar.gz/veracore_api_client-0.0.3a2/veracore_api_client/__init__.py
@@ -0,0 +1,8 @@
+"""VeraCore REST API Client Package"""
+# flake8: noqa
+
+from .api import VeraCore
+from .exceptions import \
+        VeraCoreRequestForbidden, \
+        VeraCoreRequestError, \
+        VeraCoreNoSession
