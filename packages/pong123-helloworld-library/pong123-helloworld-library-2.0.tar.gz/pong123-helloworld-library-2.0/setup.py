725152fa1ee8271797e7fa27c5650f1839ff6ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:15 +0000

    Add package pong123-helloworld-library/2.0/pong123-helloworld-library-2.0.tar.gz

---
+++ packages/pong123-helloworld-library/pong123-helloworld-library-2.0.tar.gz/pong123-helloworld-library-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pong123-helloworld-library",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/tawatchai18/demo-helloworld-library',
+    author='Somkiat',
+    author_email='somkiat.p@gmail.com',
+)
