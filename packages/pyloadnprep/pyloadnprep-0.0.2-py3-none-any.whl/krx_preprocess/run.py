1cecaf9ef903f25d2f08cebe82d84a4f84173a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:18 +0000

    Add package pyloadnprep/0.0.2/pyloadnprep-0.0.2-py3-none-any.whl

---
+++ packages/pyloadnprep/pyloadnprep-0.0.2-py3-none-any.whl/krx_preprocess/run.py
@@ -0,0 +1,12 @@
+from preprocessing import technical_indices
+
+#https://wikidocs.net/78954
+#디렉터리 구조
+#krx_preprocess
+#-krx_preprocess
+##-__init__.py
+##-preprocessing.py
+
+
+#-setup.py
+#-README.md
