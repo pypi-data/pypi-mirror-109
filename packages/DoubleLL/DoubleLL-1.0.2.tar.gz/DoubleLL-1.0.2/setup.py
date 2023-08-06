f4b96480b4787f5dad09808a377c58d3a6218451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:06 +0000

    Add package DoubleLL/1.0.2/DoubleLL-1.0.2.tar.gz

---
+++ packages/DoubleLL/DoubleLL-1.0.2.tar.gz/DoubleLL-1.0.2/setup.py
@@ -0,0 +1,24 @@
+  
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DoubleLL", 
+    version="1.0.2",
+    author="Маскаленко Максим",
+    author_email="Maxs.ru2002@gmail.com",
+    description="Двусвязный список в Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lev1asan",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "DLL"},
+    packages=setuptools.find_packages(where="DLL"),
+    python_requires=">=3.0",
+)
