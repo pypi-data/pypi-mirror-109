ec74633f38cd524475c0862c8610321e4e5558c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:02 +0000

    Add package dbcat/0.5.2/dbcat-0.5.2.tar.gz

---
+++ packages/dbcat/dbcat-0.5.2.tar.gz/dbcat-0.5.2/dbcat/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+__version__ = "0.5.2"
+
+import yaml
+
+from dbcat.catalog import Catalog
+from dbcat.dbcat import pull  # type: ignore
+
+
+def catalog_connection(config: str) -> Catalog:
+    config_yaml = yaml.safe_load(config)
+    return Catalog(**config_yaml["catalog"])
