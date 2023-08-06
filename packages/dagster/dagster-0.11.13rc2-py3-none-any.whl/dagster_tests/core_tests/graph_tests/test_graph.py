1d42dde686f23ea0c09666da599e97c90efde52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:41 +0000

    Add package dagster/0.11.13rc2/dagster-0.11.13rc2-py3-none-any.whl

---
+++ packages/dagster/dagster-0.11.13rc2-py3-none-any.whl/dagster_tests/core_tests/graph_tests/test_graph.py
@@ -0,0 +1,62 @@
+from dagster import execute_pipeline, resource, solid
+from dagster.core.definitions.decorators.graph import graph
+from dagster.core.definitions.graph import GraphDefinition
+from dagster.core.execution.execute import execute_in_process
+
+
+def get_solids():
+    @solid
+    def emit_one(_):
+        return 1
+
+    @solid
+    def add(_, x, y):
+        return x + y
+
+    return emit_one, add
+
+
+def test_basic_graph():
+    emit_one, add = get_solids()
+
+    @graph
+    def get_two():
+        return add(emit_one(), emit_one())
+
+    assert isinstance(get_two, GraphDefinition)
+
+    result = execute_in_process(get_two)
+
+    assert result.success
+
+
+def test_composite_graph():
+    emit_one, add = get_solids()
+
+    @graph
+    def add_one(x):
+        return add(emit_one(), x)
+
+    @graph
+    def add_two(x):
+        return add(add_one(x), emit_one())
+
+    assert isinstance(add_two, GraphDefinition)
+
+
+def test_with_resources():
+    @resource
+    def a_resource(_):
+        return "a"
+
+    @solid(required_resource_keys={"a"})
+    def needs_resource(context):
+        return context.resources.a
+
+    @graph
+    def my_graph():
+        needs_resource()
+
+    # proxy for "executable/job"
+    result = execute_pipeline(my_graph.to_job(resource_defs={"a": a_resource}))
+    assert result.success
