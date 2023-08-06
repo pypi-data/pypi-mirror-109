6237c93e1b3d87f8dfbfe3d3c1ee1402e5f00f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:36 +0000

    Add package qmctorch/0.2.0/qmctorch-0.2.0.tar.gz

---
+++ packages/qmctorch/qmctorch-0.2.0.tar.gz/qmctorch-0.2.0/qmctorch/solver/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['SolverBase', 'SolverSlaterJastrow',
+           'SolverSlaterJastrowHorovod']
+
+from .solver_base import SolverBase
+from .solver_slater_jastrow import SolverSlaterJastrow
+from .solver_slater_jastrow_horovod import SolverSlaterJastrowHorovod
