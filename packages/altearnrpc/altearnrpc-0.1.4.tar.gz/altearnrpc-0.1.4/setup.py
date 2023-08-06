f1bfc9f3ae4f08bbfaad0b309410842cda6d467c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:01 +0000

    Add package altearnrpc/0.1.4/altearnrpc-0.1.4.tar.gz

---
+++ packages/altearnrpc/altearnrpc-0.1.4.tar.gz/altearnrpc-0.1.4/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="altearnrpc",
+    version="0.1.4",
+    description="Un client Discord RPC pour Altearn",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/Aeris1One/altearn-discord-rpc",
+    author="Aeris One",
+    author_email="aeris@e.email",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["altearnrpc"],
+    include_package_data=True,
+    install_requires=["pypresence", "tqdm"],
+    entry_points={
+        "console_scripts": [
+            "altearnrpc=altearnrpc.__main__:main",
+        ]
+    },
+)
