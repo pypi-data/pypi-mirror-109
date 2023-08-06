e942cefde20025dca296d65f3d803939c81b771f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:26 +0000

    Add package world-rowing/0.1.3/world_rowing-0.1.3.tar.gz

---
+++ packages/world-rowing/world_rowing-0.1.3.tar.gz/world_rowing-0.1.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='world_rowing',
+    version='0.1.3',
+    description='Library for loading and presenting data from the worldrowing.com',
+    author='Matthew Grifiths',
+    author_email='matthewghgriffiths@gmail.com',
+    url='https://github.com/matthewghgriffiths/rowing',
+    packages=['world_rowing'],
+    entry_points={
+        'console_scripts': [
+            'world_rowing = world_rowing.cli:run'
+        ]
+    },
+    license='MIT', 
+    long_description=long_description, 
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'matplotlib',
+        'requests',
+        'cmd2>=2.0.0'
+    ],
+    python_requires=">=3.8",
+)
