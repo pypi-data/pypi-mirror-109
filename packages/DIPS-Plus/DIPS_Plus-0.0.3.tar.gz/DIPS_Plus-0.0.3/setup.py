8d7cecbc0718bca911427bf0d55fb19641da5ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:36 +0000

    Add package DIPS-Plus/0.0.3/DIPS_Plus-0.0.3.tar.gz

---
+++ packages/DIPS-Plus/DIPS_Plus-0.0.3.tar.gz/DIPS_Plus-0.0.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DIPS_Plus',
+    version='0.0.3',
+    description='The Enhanced Database of Interacting Protein Structures for Interface Prediction',
+    author='Alex Morehead',
+    author_email='alex.morehead@gmail.com',
+    url='https://github.com/amorehead/DIPS-Plus',
+    install_requires=['setuptools', 'dill', 'tqdm', 'easy-parallel-py3', 'atom3-py3', 'click'],
+    packages=find_packages(),
+)
