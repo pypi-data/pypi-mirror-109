bf117e5fc83cef1f8cd81eb7e3e147c977eebfc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:11 +0000

    Add package LWJpack/1.0/LWJpack-1.0.tar.gz

---
+++ packages/LWJpack/LWJpack-1.0.tar.gz/LWJpack-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='LWJpack',            # 对外模块名
+    version='1.0',         # 版本号
+    description='无',     # 描述
+    author='liweijian',          # 作者
+    author_email='339338717@qq.com',    # 邮箱
+    py_modules=['liweijian_1.module1', 'liweijian_1.module2']       # 要发布的模块
+)
