61d388442906bebed4f6aa47806ab1ddc26adce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:54 +0000

    Add package shangqi-cloud-lib/0.1.8/shangqi_cloud_lib-0.1.8.tar.gz

---
+++ packages/shangqi-cloud-lib/shangqi_cloud_lib-0.1.8.tar.gz/shangqi_cloud_lib-0.1.8/shangqi_cloud_lib/utils/ThreadUtil.py
@@ -0,0 +1,15 @@
+from threading import Thread
+
+
+class MyThread(Thread):
+
+    def __init__(self, target, args=()):
+        super(MyThread, self).__init__()
+        self.func = target
+        self.args = args
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        return self.result
