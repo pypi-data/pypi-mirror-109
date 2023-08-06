052fb51e2b3aefc8b7f19c63f4b1e8ab4dd4c53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:02 +0000

    Add package charts-pythondouble/0.0.2/charts_pythondouble-0.0.2.tar.gz

---
+++ packages/charts-pythondouble/charts_pythondouble-0.0.2.tar.gz/charts_pythondouble-0.0.2/charts_pythondouble/main.py
@@ -0,0 +1,18 @@
+import pyecharts
+from pyecharts.charts import *
+class Pycharts:
+    def __init__(self):
+        pass
+    def line(self,xais=list,yais=list,name=str,filename=str):
+        
+        bar = Line()
+        bar.add_xaxis(xais)
+        bar.add_yaxis(name,yais)
+        bar.render(filename+".html")
+    def bar(self,xais=list,yais=list,name=str,filename=str):
+        
+        bar2 = Bar()
+        bar2.add_xaxis(xais)
+        bar2.add_yaxis(name,yais)
+        bar2.render(filename+".html")
+
