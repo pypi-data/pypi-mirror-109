ff3c6f99f1f39b4d7880ac48d30d6cddb3075be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:29 +0000

    Add package odoo12-addon-foodcoopbcn/12.0.0.0.4.99.dev1/odoo12_addon_foodcoopbcn-12.0.0.0.4.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-foodcoopbcn/odoo12_addon_foodcoopbcn-12.0.0.0.4.99.dev1-py3-none-any.whl/odoo/addons/foodcoopbcn/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Odoo customizations for FoodCoopBCN",
+    'version': '12.0.0.0.4',
+    'depends': ['easy_my_coop_es'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': """
+    Odoo customizations for FoodCoopBCN.
+    """,
+    "license": "AGPL-3",
+    'data': [
+        "data/ir_default.xml",
+        "views/become_cooperator_view.xml",
+        "views/subscription_request_view.xml",
+    ],
+}
