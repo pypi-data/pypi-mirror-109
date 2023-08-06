c498d6e37dd263c6d5218e85005fe73d47a1cd5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:26 +0000

    Add package firstimpression/0.5.5/firstimpression-0.5.5.tar.gz

---
+++ packages/firstimpression/firstimpression-0.5.5.tar.gz/firstimpression-0.5.5/firstimpression/api/__init__.py
@@ -0,0 +1,5 @@
+from firstimpression.api import bbc
+from firstimpression.api import ns
+from firstimpression.api import nu_nl
+from firstimpression.api import traffic
+from firstimpression.api import request
