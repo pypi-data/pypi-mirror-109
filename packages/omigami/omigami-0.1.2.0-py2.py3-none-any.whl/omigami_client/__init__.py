d99681222bb12dd188a26f808fa7e1fbc4fceb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:02 +0000

    Add package omigami/0.1.2.0/omigami-0.1.2.0-py2.py3-none-any.whl

---
+++ packages/omigami/omigami-0.1.2.0-py2.py3-none-any.whl/omigami_client/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Omigami."""
+
+__author__ = """Data Revenue GmbH"""
+__email__ = "carlos@datarevenue.com"
+__version__ = "0.1.0"
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+from omigami.spec2vec import Spec2Vec
