be3f78c9d83331b2b7827f87ed0cda19d04174f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:03 +0000

    Add package harshit/0.0.1/harshit-0.0.1-py3-none-any.whl

---
+++ packages/harshit/harshit-0.0.1-py3-none-any.whl/harshit/harshit.py
@@ -0,0 +1,13 @@
+def square():
+	n = int(input('Enter the number :'))
+	print('Square is :', n*n)
+
+def greeting():
+	print('My name is Harshit :)')
+	nme = input('What\'s your name ? ')
+	print('Hello ', nme)
+
+about = 'My name is Harshit. I am a Python developer'
+
+if __name__ == '__main__':
+	square()
