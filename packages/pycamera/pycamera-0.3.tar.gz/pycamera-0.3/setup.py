546d0fc1246f20572154c3617b348e3b14ff3bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:56 +0000

    Add package pycamera/0.3/pycamera-0.3.tar.gz

---
+++ packages/pycamera/pycamera-0.3.tar.gz/pycamera-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+readme = ""
+with open("README-pypi.md") as f:
+    readme = f.read()
+
+setup(
+    name="pycamera",
+    version="0.3",
+    description="An easier solution to computer vision.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IsmaeelAkram/pycamera",
+    author="Ismaeel Akram",
+    python_requires=">=3.4.0",
+    license="MIT",
+    install_requires=["opencv-python", "numpy"],
+    packages=["pycamera"],
+    zip_safe=False,
+)
