06e15a129ba40328b477eb18abf6f0e798c7bd18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:50 +0000

    Add package lpea/1.0.1/lpea-1.0.1.tar.gz

---
+++ packages/lpea/lpea-1.0.1.tar.gz/lpea-1.0.1/README.md
@@ -0,0 +1,3 @@
+# lpea
+from lpea import *
+Algorithm.handle(E或者D, 待解密密文, 待加密明文)
---
+++ packages/lpea/lpea-1.0.1.tar.gz/lpea-1.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="lpea",
+    version="1.0.1",
+    author="Lin Siyu",
+    author_email="lsy20113@163.com",
+    description="lp加密算法",
+    long_description='调用此方法进行lp加密',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
