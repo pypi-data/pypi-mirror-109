5f26ea5722a51ea9e3abaa639f16c2a462b0f5dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:55 +0000

    Add package Query2Vec/0.1.2/Query2Vec-0.1.2.tar.gz

---
+++ packages/Query2Vec/Query2Vec-0.1.2.tar.gz/Query2Vec-0.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+
+setuptools.setup(
+    name = "Query2Vec",
+    packages = ["Query2Vec"],
+    version = "0.1.2",
+    license = 'MIT',
+    author = "Maede Ashofteh Barabadi",
+    description = "Query2Vec package using word2vec and tfidf",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    install_requires = [
+    	"sklearn",
+    	"gensim",
+    	"tqdm",
+    	"numpy"
+    ],
+    classifiers = [
+    	"Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+)
