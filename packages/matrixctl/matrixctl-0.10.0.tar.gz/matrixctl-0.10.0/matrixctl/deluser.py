47ce45e8c3ae39fc6e09753ff4608eb497fe39a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:34 +0000

    Add package matrixctl/0.10.0/matrixctl-0.10.0.tar.gz

---
+++ packages/matrixctl/matrixctl-0.10.0.tar.gz/matrixctl-0.10.0/matrixctl/deluser.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# matrixctl
+# Copyright (c) 2020  Michael Sasser <Michael@MichaelSasser.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Use this module to add the ``deluser`` subcommand to ``matrixctl``."""
+
+from __future__ import annotations
+
+import logging
+
+from argparse import ArgumentParser
+from argparse import Namespace
+from argparse import _SubParsersAction as SubParsersAction
+
+from .errors import InternalResponseError
+from .handlers.api import API
+from .handlers.toml import TOML
+
+
+__author__: str = "Michael Sasser"
+__email__: str = "Michael@MichaelSasser.org"
+
+
+logger = logging.getLogger(__name__)
+
+
+def subparser_deluser(subparsers: SubParsersAction) -> None:
+    """Create a subparser for the ``matrixctl deluser`` command.
+
+    Parameters
+    ----------
+    subparsers : argparse._SubParsersAction
+        The object which is returned by
+        ``parser.add_subparsers()``.
+
+    Returns
+    -------
+    None
+
+    """
+    parser: ArgumentParser = subparsers.add_parser(
+        "deluser", help="Deletes a user"
+    )
+    parser.add_argument("user", help="The username to delete")
+    parser.set_defaults(func=deluser)
+
+
+def deluser(arg: Namespace) -> int:
+    """Delete a user from the the matrix instance.
+
+    Parameters
+    ----------
+    arg : argparse.Namespace
+        The ``Namespace`` object of argparse's ``parse_args()``
+
+    Returns
+    -------
+    err_code : int
+        Non-zero value indicates error code, or zero on success.
+
+    """
+    toml: TOML = TOML()
+    api: API = API(toml.get("API", "Domain"), toml.get("API", "Token"))
+    try:
+        api.url.path = f"deactivate/@{arg.user}:{toml.get('API','Domain')}"
+        api.url.api_version = "v1"
+        api.method = "POST"
+        api.request({"erase": True})
+    except InternalResponseError:
+        logger.error("The user was not deleted.")
+
+    return 0
+
+
+# vim: set ft=python :
