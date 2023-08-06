54e7e330855de3b4a35fca255b1912bb4ecf2d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:12 +0000

    Add package saturn5/0.0.1/saturn5-0.0.1-py3-none-any.whl

---
+++ packages/saturn5/saturn5-0.0.1-py3-none-any.whl/saturn5/__init__.py
@@ -0,0 +1,16 @@
+'''
+description:
+author: ['Felipe Sá']
+version: 1.0.0
+created: 2021-06-14
+last modified date: 2021-06-14
+'''
+
+
+""" saturn5. An example python library. """
+
+__version__ = "0.0.1"
+__author__ = 'Felipe Sá'
+
+from .saturn5 import launch
+
