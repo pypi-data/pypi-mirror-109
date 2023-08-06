f4ea5fa7f91dfc1c4235069e3fe681d32cee4be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:34 +0000

    Add package ledxbot/2.5.8/ledxbot-2.5.8.tar.gz

---
+++ packages/ledxbot/ledxbot-2.5.8.tar.gz/ledxbot-2.5.8/Ld/defaults.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+“Commons Clause” License Condition v1.0
+Copyright Oli 2019-2020
+The Software is provided to you by the Licensor under the
+License, as defined below, subject to the following condition.
+Without limiting other conditions in the License, the grant
+of rights under the License will not include, and the License
+does not grant to you, the right to Sell the Software.
+For purposes of the foregoing, “Sell” means practicing any or
+all of the rights granted to you under the License to provide
+to third parties, for a fee or other consideration (including
+without limitation fees for hosting or consulting/ support
+services related to the Software), a product or service whose
+value derives, entirely or substantially, from the functionality
+of the Software. Any license notice or attribution required by
+the License must also include this Commons Clause License
+Condition notice.
+Software: PartyBot (fortnitepy-bot)
+License: Apache 2.0
+
+You are one honestly one of the saddest person to ever live
+and I genuinely dislike you. People like you think the
+world is yours and you can just take it. I spent hours and
+hours writing this code for you to just steal it, I hope you
+feel better about yourself after you exploit my honest code
+for money, for supporters, for discord members, etc.
+
+Fuck you.
+"""
+
+
+class Defaults:
+    def __init__(self, data: dict = {}) -> None:
+        self.raw = data
+
+        self.skin = data.get('default_skin', 'CID_028_Athena_Commando_F')
+        self.backpack = data.get('default_backpack', 'BID_023_Pinkbear')
+        self.pickaxe = data.get('default_pickaxe', 'Pickaxe_Lockjaw')
+        self.banner = data.get('banner', 'otherbanner51')
+        self.banner_colour = data.get('banner_colour', 'defaultcolor15')
+        self.level = data.get('default_level', 999)
+        self.tier = data.get('default_bp_tier', 999999999)
+        self.emote = data.get('default_emote', 'EID_NoDance')
+        self.status = data.get('status', 'https://discord.gg/2kqu5SZMWZ')
+        self.welcome = data.get('welcome', 'Notice: Join discord.gg/2kqu5SZMWZ for a free lobby bot.')
+        self.whisper = data.get('whisper', 'https://discord.gg/2kqu5SZMWZ.')
---
+++ packages/ledxbot/ledxbot-2.5.8.tar.gz/ledxbot-2.5.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '2.5.8'
+DESCRIPTION = 'a advanced bot for fortnite'
+LONG_DESCRIPTION = 'You want your Short Description to be 2 to 3 sentences and the rest of your content in the Long Description. Not only does the Short Description help you with SEO but it also used for marketplaces such as Google Products, TheFind, Amazon, etc.  Please do not stick HTML code in the Short Description as it might cause the product not to be sent to these marketplaces.'
+
+# Setting up
+setup(
+    name="ledxbot",
+    version=VERSION,
+    author="Led (HarryD)",
+    author_email="<ledxbotofficial@outlook.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['aiofiles', 'aiohttp', 'fortnitepy', 'sanic', 'BenBotAsync', 'FortniteAPIAsync', 'crayons'],
+    keywords=['python', 'sanic', 'BenBotAsync', 'FortniteAPIAsync'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
