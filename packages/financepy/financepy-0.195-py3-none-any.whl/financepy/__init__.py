04ce718c69a231178acfd3d2b38ee4ab3a19fcde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:25 +0000

    Add package financepy/0.195/financepy-0.195-py3-none-any.whl

---
+++ packages/financepy/financepy-0.195-py3-none-any.whl/financepy/__init__.py
@@ -0,0 +1,11 @@
+cr = "\n"
+
+s = "####################################################################" + cr
+s += "# FINANCEPY BETA Version " + str('0.195') + " - This build:  13 Jun 2021 at 18:22 #" + cr
+s += "#      This software is distributed FREE & WITHOUT ANY WARRANTY    #" + cr
+s += "# For info and disclaimer - https://github.com/domokane/FinancePy  #" + cr
+s += "#      Send any bug reports or comments to quant@financepy.com     #" + cr
+s += "####################################################################"
+s += cr
+
+print(s)
