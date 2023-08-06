fd2d1ea72266295a2ccc8ef3b8a240adf6ca74eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:56:43 +0000

    Add package rashsetup/0.0.2/RashSetup-0.0.2.tar.gz

---
+++ packages/rashsetup/RashSetup-0.0.2.tar.gz/RashSetup-0.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rashsetup']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'rashsetup',
+    'version': '0.0.2',
+    'description': 'Setup Module that can be used for both testing Rash and also Setting up Rash',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
