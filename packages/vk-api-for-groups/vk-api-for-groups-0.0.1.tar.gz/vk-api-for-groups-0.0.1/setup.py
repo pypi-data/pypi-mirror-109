fd3b0fe7ba2f13857b1afa7dc3e2ddfd8b0b4b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:17 +0000

    Add package vk-api-for-groups/0.0.1/vk-api-for-groups-0.0.1.tar.gz

---
+++ packages/vk-api-for-groups/vk-api-for-groups-0.0.1.tar.gz/vk-api-for-groups-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='vk-api-for-groups',
+    version='0.0.1',
+    description='A package that provides simple vk-api integration. Mostly for groups-related methods.',
+    url='https://github.com/Rokoel/api_for_groups_vk',
+    author='Rokoel',
+    author_email='rokoelvel@gmail.com',
+    license='MIT Licence',
+    packages=[''],
+    install_requires=["setuptools>=42",
+                      "requests",
+                      ],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
---
+++ packages/vk-api-for-groups/vk-api-for-groups-0.0.1.tar.gz/vk-api-for-groups-0.0.1/src/__init__.py
@@ -0,0 +1,8 @@
+"""
+vk_api_for_groups.
+
+Python package, that allows posting to VKontakte's groups with ease.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Rokoel'
