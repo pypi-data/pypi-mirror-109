06117e202513a983e31edb1def6211374b133272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:44 +0000

    Add package eegwlib/0.1.3/eegwlib-0.1.3.tar.gz

---
+++ packages/eegwlib/eegwlib-0.1.3.tar.gz/eegwlib-0.1.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='eegwlib',
+    version='0.1.3',
+    author='Evan Hathaway',
+    author_email='evan.hathaway@bel.company',
+    description='Helper functions for EEG workflows',
+    packages=['eegwlib'],
+)
