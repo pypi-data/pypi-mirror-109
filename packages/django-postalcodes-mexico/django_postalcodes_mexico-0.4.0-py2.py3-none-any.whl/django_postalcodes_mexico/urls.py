205b9038c7aedd1fb111bd4391ca8c4bd33bc754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:08 +0000

    Add package django-postalcodes-mexico/0.4.0/django_postalcodes_mexico-0.4.0-py2.py3-none-any.whl

---
+++ packages/django-postalcodes-mexico/django_postalcodes_mexico-0.4.0-py2.py3-none-any.whl/django_postalcodes_mexico/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.urls import path
+from django.views.generic import TemplateView
+
+from . import views
+
+
+app_name = 'django_postalcodes_mexico'
+urlpatterns = [
+    # There are postal codes with leading zeros, hence slug is prefered over int
+    path('postal-code/<slug:postal_code>/', views.getPostalCodeData, name='get-postal-code-data'),
+    path('postal-code-search/<slug:postal_code>/', views.searchSimilarPostalCode, name='search-postal-code-data')
+]
