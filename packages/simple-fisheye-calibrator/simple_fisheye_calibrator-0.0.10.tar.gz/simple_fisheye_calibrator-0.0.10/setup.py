240fdfa14f87b52efe23d15f4a04242ce9f605f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:27 +0000

    Add package simple-fisheye-calibrator/0.0.10/simple_fisheye_calibrator-0.0.10.tar.gz

---
+++ packages/simple-fisheye-calibrator/simple_fisheye_calibrator-0.0.10.tar.gz/simple_fisheye_calibrator-0.0.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+from os import listdir
+
+with open("README.md") as f:
+    long_description = f.read()
+
+scripts = ["scripts/"+i for i in listdir("scripts")]
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+if __name__ == "__main__":
+    setup(
+        name="simple_fisheye_calibrator",
+        scripts=scripts,
+        version="0.0.10",
+        description="Simple GUI-based correction of fisheye images. The correction parameters specified on the screen can be diverted to opencv's fisheye correction parameters.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Katsuya Hyodo",
+        author_email="rmsdh122@yahoo.co.jp",
+        url="https://github.com/PINTO0309/simple_fisheye_calibrator",
+        license="MIT License",
+        packages=find_packages(),
+        platforms=["linux", "unix"],
+        python_requires=">3.6",
+        install_requires=_requires_from_file('requirements.txt'),
+    )
