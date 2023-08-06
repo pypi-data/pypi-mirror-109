9a4137c4fde0d176d1ea931216d2e5ff024138a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:53 +0000

    Add package mlaas/0.0.10/mlaas-0.0.10.tar.gz

---
+++ packages/mlaas/mlaas-0.0.10.tar.gz/mlaas-0.0.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.10' 
+DESCRIPTION = 'ML as a service toolkit'
+LONG_DESCRIPTION = 'ML as a service toolkit'
+
+install_requires = [
+    'requests',
+]
+# Setting up
+setup(
+        name="mlaas", 
+        version=VERSION,
+        author="dvirginz@gmail.com",
+        author_email="<dvirginz@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        keywords=['python', 'ML'],
+        install_requires=install_requires,
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+        ]
+)
